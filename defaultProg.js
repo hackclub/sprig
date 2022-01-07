522d07574f555cbbc0f397c6c187e17e3f646c48
Cedric Hutchings <ced@misguided.enterprises>
2022-01-07 15:13:18 -0500

    Rotation -> rotate, radians -> degrees

--- defaultProg.js
+++ defaultProg.js
@@ -3,6 +3,7 @@ const canvas = document.querySelector(".game-canvas");
 
 const e = new Engine(canvas, 300, 300);
 
+
 e.add({
 	tags: ["player"],
 	solid: true,
@@ -11,6 +12,7 @@ e.add({
 	// sprite: sprite_jeb,
 	// scale: 2,
 	draw: (obj) => {
+		obj.rotation += 0.019;
 		if (e.heldKey("ArrowLeft")) obj.x -= 3;
 		if (e.heldKey("ArrowRight")) obj.x += 3;
 	},
