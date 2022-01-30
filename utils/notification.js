c5ff4c2b92e52d915193c5276b479798616a9234
Max Wofford <max@maxwofford.com>
2022-01-30 15:13:35 -0500

    Remove outdated notification commented logic

--- utils/notification.js
+++ utils/notification.js
@@ -1,7 +1,6 @@
 // should write: we're creating elements in here, but we should redo to fit with uhtml
 
 export default function notification({message, type}={}) {
-  // document.querySelector(".shared-modal").classList.toggle("hide");
   const el = document.createElement('div')
   el.classList.add('shared-modal')
   el.innerText = message
