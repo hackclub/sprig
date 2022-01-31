9a8d606e73fdff9cfb0a205adae603fbaf6b61de
Max Wofford <max@maxwofford.com>
2022-01-31 17:25:29 -0500

    Improve project name sanitizing

--- components/nameBar.js
+++ components/nameBar.js
@@ -1,15 +1,28 @@
 import { dispatch } from '../dispatch.js'
 import { html } from '../uhtml.js'
 
+const save = (e) => {
+  const name = dispatch("SET_NAME", { name: e.target.innerText })
+  e.target.textContent = name
+  dispatch("RENDER")
+}
 const input = (e) => {
-  dispatch("SOUND", "click")
-  dispatch("SET_NAME", { name: e.target.innerText })
+  console.log({e})
+  if (e.inputType === "insertParagraph") {
+    dispatch("SOUND", "yes")
+    e.target.blur()
+    // ^ this will end typing & trigger a save
+  } else {
+    dispatch("SOUND", "click")
+    dispatch("SET_NAME", { name: e.target.innerText })
+  }
 }
 
 export default (state) => (
   html`
   <h1 contenteditable="true"
       @input=${input}
+      @blur=${save}
   >
   ${state.name}
   </h1>
--- dispatch.js
+++ dispatch.js
@@ -254,8 +254,9 @@ const ACTIONS = {
     dispatch("RENDER");
   },
   SET_NAME({ name }, state) {
-    state.name = name
-    console.log(state.name)
+    state.name = name.trim().replace(/\s+/g,'-')
+
+    return state.name
   },
   CHANGE_ASSET_NAME({ index, newName }, state) {
     const usedNames = state.assets.map(x => x.name);
