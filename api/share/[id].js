949f238ad4677e4150d4fdfa1942e5d311846971
Caleb Denio <cjdenio44@gmail.com>
2022-02-08 17:07:43 -0500

    oops

--- api/share/[id].js
+++ api/share/[id].js
@@ -2,13 +2,15 @@ import ejs from "ejs";
 
 export default function handler(req, res) {
   res.send(
-    ejs.render(`<!DOCTYPE html>
+    ejs.render(
+      `<!DOCTYPE html>
   
   <html>
     <head>
       <meta http-equiv="refresh" content="0; url=<%= url %>" />
     </head>
-  </html>`),
-    { url: `https://gamelab.hackclub.com/?id=${req.query.id}` }
+  </html>`,
+      { url: `https://gamelab.hackclub.com/?id=${req.query.id}` }
+    )
   );
 }
