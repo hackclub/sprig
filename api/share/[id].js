b6bb019cde7352e3f4f8302d06427cefff25e922
Caleb Denio <cjdenio44@gmail.com>
2022-02-08 17:03:58 -0500

    test

---
+++ api/share/[id].js
@@ -0,0 +1,14 @@
+import ejs from "ejs";
+
+export default function handler(req, res) {
+  res.send(
+    ejs.render(`<!DOCTYPE html>
+  
+  <html>
+    <head>
+      <meta http-equiv="refresh" content="0; url=<%= url %>" />
+    </head>
+  </html>`),
+    { url: `https://gamelab.hackclub.com/?id=${req.query.id}` }
+  );
+}
