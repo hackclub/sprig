81c84a8853715086d613c5b8bec78f528743cf66
Caleb Denio <cjdenio44@gmail.com>
2022-02-08 17:29:59 -0500

    more stuff

--- api/share.js
+++ api/share.js
@@ -1,16 +1,44 @@
 import ejs from "ejs";
+import axios from "axios";
 
-export default function handler(req, res) {
-  res.send(
-    ejs.render(
-      `<!DOCTYPE html>
+const template = `
+<!DOCTYPE html>
   
-  <html>
-    <head>
-      <meta http-equiv="refresh" content="0; url=<%= url %>" />
-    </head>
-  </html>`,
-      { url: `https://gamelab.hackclub.com/?id=${req.query.id}` }
-    )
-  );
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=<%= game.url %>" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="<%= game.name %> on Game Lab" />
+    <meta property="og:url" content="<%= url %>" />
+
+    <title><%= game.name %> on Game Lab</title>
+  </head>
+
+  <body>
+    <a href="<%= game.url %>">Redirecting to Game Lab...</a>
+  </body>
+</html>
+`;
+
+export default async function handler(req, res) {
+  const { id } = req.query;
+
+  try {
+    const { data: game } = await axios(
+      `https://project-bucket-hackclub.s3.eu-west-1.amazonaws.com/${id}.json`
+    );
+
+    res.send(
+      ejs.render(template, {
+        game: {
+          url: `https://gamelab.hackclub.com/?id=${id}`,
+          name: game.name,
+        },
+        url: `https://gamelab.hackclub.com/share/${id}`,
+      })
+    );
+  } catch (e) {
+    res.redirect("/");
+  }
 }
