102a808ce7d0a00aabe55b62ede36103bf410354
Caleb Denio <cjdenio44@gmail.com>
2022-02-08 19:02:22 -0500

    actually make it work (sorta)

--- api/preview.js
+++ api/preview.js
@@ -3,6 +3,13 @@ import chrome from "chrome-aws-lambda";
 
 /** @type {import("@vercel/node").VercelApiHandler} */
 export default async function handler(req, res) {
+  const { id } = req.query;
+
+  if (!id) {
+    res.send("What kinda wacky shenanigans ya tryin' to pull here????");
+    return;
+  }
+
   const browser = await core.launch({
     args: chrome.args,
     executablePath: await chrome.executablePath,
@@ -10,8 +17,8 @@ export default async function handler(req, res) {
   });
 
   const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
-  await page.goto("https://google.com");
+  await page.setViewport({ width: 1200, height: 600 });
+  await page.goto(`https://hackclub.gamelab.com/?id=${id}`);
   const file = await page.screenshot();
 
   res.setHeader("Content-Type", "image/png");
