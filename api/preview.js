12adc8deec93095b2b4a92938cd9b04e1385f1d9
Caleb Denio <cjdenio44@gmail.com>
2022-02-08 19:15:04 -0500

    fun stuff

--- api/preview.js
+++ api/preview.js
@@ -1,6 +1,12 @@
 import core from "puppeteer-core";
 import chrome from "chrome-aws-lambda";
 
+function wait(ms) {
+  return new Promise((r) => {
+    setTimeout(r, ms);
+  });
+}
+
 /** @type {import("@vercel/node").VercelApiHandler} */
 export default async function handler(req, res) {
   const { id } = req.query;
@@ -19,7 +25,8 @@ export default async function handler(req, res) {
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 600 });
   await page.goto(`https://gamelab.hackclub.com/?id=${id}`);
-  await page.click(".run-button");
+  await wait(100);
+  //   await page.click(".run-button");
   const file = await page.screenshot();
 
   res.setHeader("Content-Type", "image/png");
