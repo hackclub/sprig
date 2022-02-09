e5d757b2e526a7f5a4face432723fdd24975ae92
maggie-j-liu <maggie.j.liu@gmail.com>
2022-02-08 16:27:25 -0800

    fix hitbox

--- Engine.js
+++ Engine.js
@@ -226,13 +226,14 @@ class Object {
       ctx.fillRect(-2, -2, 4, 4);
     }
 
-    if (this._update !== null) this._update(obj);
     ctx.restore();
 
     if (Engine.show.hitbox) {
       ctx.strokeStyle = "grey";
       ctx.strokeRect(this.x - ox, this.y - oy, w, h);
     }
+
+    if (this._update !== null) this._update(obj);
   }
 
   set x(val) {
