5ec2209570457eba9b8be36752aa09f6cdb5185a
leomcelroy <leomcelroy@gmail.com>
2022-02-16 16:19:29 -0500

    Update favicon.js

--- utils/favicon.js
+++ utils/favicon.js
@@ -3,7 +3,7 @@
 // `favicon('yellow.png')` to set to a yellow variant of the favicon
 // `favicon('loading.gif)` to set to a loading icon
 export default function favicon(type = 'white.png') {
-  console.log('setting favicon', type)
+  // console.log('setting favicon', type)
   const icon = document.querySelector("link[rel~='icon']");
   if (!icon) {
       icon = document.createElement('link');
