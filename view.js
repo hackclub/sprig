49c28c449c48b75f19720f26241c1732898cadbb
leomcelroy <leomcelroy@gmail.com>
2022-03-04 14:43:20 -0500

    Update view.js

--- view.js
+++ view.js
@@ -368,7 +368,7 @@ export function view(state) {
                     style="width:16px;height:16px;filter:invert(1);margin-right:5px;"
                   />`,
                   sprite: html`<img
-                    src="assets/favicon/white.png"
+                    src="assets/favicon/white-alt-2.png"
                     style="width:11px;height:11px;padding:3px;margin-right:5px;"
                   />`,
                 }[x.type]}
