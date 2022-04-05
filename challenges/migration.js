2413be74d2702cb13d9222b48c9e11072f11dccc
Cedric Hutchings <ced@misguided.enterprises>
2022-04-05 17:49:16 -0400

    Add leading zeroes to challenge files to ensure order is kept even when sorted lexographically

--- challenges/migration.js
+++ challenges/migration.js
@@ -4,10 +4,13 @@ for await (const { name: path, isFile } of Deno.readDir("./"))
     let [, index, name] = path.match(/([0-9]+)-(.+)\.json$/) ?? [];
     if (name === undefined) return;
     index = +index;
-    name = name.replace(/-/g, " ");
 
     const content = JSON.parse(await Deno.readTextFile(path));
     content.version = "0.2.3";
-    content.name = name;
-    await Deno.writeTextFile(path, JSON.stringify(content, null, 2));
+    content.name = name.replace(/-/g, " ");
+    await Deno.remove(path);
+    await Deno.writeTextFile(
+      `${(""+index).padStart(2, 0)}-${name}.json`,
+      JSON.stringify(content, null, 2)
+    );
   })();
