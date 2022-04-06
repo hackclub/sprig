4118c981f967e7f5fbc868184accb26c2e6ceed0
Cedric Hutchings <ced@misguided.enterprises>
2022-04-06 00:02:43 -0400

    Add zIndex challenge

--- challenges/quest.js
+++ challenges/quest.js
@@ -23,38 +23,43 @@ export default [
     "id": "2227fd06204c96c0a0692fa7b8769ec8"
   },
   {
-    "name": "add bounce sound",
+    "name": "all in order",
     "index": 4,
+    "id": "6633a9d9f751e3cacd5799be883ba1ac"
+  },
+  {
+    "name": "add bounce sound",
+    "index": 5,
     "id": "6db84f68a296a536aa62a902d80fd6be"
   },
   {
     "name": "off the wall",
-    "index": 5,
+    "index": 6,
     "id": "d6c5d0ab4d7c8e450b69afce5d84be82"
   },
   {
     "name": "debarchery",
-    "index": 6,
+    "index": 7,
     "id": "82f418a0109533313c3344fb8007135f"
   },
   {
     "name": "hoppin rockets",
-    "index": 7,
+    "index": 8,
     "id": "06008275810963a143ead4a879b90bc9"
   },
   {
     "name": "animation_station",
-    "index": 8,
+    "index": 9,
     "id": "f060e1a85204e6a5c263b57a9590bc87"
   },
   {
     "name": "one jump",
-    "index": 9,
+    "index": 10,
     "id": "8ac4dd8e5791020bd11f673dd5c5727c"
   },
   {
     "name": "mile high club",
-    "index": 10,
+    "index": 11,
     "id": "6fd0b78fbe9b470c005690afee047a43"
   }
 ];
