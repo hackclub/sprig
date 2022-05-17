f6ef3b31b9474833677aa3c0e03d48686a8560fc
Kognise <felix.mattick@gmail.com>
2022-05-17 14:50:21 -0400

    Better styles :>

--- view.js
+++ view.js
@@ -7,17 +7,17 @@ export const view = (state) => html`
     <div class="code-container">
       <codemirror-js class="code-editor"></codemirror-js>
     </div>
+    <div class="vertical-bar" aria-hidden="true"></div>
     <div class="game-output">
       <canvas class="game-canvas"></canvas>
     </div>
   </div>
-  <div class="vertical-bar"></div>
 `
 
 const menu = () => html`
   <div class="menu">
     <div class="menu-item">name</div>
-    <div class="menu-item">GitHub</div>
+    <div class="menu-item">github</div>
     <div class="menu-item dropdown-container">
       export
       <div class="dropdown-list">
