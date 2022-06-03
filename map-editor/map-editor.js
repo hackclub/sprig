ef51a4320ce379d1dac98dd0c4cf14286117166d
Kognise <felix.mattick@gmail.com>
2022-06-03 08:17:18 -0400

    Render map in editor now

--- map-editor/map-editor.js
+++ map-editor/map-editor.js
@@ -1,8 +1,14 @@
 import { render, html } from "/libs/uhtml.js";
 import { spriteTextToImageData } from "../engine/sprite.js";
 
+const SPRITE_SIZE = 16;
+
 export function createMapEditor(target) {
   const state = {
+    canvas: null,
+    width: 10,
+    height: 6,
+    cells: [[]],
     legend: {}
   }
 
@@ -10,7 +16,7 @@ export function createMapEditor(target) {
     <link rel="stylesheet" href="./map-editor/map-styles.css">
     <div class="map-editor-container">
       <div class="canvas-container">
-        hiiiii
+        <canvas></canvas>
       </div>
       <div class="sprites">
         ${Object.entries(state.legend).map(([name, sprite]) => html`
@@ -19,21 +25,46 @@ export function createMapEditor(target) {
       </div>
     </div>
   `;
+  const r = () => { render(target, view(state)); };
 
-  const r = () => {
-    render(target, view(state));
+  const resizeCanvas = () => {
+    state.canvas.width = state.width * SPRITE_SIZE;
+    state.canvas.height = state.height * SPRITE_SIZE;
   };
 
-  const init = (state) => {
+  const draw = () => {
+    const ctx = state.canvas.getContext("2d");
+    for (let y = 0; y < state.height; y++) {
+      for (let x = 0; x < state.width; x++) {
+        if (!state.cells[y]) continue;
+        const sprite = state.legend[state.cells[y][x]];
+        if (!sprite) continue;
+        ctx.putImageData(sprite.imageData, x * SPRITE_SIZE, y * SPRITE_SIZE, 0, 0, SPRITE_SIZE, SPRITE_SIZE);
+      }
+    }
+  };
+
+  const init = () => {
     r();
+    state.canvas = target.querySelector(".canvas-container > canvas");
+    resizeCanvas();
+    draw();
   };
-
-  init(state);
+  init();
 
   return {
     loadInitValue({ text, legend }) {
-      console.log(legend);
-      state.legend = legend;
+      state.legend = Object.fromEntries(
+        Object.entries(legend)
+          .map(([ key, sprite ]) => [ key, { ...sprite, imageData: spriteTextToImageData(sprite.text) } ])
+      );
+
+      state.cells = text.trim().split("\n").map(x => [...x.trim()]);
+      state.width = state.cells[0].length;
+      state.height = state.cells.length;
+      resizeCanvas();
+      draw();
+
       r();
     },
     end() {
