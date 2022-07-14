fc979cf6634485a918bd762ba94e619ce0c4e74e
leomcelroy <leomcelroy@gmail.com>
2022-07-14 16:06:22 -0400

    cleaning up repo

---
+++ games/9_puzzle.js
@@ -0,0 +1,148 @@
+/*
+@title: 9_puzzle
+@author: brian
+*/
+
+const player = "p";
+const target = "t";
+const red = "r";
+const blue = "b";
+
+setLegend(
+  [ player, bitmap`
+................
+................
+................
+................
+................
+................
+.......00.......
+......0000......
+......0000......
+.......00.......
+................
+................
+................
+................
+................
+................`],
+  [ target, bitmap`
+................
+................
+................
+................
+................
+......8888......
+.....888888.....
+.....888888.....
+.....888888.....
+.....888888.....
+......8888......
+................
+................
+................
+................
+................`],
+  [ red, bitmap`
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333`],
+  [ blue, bitmap`
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555`]
+)
+
+let level = 0;
+
+const levels = [
+  map`
+bbb
+bbr
+bb.`,
+  map`
+bbb
+bbr
+b.b`,
+  map`
+rbb
+bbb
+bb.`,
+]
+
+onInput("k", _ => {
+  if(isHole(ptile.x,ptile.y+1)) moveTileTo(ptile.x,ptile.y+1);
+  else if(isHole(ptile.x,ptile.y-1)) moveTileTo(ptile.x,ptile.y-1);
+  else if(isHole(ptile.x-1,ptile.y)) moveTileTo(ptile.x-1,ptile.y);
+  else if(isHole(ptile.x+1,ptile.y)) moveTileTo(ptile.x+1,ptile.y);
+})
+
+afterInput(_ => {
+  console.log(tilesWith(red,target));
+})
+
+const moveTileTo = (x,y) => {
+  const ut = rbTile(ptile.x,ptile.y);
+  if(ut){
+    ut.x = x;
+    ut.y = y;
+    if(tilesWith(red,target).length!=0) nextLevel();
+  }
+}
+
+const nextLevel = () => {
+  if(levels[level+1]) level++;
+  setLevel(level);
+}
+
+const setLevel = (n) => {
+  setMap(levels[n]);
+  addSprite(1,1,player);
+  addSprite(2,2,target);
+  ptile = getFirst(player);
+}
+
+const isHole = (x,y) => {
+  return (x>=0)&&(y>=0)&&(x<3)&&(y<3)&&rbTile(x,y)==undefined;
+}
+
+const rbTile = (x,y) => {
+  for(const t of getTile(x, y)){
+    if(t.type==red) return t; 
+    else if(t.type==blue) return t; 
+  }
+  return undefined;
+}
+
+onInput("up", _ => {ptile.y -= 1;})
+onInput("down", _ => {ptile.y += 1;})
+onInput("left", _ => {ptile.x -= 1;})
+onInput("right", _ => {ptile.x += 1;})
+
+setLevel(0)
