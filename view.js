5aff9628668441dc9bcf3e2e4cb0b157e5eb527b
Cedric Hutchings <ced@misguided.enterprises>
2022-07-18 14:20:43 -0400

    add heave

--- view.js
+++ view.js
@@ -245,7 +245,92 @@ cccccbcc.
 setSolids( [bean, trellis] )
     `
   },
-  { name: 'heave' },
+  {
+    name: 'heave',
+    content: `
+const bean = 'a';
+const vine = 'b';
+const trellis = 'c';
+
+setLegend(
+  [bean, bitmap\`
+................
+......000.......
+.....0.220......
+.....002020.....
+..0..022..0.....
+...0..0...0.....
+...0000...20....
+....2.0...20....
+.....0....200...
+....0...220.0...
+....0...220.0...
+....02.220...2..
+.....0000.......
+....0....0......
+....0....0......
+...00...00......\`],
+  [vine, bitmap\`
+........44......
+.......44.......
+......44.......4
+......4........4
+.....44........4
+.44..4........44
+.44.44........4.
+..444........44.
+..44......44.4..
+.44.......4444..
+44.........444..
+44..........42..
+4...........4...
+4..........44...
+4.........44....
+4........44.....\`],
+  [trellis, bitmap\`
+.L...L.....L..L.
+.L...L.....L..L.
+..L.L.L...LL.L..
+..L.L.L...L.LL..
+..L.L.L..L..LL..
+..LL...L.L..LL..
+...L...L.L..L...
+...L...LL...L...
+..LL....L...LL..
+..LL...LL...LL..
+..L.L..LL..L.L..
+.L..L..L.L.L.L..
+.L..L.L..L.L.L..
+.L..L.L..L.L..L.
+L....L....L...L.
+L....L....L...L.\`]
+);
+
+setMap(map\`
+bbbbbbbbb
+bbbb...bb
+bbb.....b
+bb......b
+b...c.a.b
+b......bb
+b.....bbb
+bb.bbbbbb
+bbbbbbbbb\`);
+
+const player = () => getFirst(bean);
+onInput( "left", () => player().x -= 1);
+onInput("right", () => player().x += 1);
+onInput( "down", () => player().y += 1);
+onInput(   "up", () => player().y -= 1);
+
+/* uhhh ... what  things should be solid?!? */
+
+/* hehe see if you can figure out what this does >:) */
+setPushables({
+  [bean]: [trellis]
+})
+    `
+  },
   { name: 'trail' },
   { name: 'grow' },
 ]
