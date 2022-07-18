26b26de283d0e3cc9c879b3acf22040720b7f458
Cedric Hutchings <ced@misguided.enterprises>
2022-07-18 14:06:02 -0400

    challenges first draft

--- view.js
+++ view.js
@@ -89,12 +89,197 @@ const drawFile = (file, i, state) => {
   `
 }  
 
+const challenges = [
+  {
+    name: 'bean',
+    content: `
+/* HERE we give each kind of sprite
+ * in our game a letter, as a shorthand. */
+const bean = 'a';
+
+/* HERE we give each sprite its art! */
+setLegend(
+  /* click "bitmap" to change the art! */
+  [bean, bitmap\`
+................
+......000.......
+.....0.220......
+.....002020.....
+..0..022..0.....
+...0..0...0.....
+...0000...20....
+....2.0...20....
+.....0....200...
+....0...220.0...
+....0...220.0...
+....02.220...2..
+.....0000.......
+....0....0......
+....0....0......
+...00...00......\`]
+);
+
+/* HERE we make a map out of those letters.
+ * try making the map bigger!
+ * (hint: CLICK "map") */
+setMap(map\`a\`);
+`
+  },
+  {
+    name: 'walk',
+    content: `
+const bean = 'a';
+
+setLegend(
+  [bean, bitmap\`
+................
+......000.......
+.....0.220......
+.....002020.....
+..0..022..0.....
+...0..0...0.....
+...0000...20....
+....2.0...20....
+.....0....200...
+....0...220.0...
+....0...220.0...
+....02.220...2..
+.....0000.......
+....0....0......
+....0....0......
+...00...00......\`]
+);
+
+setMap(map\`
+.....
+.....
+..a..
+.....
+.....\`);
+
+const player = () => getFirst(bean);
+
+/* CAN YOU ADD MORE DIRECTIONS? */
+onInput("left", () => player().x -= 1);
+onInput("down", () => player().y += 1);
+    `
+  },
+  {
+    name: 'trellis',
+    content: `
+const bean = 'a';
+const vine = 'b';
+const trellis = 'c';
+
+setLegend(
+  [bean, bitmap\`
+................
+......000.......
+.....0.220......
+.....002020.....
+..0..022..0.....
+...0..0...0.....
+...0000...20....
+....2.0...20....
+.....0....200...
+....0...220.0...
+....0...220.0...
+....02.220...2..
+.....0000.......
+....0....0......
+....0....0......
+...00...00......\`],
+  [vine, bitmap\`
+........44......
+.......44.......
+......44.......4
+......4........4
+.....44........4
+.44..4........44
+.44.44........4.
+..444........44.
+..44......44.4..
+.44.......4444..
+44.........444..
+44..........42..
+4...........4...
+4..........44...
+4.........44....
+4........44.....\`],
+  [trellis, bitmap\`
+.L...L.....L..L.
+.L...L.....L..L.
+..L.L.L...LL.L..
+..L.L.L...L.LL..
+..L.L.L..L..LL..
+..LL...L.L..LL..
+...L...L.L..L...
+...L...LL...L...
+..LL....L...LL..
+..LL...LL...LL..
+..L.L..LL..L.L..
+.L..L..L.L.L.L..
+.L..L.L..L.L.L..
+.L..L.L..L.L..L.
+L....L....L...L.
+L....L....L...L.\`]
+);
+
+setMap(map\`
+..cccccc.
+.cccbbccc
+cccbbbbcc
+ccbbbbbcc
+cbbbcbbbc
+ccbccbabc
+cccccbbcc
+cccccbcc.
+..cccccc.\`);
+
+/* PASTE SOME MOVEMENT CODE HERE ;)
+ * hint: go to your shed to get the code from the last stage! */
+
+/* ps - what happens if you make non-bean players? :O */
+
+/* what happens if you make vines solid? :3 */
+setSolids( [bean, trellis] )
+    `
+  },
+  { name: 'heave' },
+  { name: 'trail' },
+  { name: 'grow' },
+]
+
+for (const challenge of challenges)
+  challenge.content = `/*
+@title: ${challenge.name}
+@author: ced
+*/
+
+` + challenge.content;
+
 const menu = (state) => html`
   <div class="menu">
     <div class="menu-item dropdown-container">
       shed
       <div class="dropdown-list">
         ${state.savedGames.map((file, i) => drawFile(file, i, state))}
+      </div>
+    </div>
+    <div class="menu-item dropdown-container">
+      grow
+      <div class="dropdown-list">
+        ${challenges.map(({ content, name }, i) => {
+          const load = () => {
+            const cur = state.codemirror.state.doc.toString();
+
+            dispatch("SET_EDITOR_TEXT", {
+              text: content.trim(),
+              range: [0, cur.length]
+            });
+          };
+          return html`<div @click=${load}> CH. ${i+1} - ${name} </div>`
+        })}
       </div>
     </div>
     <div class="menu-item dropdown-container">
