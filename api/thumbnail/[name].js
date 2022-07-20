6a92c3eeff5a20df2b59a4b8eecd833b13773dd8
leomcelroy <leomcelroy@gmail.com>
2022-07-20 15:37:04 -0400

    fixing serverless thumbnail rendering

--- api/thumbnail/[name].js
+++ api/thumbnail/[name].js
@@ -9,52 +9,46 @@ function drawGame(name) {
   const file = path.join(process.cwd(), '../../games', `${name}.js`);
   const src = readFileSync(file, 'utf8');
 
-  // have src
-
-  // get legend
   let legend = src.match(/setLegend\(([\s\S]*?)\)/)[1];
-
-  // const getVariable = name => legend.match(/name(.*?)['"](.*?)['"]/)[2];
 
   const reconstructedLegend = {};
 
-  // for (let i = 0; i < legend.length; i++) {
-    
-  // }
-
   let lastKey = "";
   legend.split(",").forEach(x => {
-    console.log(x);
     if (x.includes("[")) {
-      lastKey = x.replace("[", "").trim();
+      const tempKey = x.replace("[", "").trim();
+      const re = new RegExp(`${tempKey}.*?=.*?["'](.*?)["']`);
+      lastKey = src.match(re)[1];
     } else {
       const value = x.match(/bitmap`([\s\S]*?)`/);
-      console.log(value);
-      if (value) reconstructedLegend[lastKey] = value;
+
+      if (value) reconstructedLegend[lastKey] = value[1];
     }
   })
 
-  console.log(reconstructedLegend);
-
-  // replace every [ thing ,
-  // with const | let thing = "newThing" | 'newThing'
-
-  // console.log(legend);
   // get first map
   const firstMap = src.match(/[^t]map`([\s\S]*?)`/)[1];
-  // draw first map
-  // console.log(firstMap);
 
+  const mapWidth = firstMap.trim().split("\n")[0].trim().length;
+  const mapHeight = firstMap.trim().split("\n").length;
   const image = {
-    data: new Uint8Array(120*168),
-    width: 128
+    data: new Uint8Array(mapWidth*mapHeight*16*16*4),
+    width: mapWidth*16
   };
+
+  firstMap.trim().split("\n").forEach( (row, y) => {
+    row.trim().split("").forEach( (sprite, x) => {
+      if (sprite === ".") return;
+      const bitmap = makeSpriteBitmap(reconstructedLegend[sprite]);
+      blitSprite(image, bitmap, x, y);
+    })
+  })
+
 
   return { name, image, url };
 }
 
 const test = drawGame("tolls");
-console.log(test);
 
 export default async function handler(request, response) {
   const { name } = request.query;
@@ -62,9 +56,21 @@ export default async function handler(request, response) {
   return response.status(200).json(data);
 }
 
+function makeSpriteBitmap(grid) {
+  const result = [];
+  const colors = Object.fromEntries(palette);
+  grid.trim().split("\n").forEach( (row, y) => {
+    row.trim().split("").forEach( (color, x) => {
+      const arr = colors[color];
+      result.push(...arr);
+    })
+  })
 
-function blitSprite(screen, sprite, tx, ty) {
-  const [_, { imageData: { data: bitmap } }] = sprite;
+  return result;
+}
+
+
+function blitSprite(screen, bitmap, tx, ty) {
   for (let x = 0; x < 16; x++)
     for (let y = 0; y < 16; y++) {
       const sx = tx*16 + x;
@@ -77,20 +83,4 @@ function blitSprite(screen, sprite, tx, ty) {
       screen.data[(sy*screen.width + sx)*4 + 2] = bitmap[(y*16 + x)*4 + 2];
       screen.data[(sy*screen.width + sx)*4 + 3] = bitmap[(y*16 + x)*4 + 3];
     }
-}
-
-function drawTiles(state, api, screen, bitmaps) {
-  const { dimensions, legend } = state;
-  const { width, height, maxTileDim } = dimensions;
-
-  const grid = api.getGrid();
-
-  for (const cell of grid) {
-    const zOrder = legend.map(x => x[0]);
-    cell.sort((a, b) => zOrder.indexOf(a.type) - zOrder.indexOf(b.type));
-
-    for (const { x, y, type } of cell) {
-      blitSprite(screen, bitmaps.find(x => x[0] == type), x, y);
-    }
-  }
 }
