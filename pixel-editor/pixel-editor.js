4a921ca3dd96122549a72da4b34f93521fef432c
Mutammim <58490040+mutammim@users.noreply.github.com>
2022-07-21 00:14:51 -0400

    Fix typo

--- pixel-editor/pixel-editor.js
+++ pixel-editor/pixel-editor.js
@@ -239,11 +239,11 @@ export function createPixelEditor(target) {
           @click=${() => { state.color = color[1]; r(); }}>
         </div>
       `
+    }
 
     return html`
       <div class="colors">${state.palette.map(drawColor)}</div>
     `
-    }
   };
 
   const r = () => {
