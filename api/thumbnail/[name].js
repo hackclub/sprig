f74d6f6363fbc581dfed7cc1e9889fc9fdd19663
leomcelroy <leomcelroy@gmail.com>
2022-07-21 17:24:47 -0400

    Update [name].js

--- api/thumbnail/[name].js
+++ api/thumbnail/[name].js
@@ -1,59 +1,56 @@
 import { palette } from "../../palette.js";
+import { baseEngine } from "../../engine/baseEngine.js";
 import { readFileSync } from 'fs';
 import path from 'path';
 import fetch from "node-fetch";
 
+function evalGameScript(script) {
+  const { api, state } = baseEngine();
+
+  let legend = null;
+  let map = null;
+
+  const patchedApi = {
+    ...api,
+    setLegend: (...bitmaps) => { legend = bitmaps; },
+    setMap: (string) => { map = string },
+    onInput: () => {}, 
+    afterInput: () => {}, 
+    setScreenSize: () => {},
+    getState: () => state,
+    playTune: () => {},
+    setTimeout: () => {},
+    setInterval: () => {},
+  }
+
+  try {
+    const fn = new Function(...Object.keys(patchedApi), script);
+    fn(...Object.values(patchedApi));
+  } catch (err) { }
+
+  return {
+    legend: Object.fromEntries(legend),
+    map
+  }
+}
+
 async function drawGame(name) {
 
   const url = `https://raw.githubusercontent.com/hackclub/sprig/main/games/${name}.js`;
-
-  // const file = path.join(process.cwd(), '../../games', `${name}.js`);
-  // console.log(file);
-  // const src = readFileSync(file, 'utf8');
   const src = await fetch(url).then( res => res.text() );
+  const { legend, map } = evalGameScript(src);
 
-  let legend = src.match(/setLegend\(([\s\S]*?)\)/)[1];
-
-  const reconstructedLegend = {};
-
-  let lastKey = "";
-  legend.split(",").forEach(x => {
-    if (x.includes("[")) {
-      // check if is variable or string
-      if (!x.match(/["']/)) {
-        const tempKey = x.replace("[", "").trim();
-        const re = new RegExp(`${tempKey}.*?=.*?["'](.*?)["']`);
-        lastKey = src.match(re)[1];
-      } else {
-        lastKey = x
-          .split("")
-          .filter(ch => !(/['"\s\[]/).test(ch))
-          .join("");
-      }
-
-    } else {
-      const value = x.match(/bitmap`([\s\S]*?)`/);
-
-      if (value) reconstructedLegend[lastKey] = value[1];
-    }
-  })
-
-  // get first map
-  const firstMap = src.match(/[^t]map`([\s\S]*?)`/)[1];
-
-  const mapWidth = firstMap.trim().split("\n")[0].trim().length;
-  const mapHeight = firstMap.trim().split("\n").length;
+  const mapWidth = map.trim().split("\n")[0].trim().length;
+  const mapHeight = map.trim().split("\n").length;
   const image = {
     data: new Uint8Array(mapWidth*mapHeight*16*16*4),
     width: mapWidth*16
   };
 
-  console.log(reconstructedLegend, firstMap);
-
-  firstMap.trim().split("\n").forEach( (row, y) => {
+  map.trim().split("\n").forEach( (row, y) => {
     row.trim().split("").forEach( (sprite, x) => {
       if (sprite === ".") return;
-      const bitmap = makeSpriteBitmap(reconstructedLegend[sprite]);
+      const bitmap = makeSpriteBitmap(legend[sprite]);
       blitSprite(image, bitmap, x, y);
     })
   })
@@ -110,3 +107,34 @@ function blitSprite(screen, bitmap, tx, ty) {
       screen.data[(sy*screen.width + sx)*4 + 3] = bitmap[(y*16 + x)*4 + 3];
     }
 }
+
+// const file = path.join(process.cwd(), '../../games', `${name}.js`);
+// console.log(file);
+// const src = readFileSync(file, 'utf8');
+
+
+//  let legend = src.match(/setLegend\(([\s\S]*?)\)/)[1];
+
+//  const reconstructedLegend = {};
+
+//  let lastKey = "";
+//  legend.split(",").forEach(x => {
+//    if (x.includes("[")) {
+//      // check if is variable or string
+//      if (!x.match(/["']/)) {
+//        const tempKey = x.replace("[", "").trim();
+//        const re = new RegExp(`${tempKey}.*?=.*?["'](.*?)["']`);
+//        lastKey = src.match(re)[1];
+//      } else {
+//        lastKey = x
+//          .split("")
+//          .filter(ch => !(/['"\s\[]/).test(ch))
+//          .join("");
+//      }
+
+//    } else {
+//      const value = x.match(/bitmap`([\s\S]*?)`/);
+
+//      if (value) reconstructedLegend[lastKey] = value[1];
+//    }
+//  })
