8d8e68e13ab1446b4291e063544ab8718ec8644e
Reese Armstrong <me@reeseric.ci>
2022-08-22 18:25:50 -0500

    Add files via upload

---
+++ games/working_getting_started.js
@@ -0,0 +1,169 @@
+/*
+@title: working_getting_started
+@author: reesericci
+
+A completed version of getting_started by @leo !
+
+*/
+
+const player = "p";
+const box = "b";
+const goal = "g";
+const wall = "w";
+
+setLegend(
+  [ player, bitmap`
+................
+................
+................
+.......0........
+.....00.000.....
+....0.....00....
+....0.0.0..0....
+....0......0....
+....0......0....
+....00....0.....
+......00000.....
+......0...0.....
+....000...000...
+................
+................
+................`],
+  [ box, bitmap`
+................
+................
+................
+...88888888888..
+...8....8....8..
+...8....8....8..
+...8....8....8..
+...8....8....8..
+...88888888888..
+...8....8....8..
+...8....8....8..
+...8....8....8..
+...8....8....8..
+...88888888888..
+................
+................`],
+  [ goal, bitmap`
+................
+................
+................
+....444444......
+...44....44.....
+...4......4.....
+...4.......4....
+...4.......4....
+...4.......4....
+...44......4....
+....4......4....
+....44....44....
+.....444444.....
+................
+................
+................`],
+  [ wall, bitmap`
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000`]
+);
+
+// remember levels start at 0 - so the "5th level" is actually level #4
+let level = 0;
+const levels = [
+  map`
+p.bg`,
+  map`
+p..
+.b.
+..g`,
+  map`
+p..g
+.b..
+....
+....`,
+  map`
+p...
+...b
+...b
+.bbg`,
+  map`
+p.w.
+.bwg
+....
+..bg`
+];
+
+const currentLevel = levels[level];
+setMap(currentLevel);
+
+setSolids([ player, box, wall ]);
+
+setPushables({
+  [player]: [box, player],
+  [box]: [box, box]
+});
+
+// START - PLAYER MOVEMENT CONTROLS
+
+onInput("w", () => {
+  getFirst(player).y -= 1;
+});
+
+onInput("a", () => {
+  getFirst(player).x -= 1;
+});
+
+onInput("s", () => {
+  getFirst(player).y += 1;
+});
+
+onInput("d", () => {
+  getFirst(player).x += 1;
+});
+
+// END - PLAYER MOVEMENT CONTROLS
+
+onInput("j", () => {
+  const currentLevel = levels[level];
+  if (currentLevel !== undefined) {
+    clearText("");
+    setMap(currentLevel);
+  }
+});
+
+afterInput(() => {
+  // count the number of tiles with goals
+  const targetNumber = tilesWith(goal).length;
+  
+  // count the number of tiles with goals and boxes
+  const numberCovered = tilesWith(goal, box).length;
+
+  if (numberCovered === targetNumber) {
+    // increase the current level number
+    level = level + 1;
+
+    const currentLevel = levels[level];
+
+    // make sure the level exists and if so set the map
+    if (currentLevel !== undefined) {
+      setMap(currentLevel);
+    } else {
+      addText("you win!", { y: 10, color: [4,163,86] });
+    }
+  }
+});
