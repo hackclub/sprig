7e2b44a9dc62b637ff37ad6c36c0fe9d6ba5e3b1
Peipr <proca001@jvvgirona.eu>
2022-08-28 02:23:17 +0200

    More levels and a new mechanic
    Which is broken because some weird thing about n.replace?

--- games/SprazeJS.js
+++ games/SprazeJS.js
@@ -8,6 +8,8 @@ const movable = "b";
 const goal = "g";
 const bg = "q";
 const wall = "w";
+const wallb = "y";
+const invis = "i";
 /*---------------------------------------------------------------------------*/
 setLegend(
   [ player, bitmap`
@@ -40,6 +42,23 @@ setLegend(
 1111LLLLLLLL1111
 1111LLLLLLLL1111
 1111LLLLLLLL1111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111`],
+  [ wallb, bitmap`
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+11111LLLLLL11111
+1111LLLLLLLL1111
+1111LLLLLLLL1111
+1111LLLLLLLL1111
+1111LLLLLLLL1111
+1111LLLLLLLL1111
+1111LLLLLLLL1111
+11111LLLLLL11111
 1111111111111111
 1111111111111111
 1111111111111111
@@ -94,8 +113,41 @@ setLegend(
 7557557557557557
 5775775775775775
 7557557557557557
-5775775775775775`]);
-let level = 0;
+5775775775775775`],
+  [ invis, bitmap`
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111`]);
+let level = 10;
+const nextlvl = tune`
+250: e5~250,
+250,
+250: e5~250,
+250,
+250: e5~250,
+250,
+250: a5~250,
+6250`;
+const fail = tune`
+375,
+375: c5-375,
+375: c5-375,
+375,
+375: c4/375,
+10125`;
 const levels = [
   map`
 p.......w.
@@ -134,12 +186,47 @@ wwbw.w.w...
 pb..
 wwbw
 .b.w
-wg.w`];
+wg.w`,
+  map`
+.wg
+.y.
+.w.
+.w.
+.w.
+.w.
+pw.
+.w.`,
+  map`
+pb.y
+bw.w
+...w
+wwyg`,
+  map`
+.p..
+qwwy
+q..y
+bgbw`,
+  map`
+pywy
+yyyy
+wwwy
+gyyy`,
+  map`
+p..........
+wbwwwwwwyww
+...........
+gwwwwwwwwww`,
+  map`
+p...q....q
+..q.q.qq.q
+qqq.q..q..
+....qq.qq.
+.qqqq..q..
+......qq.g`];
 /*---------------------------------------------------------------------------*/
 const currentLevel = levels[level];
-setMap(currentLevel); setSolids([player, movable, wall]);
-setPushables({
-  [player]: [movable, player]});
+setMap(currentLevel); setSolids([player, movable, wall, invis]);
+setPushables({[player]: [movable, player]});
 /*---------------------------------------------------------------------------*/
 onInput("s", () => {
   getFirst(player).y += 1;});
@@ -149,21 +236,38 @@ onInput("w", () => {
   getFirst(player).y += -1;});
 onInput("a", () => {
   getFirst(player).x += -1;});
-/*---------------------------------------------------------------------------*/
-onInput("j", () => {
+/*onInput("s", () => {
+  getFirst(invis).y += 1;});
+onInput("d", () => {
+  getFirst(invis).x += 1;});
+onInput("w", () => {
+  getFirst(invis).y += -1;});
+onInput("a", () => {
+  getFirst(invis).x += -1;});*/
+onInput("l", () => {
   const currentLevel = levels[level];
-  if (currentLevel !== undefined) {
-    clearText("");
-    setMap(currentLevel);
-  }
-});
+  setMap(currentLevel);
+  playTune(fail)});
+/*---------------------------------------------------------------------------*/
+
 afterInput(() => {
   const target = tilesWith(goal).length;
+  const fail = tilesWith(bg, player).length;
+  if (fail === target) {
+    playTune(fail);
+    const currentLevel = levels[level]
+    setMap(currentLevel);}
+  const goalcheck2 = tilesWith(goal, invis).length;
   const goalcheck = tilesWith(goal, player).length;
+  if (goalcheck2 === target) {
+    level = level + 1;
+    playTune(nextlvl);
+    const currentLevel = levels[level];}
   if (goalcheck === target) {
     level = level + 1;
+    playTune(nextlvl);
     const currentLevel = levels[level];
-    if (currentLevel !== undefined) {
+    if (currentLevel !== "") {
       setMap(currentLevel);} 
     else {
       setBackground(bg)
