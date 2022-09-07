3d27dd87dad82e975dc485e15d7f97d08425b1ea
Anamika <72334601+noviicee@users.noreply.github.com>
2022-09-08 00:09:32 +0530

    Added purge game

---
+++ games/purge.js
@@ -0,0 +1,166 @@
+/*
+@title: purge
+@author: noviicee
+
+Instructions:
+
+Cover the purple with green.
+
+Use w, a, s, d to move around and j to restart the level.
+
+*/
+
+const player = "g";
+const box = "b";
+const goal = "p";
+const wall = "w";
+
+setLegend(
+  [ player, bitmap`
+................
+................
+................
+.......5........
+.......0........
+..00000000000...
+..0.........0...
+..0..0...0..0...
+..0.........0...
+..0...000...0...
+..0.........0...
+..00000000000...
+................
+................
+................
+................`],
+  [ box, bitmap`
+................
+................
+................
+...444444444....
+...4...4...4....
+...4...4...4....
+...444444444....
+...444444444....
+...444444444....
+...4...4...4....
+...4...4...4....
+...444444444....
+................
+................
+................
+................`],
+  [ goal, bitmap`
+................
+................
+................
+....888888......
+...88....88.....
+...8......8.....
+...8.......8....
+...8.......8....
+...8.......8....
+...88......8....
+....8......8....
+....88....88....
+.....888888.....
+................
+................
+................`],
+  [ wall, bitmap`
+LLLLLLLLLLLLLLLL
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+L11111111111111L
+LLLLLLLLLLLLLLLL`]
+);
+
+let level = 0;
+const levels = [
+  map`
+p.w.
+.bwg
+....
+....`,
+  map`
+....g
+.bbww
+w....
+g...p`,
+  map`
+p.w.
+.bwg
+....
+..bg`,
+  map`
+..w
+..w
+gbg
+.b.
+.w.
+bw.
+g.p`
+];
+
+const currentLevel = levels[level];
+setMap(currentLevel);
+
+onInput("w", () => {
+  getFirst(player).y -= 1;
+});
+
+onInput("s", () => {
+  getFirst(player).y += 1;
+});
+
+onInput("a", () => {
+  getFirst(player).x -= 1;
+});
+
+onInput("d", () => {
+  getFirst(player).x += 1;
+});
+
+onInput("j", () => {
+  const currentLevel = levels[level];
+  if (currentLevel !== undefined) {
+    clearText();
+    setMap(currentLevel);
+  }
+});
+
+setSolids([ player, box, wall ]);
+
+setPushables({
+  [ player ]: [ box ]
+});
+
+afterInput(() => {
+  const numberCovered = tilesWith(goal, box).length;
+  const targetNumber = tilesWith(goal).length;
+
+  if (numberCovered === targetNumber) {
+    // increase the current level number
+    level = level + 1;
+
+    const currentLevel = levels[level];
+
+    // make sure the level exists and if so set the map
+    if (currentLevel !== undefined) {
+      setMap(currentLevel);
+    } else {
+      addText("you win!", { y: 4, color: [255, 0, 0] });
+    }
+  }
+});
