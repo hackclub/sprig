77990b2935ac101da18af854871c7b9d2e80fa54
hamsterkoo <115516134+hamsterkoo@users.noreply.github.com>
2022-10-19 19:29:11 -0400

    Update BreadMaze.js
    Added;
    - 2 levels 
    - timer

--- games/BreadMaze.js
+++ games/BreadMaze.js
@@ -1,12 +1,13 @@
 /*
 @title: BreadMaze
-@author: your_name
+@author: Aiden
 */
 
 const player = "p";
 const box = "b";
 const bread = "r";
-const goal = "g"
+const goal = "g";
+const thing = "t";
 
 setLegend(
   [ player, bitmap`
@@ -76,10 +77,27 @@ setLegend(
 ................
 ................
 ................
-................`]
+................`],
+  [ thing,bitmap`
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000900000090000
+0000000000000000
+0000000000000000
+0000000000000000
+0000900000090000
+0000999999990000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000`]
 );
 
-setSolids([ bread, box, player ]);
+setSolids([ bread, box, player, thing ]);
 
 let level = 0;
 const levels = [
@@ -92,12 +110,32 @@ rrrrrrrr.
 .r.....r.
 .rrrrrrr.
 .........`,
+  map`
+........
+pb.....r
+rrrrrrtr
+........
+........
+........
+g.......`,
+  map`
+p........r
+......b.r.
+.......r..
+......r...
+.....r....
+....t...rg
+.b.r......
+..r...r...
+.r....r...
+r....rg...`
 ];
 
 setMap(levels[level]);
 
 setPushables({
-  [ player ]: [ box ],
+  [ player ]: [ box, thing ],
+  [ box ]: [ thing ]
 });
 
 onInput("w", () => {
@@ -115,6 +153,27 @@ onInput("s", () => {
 onInput("d", () => {
   getFirst(player).x += 1
 });
+onInput("j", () => {
+  const currentLevel = levels[level];
+  if (currentLevel !== undefined) {
+    clearText();
+    setMap(currentLevel);
+  }
+});
+
+//timer - code borrowerd from flurffy
+var tempototal = 10;
+var tempodescendo = setInterval(function(){
+tempototal--;
+clearText();
+addText(""+tempototal, { y: 1 , color: [255,0,0] });
+if(tempototal <= 0){
+      clearTile(getFirst(player).x,getFirst(player).y);
+      clearInterval(tempodescendo);
+      clearText()
+}
+},1000);
+
 afterInput(() => {
 // count the number of tiles with goals
   const targetNumber = tilesWith(goal).length;
@@ -131,8 +190,12 @@ afterInput(() => {
     // make sure the level exists and if so set the map
     if (currentLevel !== undefined) {
       setMap(currentLevel);
-    } else {
+    } 
+    else {
       addText("you win!", { y: 4, color: [255, 0, 0] });
     }
   }
+
+
+  
 });
