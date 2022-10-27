e79b7f2a66079c29d2a3e2f0e243f994610dc8a4
Cedric Hutchings <ced@misguided.enterprises>
2022-10-27 15:12:41 -0400

    new, better, simpler upload flow

--- upload.js
+++ upload.js
@@ -1,32 +1,7 @@
-import { Serial } from "./upload/serial.js";
-import { transfer } from "./upload/ymodem.js";
 import { dispatch } from "./dispatch.js";
 
-function delay(ms) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, ms);
-  });
-}
-
-async function getPort() {
-  if (navigator.serial === undefined)
-    throw "your browser does not support the Web Serial API. please try again in a recent version of Chrome.";
-  const ports = await navigator.serial.getPorts();
-  if (ports.length !== 1) {
-    dispatch("UPLOAD_LOG", "please pick a device.");
-    return await navigator.serial.requestPort({
-      filters: [
-        { usbVendorId: 0x2e8a, usbProductId: 10 },
-      ]
-    });
-  } else {
-    return ports[0];
-  }
-}
-
 const imports = `
+let setTimeout, setInterval, clearInterval, clearTimeout;
 const {
   /* sprite interactions */ setSolids, setPushables,
   /*              see also: sprite.x +=, sprite.y += */
@@ -49,7 +24,153 @@ const {
   /* how much sprite has moved since last onInput: sprite.dx, sprite.dy */
 
   playTune,
-} = require("engine");
+} = (() => {
+const exports = {};
+/* re-exports from C; bottom of module_native.c has notes about why these are in C */
+exports.setMap = map => native.setMap(map.trim());
+exports.addSprite = native.addSprite;
+exports.getGrid = native.getGrid;
+exports.getTile = native.getTile;
+exports.tilesWith = native.tilesWith;
+exports.clearTile = native.clearTile;
+exports.getFirst = native.getFirst;
+exports.getAll = native.getAll;
+exports.width = native.width;
+exports.height = native.height;
+exports.setBackground = native.setBackground;
+
+
+/* opts: x, y, color (all optional) */
+exports.addText = (str, opts={}) => {
+  // console.log("engine.js:addText");
+  const CHARS_MAX_X = 21;
+  const padLeft = Math.floor((CHARS_MAX_X - str.length)/2);
+
+  native.text_add(
+    str,
+    opts.color ?? [10, 10, 40],
+    opts.x ?? padLeft,
+    opts.y ?? 0
+  );
+}
+
+exports.clearText = () => native.text_clear();
+
+
+exports.setLegend = (...bitmaps) => {
+  // console.log("engine.js:setLegend");
+  native.legend_clear();
+  for (const [charStr, bitmap] of bitmaps) {
+    native.legend_doodle_set(charStr, bitmap.trim());
+  }
+  native.legend_prepare();
+};
+
+exports.setSolids = solids => {
+  // console.log("engine.js:setSolids");
+  native.solids_clear();
+  solids.forEach(native.solids_push);
+};
+
+exports.setPushables = pushTable => {
+  // console.log("engine.js:setPushables");
+  native.push_table_clear();
+  for (const [pusher, pushesList] of Object.entries(pushTable))
+    for (const pushes of pushesList)
+      native.push_table_set(pusher, pushes);
+};
+
+let afterInputs = [];
+// exports.afterInput = fn => (console.log('engine.js:afterInputs'), afterInputs.push(fn));
+exports.afterInput = fn => afterInputs.push(fn);
+
+const button = {
+  pinToHandlers: {
+     "5": [],
+     "7": [],
+     "6": [],
+     "8": [],
+    "12": [],
+    "14": [],
+    "13": [],
+    "15": [],
+  },
+  keyToPin: {
+    "w":  "5",
+    "s":  "7",
+    "a":  "6",
+    "d":  "8",
+    "i": "12",
+    "k": "14",
+    "j": "13",
+    "l": "15",
+  }
+};
+
+native.press_cb(pin => {
+  button.pinToHandlers[pin].forEach(f => f());
+
+  afterInputs.forEach(f => f());
+
+  native.map_clear_deltas();
+});
+
+{
+  let timers = [];
+  let id = 0;
+  setTimeout  = (fn, ms) => (timers.push({ fn, ms, id }), id++);
+  setInterval = (fn, ms) => (timers.push({ fn, ms, id, restartAt: ms }), id++);
+  clearTimeout = clearInterval = id => {
+    timers = timers.filter(t => t.id != id);
+  };
+  native.frame_cb(dt => {
+    timers = timers.filter(tim => {
+      if (tim.ms <= 0) {
+        /* trigger their callback */
+        tim.fn();
+
+        /* in case they cleared themselves */
+        if (!timers.some(t => t == tim))
+          return false;
+
+        /* restart intervals, clear timeouts */
+        if (tim.restartAt !== undefined)
+          tim.ms = tim.restartAt;
+        else
+          return false;
+      }
+      tim.ms -= dt;
+      return true;
+    });
+  });
+}
+
+exports.onInput = (key, fn) => {
+  // console.log("engine.js:onInput");
+  const pin = button.keyToPin[key];
+
+  if (pin === undefined)
+    throw new Error(\`the sprig doesn't have a "\${key}" button!\`);
+
+  button.pinToHandlers[pin].push(fn);
+};
+
+exports.playTune = () => {};
+
+function _makeTag(cb) {
+  return (strings, ...interps) => {
+    if (typeof strings === "string") {
+      throw new Error("Tagged template literal must be used like name\`text\`, instead of name(\`text\`)");
+    }
+    const string = strings.reduce((p, c, i) => p + c + (interps[i] ?? ''), '');
+    return cb(string);
+  }
+}
+exports.bitmap = _makeTag(text => text);
+exports.tune = _makeTag(text => text);
+exports.map = _makeTag(text => text);
+return exports;
+})();
 `;
 
 const startupSound = `
@@ -78,49 +199,73 @@ ${imports}
 ${code}
 `;
 
-async function uploadToSerial(serial, code) {
-  let respStr;
-  serial.on("data", t => {
-    for (let c of t) {
-      if (c === 10) return;  // Newline
-      if (c === 13) {        // Carriage return
-        dispatch('UPLOAD_LOG', '< ' + respStr);
-        respStr = '';
+async function getPort() {
+  if (navigator.serial === undefined)
+    throw "your browser does not support the Web Serial API. please try again in a recent version of Chrome.";
+  const ports = await navigator.serial.getPorts();
+  if (ports.length !== 1) {
+    dispatch("UPLOAD_LOG", "please pick a device.");
+    return await navigator.serial.requestPort({
+      filters: [
+        { usbVendorId: 0x2e8a, usbProductId: 10 },
+      ]
+    });
+  } else {
+    return ports[0];
+  }
+}
+
+/* don't await me, just let me sit on promise queue */
+async function logSerialOutput(port) {
+  async function* streamAsyncIterator(stream) {
+    // Get a lock on the stream
+    const reader = stream.getReader();
+
+    try {
+      while (true) {
+        // Read from the stream
+        const {done, value} = await reader.read();
+        // Exit if we're done
+        if (done) return;
+        // Else yield the chunk
+        yield value;
       }
-      else respStr += String.fromCharCode(c);
     }
-  })
-
-  let disconnected = false;
-  const checkDisconnect = () => {
-    if (disconnected) throw new Error("Device disconnected unexpectedly");
-  };
-  serial.on("disconnect", () => disconnected = true)
-
-  const write = (s) => {
-    checkDisconnect();
-    dispatch('UPLOAD_LOG', '> ' + s);
-    return serial.write(s);
+    finally {
+      reader.releaseLock();
+    }
   }
 
-  await write("\r.hi\r");
-  await write("\r.reset\r");
-  await write('\r')
-  await write(".flash -w\r");
-  await delay(500);
+  console.log("for await on reader");
+  const reader = port.readable.pipeThrough(new TextDecoderStream());
+  let str = "";
+  let flushtimeout = setTimeout(() => {}, 0);
+  const flush = () => { console.log('[pico] ' + str); str = "" };
+  for await (const val of streamAsyncIterator(reader)) {
+    str += val;
+    clearTimeout(flushtimeout);
+    flushtimeout = setTimeout(flush, 10);
+  }
+}
 
-  dispatch("UPLOAD_LOG", "transferring file...");
-  checkDisconnect();
-  const result = await transfer(
-    serial,
-    "code",
-    new TextEncoder().encode(startupBurrito(code))
-  );
-  dispatch("UPLOAD_LOG", `wrote ${result.writtenBytes}/${result.totalBytes} bytes`);
-  await delay(500);
-  checkDisconnect();
-  await write("\r.reset\r");
-  await write("\r.load\r");
+async function uploadToSerial(message, writableStream) {
+  // defaultWriter is of type WritableStreamDefaultWriter
+  const defaultWriter = writableStream.getWriter();
+
+  const buf = new TextEncoder().encode(message);
+  console.log(new TextDecoder().decode(buf));
+
+  await defaultWriter.ready;
+  // await defaultWriter.write(new ArrayBuffer(htonl(message.length)));
+  await defaultWriter.write(new Uint32Array([buf.length]).buffer);
+
+  await defaultWriter.ready;
+  await defaultWriter.write(buf);
+  console.log(`wrote ${buf.length} chars`);
+
+  // Ensure all are written before closing
+  await defaultWriter.ready;
+  defaultWriter.close();
 }
 
 export async function upload(code) {
@@ -132,11 +277,13 @@ export async function upload(code) {
 
     dispatch("UPLOAD_LOG", "port found, opening serial stream...");
     const start = Date.now();
-    serial = new Serial(port);
-    await serial.open({ baudRate: 115200 });
+
+    /* connect and begin logging output */
+    await port.open({ baudRate: 115200 });
+    logSerialOutput(port);
     dispatch("UPLOAD_LOG", "connected to rp2040!");
     
-    await uploadToSerial(serial, code);
+    await uploadToSerial(startupBurrito(code), port.writable);
     dispatch("UPLOAD_LOG", `upload complete in ${((Date.now() - start) / 1000).toFixed(2)}s! you may need to unplug and replug your device.`);
   } catch (error) {
     dispatch("UPLOAD_LOG", `error: ${error.toString()}`);
