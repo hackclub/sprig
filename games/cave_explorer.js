ed49ac2bd177be23b0d19df7005fd037fd366b96
superchunkyhamburger <117750561+superchunkyhamburger@users.noreply.github.com>
2022-11-08 09:49:55 -0500

    Add files via upload

---
+++ games/cave_explorer.js
@@ -0,0 +1,140 @@
+/*
+@title: cave_explorer
+@author: sid khare
+
+
+Instructions:
+
+Color the cave.
+J to reset.
+*/
+
+const player = "p";
+const wall = "w";
+const paint = "t";
+
+setLegend(
+  [ player, bitmap`
+................
+................
+................
+................
+.....000........
+.....000........
+.....000........
+......0.........
+....00000.......
+...0..0..0......
+...0..0..0......
+.....000........
+.....0.0........
+.....0.0........
+................
+................`],
+  [ wall, bitmap`
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000
+0000000000000000`],
+  [ paint, bitmap`
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333
+3333333333333333`]
+);
+
+setSolids([player, wall]);
+
+let level = 0;
+const levels = [
+  map`
+p.wwwwwwwwwwwwwww
+w..wwwwwwwww...ww
+ww..wwwwwww..w..w
+www..wwwww..www..
+wwww..www..wwwww.
+wwwww..w..wwwwwww
+wwwwww...wwwwwwww
+wwwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwwww`,
+   map`
+p...ww...........
+.ww.ww.wwwwwwwww.
+.w..ww.........w.
+......wwwwwwww.w.
+.ww.w..ww......w.
+..w..w..w.wwwwww.
+...w..w.w......w.
+.w..w...wwwwwwww.
+.....w...........`,
+];
+
+setMap(levels[level])
+const p = getFirst(player);
+addSprite(p.x - p.dx, p.y - p.dy, paint)
+
+setPushables({
+  [ player ]: [],
+});
+
+onInput("s", () => {
+  getFirst(player).y += 1
+});
+
+onInput("w", () => {
+  getFirst(player).y -= 1
+});
+
+onInput("d", () => {
+  getFirst(player).x += 1
+});
+
+onInput("a", () => {
+  getFirst(player).x -= 1
+});
+
+onInput("j", _ => {
+  setMap(levels[level]);
+  const p = getFirst(player);
+  addSprite(p.x, p.y, paint)
+  
+})
+
+afterInput(_ => {
+  const p = getFirst(player);
+  if (p.dy !== 0 || p.dx !==0) {
+    addSprite(p.x, p.y, paint)
+  }
+
+  if (getAll(paint).length === width() * height() - getAll(wall).length) {
+    level++;
+    if (level in levels) setMap(levels[level])
+    else addText("you win", { y: 5, color: color`H`})
+    const p = getFirst(player);
+    addSprite(p.x, p.y, paint)
+  }
+})
