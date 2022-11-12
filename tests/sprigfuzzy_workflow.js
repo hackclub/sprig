2ca833f2a1e22f1febdd834b3ff5c5d44ace5bd0
SamDev-7 <hello@samliu.dev>
2022-11-11 22:49:52 -0500

    Allow games/ prefix in test input

--- tests/sprigfuzzy_workflow.js
+++ tests/sprigfuzzy_workflow.js
@@ -30,7 +30,10 @@ import { baseEngine } from "../engine/baseEngine.js"
 
 let brokenGames = [];
 const SKIP = ["mandelbrot.js"];
-const ONLY = Deno.args;
+
+const ONLY = Deno.args.filter(x => x.startsWith('games/')).map(x => x.slice(6)).concat(Deno.args.filter(x => !x.startsWith('games/')));
+
+console.log(ONLY);
 
 async function main() {
   brokenGames = [];
