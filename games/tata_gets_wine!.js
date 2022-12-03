79b18bd12a7f056f8bbbea08bfd0c5e7eac05f18
tata47778 <119729144+tata47778@users.noreply.github.com>
2022-12-03 16:56:12 +0800

    Update tata_gets_wine!.js

--- games/tata_gets_wine!.js
+++ games/tata_gets_wine!.js
@@ -1,74 +1,7 @@
 /*
-@title: tata gets wine!
+@title: tata_gets_wine!
 @author: tata
 
-Instructions:
-
-Welcome to Sprig!!!
-
-Hit "run" to execute the code and
-start the game (you can also press shift+enter).
-
-To beat each level you'll have to edit the code.
-
-The code for this game starts below this comment.
-
-The objective is to push the purple boxes onto the green goals.
-Press j to reset the current level.
-
-Click the "open help" to discover your toolkit.
-
---------
-Level 1
---------
-
-Make the purple block pushable.
-
---------
-Level 2
---------
-
-Add controls to move up and left, use "w" and "a" as inputs
-
-Tip: 
-Do you find it annoying restarting at level 0?
-Try adjusting the starting level.
-
---------
-Level 3
---------
-
-Edit the map.
-
---------
-Level 4
---------
-
-Make boxes push boxes.
-
---------
-Level 5
---------
-
-Add sound effects when you move.
-
---------
-Level 6
---------
-
-Solve the puzzle!
-
---------
-END
---------
-
-Make your own game! Try
- - adding two players
- - leaving a trail as you move
- - having different blocks and goal types
- - come up with your own mechanic!
-
-*/
 
 
 const player = "p";
