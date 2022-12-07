00d7158889c524e6f7a265e92cf7fdee07dedbac
Omay <70179858+Omay238@users.noreply.github.com>
2022-12-06 19:59:26 -0700

    henlo

--- games/Advent of Sprig.js
+++ games/Advent of Sprig.js
@@ -1,11 +1,11 @@
 /*
-@title: Advent of Sprig 2
+@title: Advent of Sprig
 @author: Leonard (Omay)
 Every day, I will add a new game or level of a game.
 */
 
 let scene = "menu";
-let availDays = 2;
+let availDays = 6;
 
 const d0l = "a";
 const d1l = "b";
@@ -35,6 +35,8 @@ const wall = "y";
 const goal = "z";
 const box = "0";
 const bg2 = "1";
+const on = "2";
+const off = "3";
 
 setLegend(
   [player, bitmap`
@@ -71,23 +73,6 @@ CCC1CCCCCCC1CCCC
 CCCC1CCCCCCC1CCC
 CCCC1CCCCCCC1CCC
 CCCC1CCCCCCC1CCC`],
-  [goal, bitmap`
-................
-.......DD.......
-......DDDD......
-.......DD.......
-......DDDD......
-.....DDDDDD.....
-......DDDD......
-.....DDDDDD.....
-....DDDDDDDD....
-.....DDDDDD.....
-....DDDDDDDD....
-...DDDDDDDDDD...
-.......CC.......
-.......CC.......
-.......CC.......
-.......CC.......`],
   [box, bitmap`
 .....333.33.....
 .....3..3.3.....
@@ -105,6 +90,57 @@ DDDDDDD33DDDDDDD
 DDDDDDD33DDDDDDD
 DDDDDDD33DDDDDDD
 .DDDDDD33DDDDDD.`],
+  [goal, bitmap`
+................
+.......DD.......
+......DDDD......
+.......DD.......
+......DDDD......
+.....DDDDDD.....
+......DDDD......
+.....DDDDDD.....
+....DDDDDDDD....
+.....DDDDDD.....
+....DDDDDDDD....
+...DDDDDDDDDD...
+.......CC.......
+.......CC.......
+.......CC.......
+.......CC.......`],
+  [on, bitmap`
+4444444444444444
+4444444444444444
+4444444DD4444444
+444444DDDD444444
+44444DDDDDD44444
+444444DDDD444444
+44444DDDDDD44444
+4444DDDDDDDD4444
+44444DDDDDD44444
+4444DDDDDDDD4444
+444DDDDDDDDDD444
+4444444DD4444444
+4444444DD4444444
+4444444DD4444444
+4444444444444444
+4444444444444444`],
+  [off, bitmap`
+3333333333333333
+33333333CCCCC333
+3333333CCCCCC333
+3333333CCCCCC333
+333333CCCCC3CC33
+333333CCCC33CC33
+33333CCCCC333333
+33333CCCCCC33333
+3333CCCCCCCC3333
+3333CCCCCCCC3333
+333CCCCCCCCCC333
+333CCCCCCCCCC333
+33CCCCCCCCCCCC33
+33CCCCCCCCCCCC33
+3333333333333333
+3333333333333333`],
   [sel, bitmap`
 2222222222222222
 22............22
@@ -503,17 +539,28 @@ setPushables({[player]: [box]});
 
 let level = 0;
 const levels = [
-  map`babbca
-bbaabc
-abacab
-ababcc`,
+  map`
+.............
+.b.a.b.b.c.a.
+.............
+.b.b.a.a.b.c.
+.............
+.a.b.a.c.a.b.
+.............
+.a.b.a.b.c.c.
+.............`,
 ];
 const levels2 = [
   map`
-kosnln
-olmpmn
-ltqmsp
-rrtqko`,
+.............
+.k.o.s.n.l.n.
+.............
+.o.l.m.p.m.n.
+.............
+.l.t.q.m.s.p.
+.............
+.r.r.t.q.k.o.
+.............`,
 ];
 const games = [
   map`
@@ -534,8 +581,45 @@ y....0zy.y
 y.yyyyyy.y
 y......0zy
 yyyyyyyyyy`,
+  map`
+yyyyyyyyyy
+yz.....0.y
+yyyyyy0y.y
+yz...0.y.y
+yyyy0y.y.y
+yz.0.y.y.y
+yyyyzyzyxy
+yyyyyyyyyy`,
+  map`
+yyyyyyyyyy
+y333333y3y
+y3yyyy3y3y
+y3y3xy3y3y
+y3y3333y3y
+y3yyyyyy3y
+y33333333y
+yyyyyyyyyy`,
+  map`
+yyyyyyyyyy
+yx333333yy
+y3y3y3y3yy
+y3333333yy
+y3y3y3y3yy
+y3333333yy
+yyyyyyyyyy
+yyyyyyyyyy`,
+  map`
+yyyyyyyyyy
+yx.....33y
+y.yyyyy33y
+y.yzzz033y
+y.y00z033y
+y33333333y
+y33333333y
+yyyyyyyyyy`
 ];
-const days = [[10,4,18,13,21,3],[14,11,2,5,12,23],[1,19,6,22,8,15],[7,17,9,16,20,24]];
+var m = Math.Infinity;
+const days = [[m,m,m,m,m,m,m,m,m,m,m,m,m],[m,10,m,4,m,18,m,13,m,21,m,3,m],[m,m,m,m,m,m,m,m,m,m,m,m,m],[m,14,m,11,m,2,m,5,m,12,m,23,m],[m,m,m,m,m,m,m,m,m,m,m,m,m],[m,1,m,19,m,6,m,22,m,8,m,15,m],[m,m,m,m,m,m,m,m,m,m,m,m,m],[m,7,m,17,m,9,m,16,m,20,m,24,m],[m,m,m,m,m,m,m,m,m,m,m,m,m]];
 function addMap(mapa){
   mapa = mapa.split("\n");
   for(var i = 0; i < mapa.length; i++){
@@ -554,28 +638,28 @@ setBackground(bg);
 onInput("w", () => {
   if(scene === "menu"){
     getFirst(sel).y -= 1;
-  }else if(scene === "game1" || scene === "game2" || scene === "game3"){
+  }else{
     getFirst(player).y -= 1;
   }
 });
 onInput("a", () => {
   if(scene === "menu"){
     getFirst(sel).x -= 1;
-  }else if(scene === "game1" || scene === "game2" || scene === "game3"){
+  }else{
     getFirst(player).x -= 1;
   }
 });
 onInput("s", () => {
   if(scene === "menu"){
     getFirst(sel).y += 1;
-  }else if(scene === "game1" || scene === "game2" || scene === "game3"){
+  }else{
     getFirst(player).y += 1;
   }
 });
 onInput("d", () => {
   if(scene === "menu"){
     getFirst(sel).x += 1;
-  }else if(scene === "game1" || scene === "game2" || scene === "game3"){
+  }else{
     getFirst(player).x += 1;
   }
 });
@@ -585,12 +669,33 @@ onInput("i", () => {
       scene = "game"+days[getFirst(sel).y][getFirst(sel).x];
       setMap(games[days[getFirst(sel).y][getFirst(sel).x]-1]);
       setBackground(bg2);
+      checkOnOff();
     }
   }
 });
-afterInput(() => {
+onInput("j", () => {
   if(scene !== "menu"){
-    if(tilesWith(goal, box).length === tilesWith(goal).length){
+    setMap(games[parseInt(scene.substr(4))-1]);
+    setBackground(bg2);
+    checkOnOff();
+  }
+});
+function checkOnOff(){
+  var a = tilesWith(player);
+  for(var i = 0; i < a.length; i++){
+    for(var j = 0; j < a[i].length; j++){
+      if(a[i][j].type === on){
+        a[i][j].type = off;
+      }else if(a[i][j].type === off){
+        a[i][j].type = on;
+      }
+    }
+  }
+}
+afterInput(() => {
+  checkOnOff();
+  if(scene !== "menu"){
+    if(tilesWith(goal, box).length === tilesWith(goal).length && getAll(off).length === 0){
       scene = "menu";
       setMap(levels[level]);
       addMap(levels2[level]);
