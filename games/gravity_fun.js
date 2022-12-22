a3e76f5d9b8b14504ff1f6f0a937d5515d97391c
phantomeniasll <77569098+phantomeniasll@users.noreply.github.com>
2022-12-22 15:58:17 +0100

    Update gravity_fun.js
    Add Graphic Overhaul

--- games/gravity_fun.js
+++ games/gravity_fun.js
@@ -13,36 +13,36 @@ setLegend(
 0000000000000000
 0000000000000000
 0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
+000LLLLLLLLLL000
+000LLLLLLLLLL000
+000LL111111LL000
+000LL111111LL000
+000LL111111LL000
+000LL111111LL000
+000LL111111LL000
+000LL111111LL000
+000LLLLLLLLLL000
+000LLLLLLLLLL000
 0000000000000000
 0000000000000000
 0000000000000000`],
   [ pipe, bitmap`
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000`],
+LLCC33333333CCCC
+LLCC33333333CCCC
+LCCC33333333CCCC
+LCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC
+CCCC33333333CCCC`],
 );
 
 let level =  map`
@@ -94,7 +94,7 @@ function generateLevel(xDim, yDim,xPlayer, yPlayer) {
   for(var x = 0; x <xDim; x++){
     var row = "";
     for(var y=0; y < yDim; y++){
-      if(x < xPlayer && y < yPlayer ){
+      if(x+1 < xPlayer+1 && y < yPlayer  && y != 0 && x != 0){
       row += "p";}
       else{
         row += ".";}
@@ -118,34 +118,34 @@ setPushables({
   [player]: []
 });
 
+//CONSTANTS
+var openingWidth = 10;
+var gravity = 0.3;
+var jumpStrength = 1.8;
+var updateTimems = 30;
+
 // START - PLAYER MOVEMENT CONTROLS
 
 onInput("w", () => {
-  velocity = -1.5;
+  velocity = -jumpStrength;
 });
 
 // END - PLAYER MOVEMENT CONTROLS
-
-onInput("j", () => {
-  const currentLevel = levels[level];
-  if (currentLevel !== undefined) {
-    clearText("");
-    setMap(currentLevel);
-  }
-});
 
 function randomIntFromInterval(min, max) { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+
+
 var velocity = 0;
 var pipes = [Math.round(width/3),Math.round(2*width/3),width-1];
 var openings = [30,40,20];
-var openingWidth = 10;
+
 var frontPipeIndex = 0;
 var score = 0;
 clearText();
-addText("Score:"+score,width/2,3,color`H`);
+addText("Score:"+score);
 
 function drawPipes(){
   for(var i=0;i<pipes.length;i++)
@@ -176,13 +176,13 @@ function movePipes(){
     }
     else{
       p.x -= 1;
-      
+
     }
   });
   if(respawnFrontPipe){
     score += 1;
     clearText();
-    addText("Score:"+score,width/2,3,color`H`);
+    addText("Score:"+score);
     unDrawPipe(0);
     openings[frontPipeIndex] = randomIntFromInterval(20, height-20);
     for(var y=0;y<height;y++){
@@ -195,29 +195,34 @@ function movePipes(){
 }
 drawPipes();
 var physicUpdate = () => {
-  getAll(player).forEach(p => p.y += Math.floor(velocity));
+  let die = false;
+  getAll(player).forEach(p => {
+    p.y += Math.floor(velocity)
+    if(p.y == 0 || p.y == width -3 )
+    {
+      die=true;
+    }
+  });
   movePipes();
-  if(tilesWith(player,pipe).length != 0){
+  if(tilesWith(player,pipe).length != 0 || die){
     score = 0;
     clearText();
-    addText("Score:"+score,width/2,3,color`H`);
+    addText("Score:"+score);
     setMap(currentLevel);
     velocity = 0;
     for(var i=0; i<3;i++)
       {
         openings[i] = randomIntFromInterval(20, height-20);
       }
-    
-    openingWidth = 10;
     frontPipeIndex = 0;
     unDrawPipes();
     drawPipes();
   }
- 
-velocity += 0.2;
-setTimeout(physicUpdate,30);     
+
+velocity += gravity;
+setTimeout(physicUpdate,updateTimems);     
 }
 physicUpdate();
 afterInput(() => {
-  
+
 });
