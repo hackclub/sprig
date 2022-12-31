d1d45d9abfa1cca4f871697d4df2813bf92ce0d5
P-Rama-Ganesh <121034092+P-Rama-Ganesh@users.noreply.github.com>
2023-01-01 01:58:01 +0530

    Add files via upload

---
+++ puzzle_simple_5.js
@@ -0,0 +1,172 @@
+/*
+@title: puzzle_simple_5
+@author: P. Rama Ganesh
+
+const player = "p";
+const box = "b";
+const goal = "g";
+const wall = "w";
+
+setLegend(
+  [ player, bitmap`
+......L.........
+....0LL.L.......
+...00LLLL.......
+...LLLLLL.......
+...0L...L.......
+...0.3.30.......
+...0....0.......
+.00000000000....
+.00444047440....
+.00444044440....
+.00444000040....
+.00000000000....
+.00066066000....
+...0660660......
+...0660660......
+...0000000......`],
+  [ box, bitmap`
+................
+................
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+..CCCCCCCCCCC...
+................
+................
+................`],
+  [ goal, bitmap`
+................
+................
+....4444444.....
+...444444444....
+..44444444444...
+..44422222444...
+..44422222444...
+..44422222444...
+..44422222444...
+..44422222444...
+..44444444444...
+...444444444....
+....4444444.....
+................
+................
+................`],
+  [ wall, bitmap`
+CCC0CCCC0CCCC0CC
+0000000000000000
+C0CCCC0CCCC0CCCC
+0000000000000000
+CCC0CCCC0CCCC0CC
+0000000000000000
+C0CCCC0CCCC0CCCC
+0000000000000000
+CCC0CCCC0CCCC0CC
+0000000000000000
+C0CCCC0CCCC0CCCC
+0000000000000000
+CCC0CCCC0CCCC0CC
+0000000000000000
+C0CCCC0CCCC0CCCC
+0000000000000000`]
+);
+
+let level = 0;
+const levels = [
+  map`
+www..
+..b..
+pw.g.`,
+  map`
+w.gb..
+..w...
+w..pw.
+.bw.w.
+.....g`,
+  map`
+ww....
+w...b.
+ww..w.
+w.p..g
+w..b.b
+w..ww.
+w..gwg`,
+  map`
+..gg..
+w.....
+...www
+.bp.b.
+.b..b.
+www...
+..gg..`,
+  map`
+wwwwwwww
+wwww..ww
+wgpb..ww
+www..gww
+wgw...ww
+w.w.b.gw
+wbbgb..w
+w......w
+wwwwwwww`
+ ];
+
+const currentLevel = levels[level];
+setMap(currentLevel);
+
+setSolids([ player, box, wall ]);
+
+setPushables({
+  [player]: [box]
+});
+
+
+
+onInput("a", () => {
+  getFirst(player).x -= 1;
+});
+
+onInput("d", () => {
+  getFirst(player).x += 1;
+});
+onInput("w", () => {
+  getFirst(player).y -= 1;
+});
+onInput("s", () => {
+  getFirst(player).y += 1;
+});
+
+
+onInput("j", () => {
+  const currentLevel = levels[level];
+  if (currentLevel !== undefined) {
+    clearText("");
+    setMap(currentLevel);
+  }
+});
+
+afterInput(() => {
+  const targetNumber = tilesWith(goal).length;
+  
+  const numberCovered = tilesWith(goal, box).length;
+
+  if (numberCovered === targetNumber) {
+    level = level + 1;
+
+    const currentLevel = levels[level];
+
+    if (currentLevel !== undefined) {
+      setMap(currentLevel);
+    } else {
+      addText("you win!", { y: 4, color: color`3` });
+      
+    }
+  }
+});
