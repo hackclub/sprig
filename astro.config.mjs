2d29183851d8e005848e875c9266fa32603ea8f3
Kognise <felix.mattick@gmail.com>
2023-02-13 13:46:42 -0500

    Fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
-import vercel from '@astrojs/vercel/edge'
+import vercel from '@astrojs/vercel/serverless'
 import prefresh from '@prefresh/vite'
 
 export default defineConfig({
