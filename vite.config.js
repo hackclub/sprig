5c776c68d73841cccd92bcb0db83700b4cb3bc35
Kognise <felix.mattick@gmail.com>
2023-02-17 13:58:58 -0500

    Multiple rollup entrypoints:

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
 export default {
-  publicDir: "games"
+  publicDir: "games",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(_dirname, "index.html"),
+        migration: resolve(_dirname, "migration-iframe.html")
+      }
+    }
+  }
 }
