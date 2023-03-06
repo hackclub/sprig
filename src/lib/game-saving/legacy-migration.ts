25f51736aaf65cedb022d22c1231bdb5a9ddca45
Kognise <felix.mattick@gmail.com>
2023-03-06 11:24:53 -0500

    Add warning copy on unsupported migration browsers

--- src/lib/game-saving/legacy-migration.ts
+++ src/lib/game-saving/legacy-migration.ts
@@ -1,9 +1,20 @@
+import { Signal, useSignal } from '@preact/signals'
+import { useEffect } from 'preact/hooks'
+	
+// const helperUrl = 'http://localhost:3001/migration-helper.html'
+const helperUrl = 'https://editor.sprig.hackclub.com/migration-helper.html'
+
 export const getPuzzleLabFromLocalStorage = (allowRedirect: boolean): Promise<string> => new Promise((resolve) => {
 	const params = new URLSearchParams(window.location.search)
-	if (params.get('puzzleLab') !== null) return resolve(params.get('puzzleLab')!)
-	
-	// const helperUrl = 'http://localhost:3001/migration-helper.html'
-	const helperUrl = 'https://editor.sprig.hackclub.com/migration-helper.html'
+	if (params.get('puzzleLab') !== null) {
+		// Remove the query param, it's cleaner for the user and means
+		// reloading the page refreshes the localStorage data.
+		const url = new URL(window.location.toString())
+		url.searchParams.delete('puzzleLab')
+		history.replaceState({}, '', url.toString())
+
+		return resolve(params.get('puzzleLab')!)
+	}
 
 	let iframe: HTMLIFrameElement
 	const listener = (event: MessageEvent<unknown>) => {
@@ -20,6 +31,8 @@ export const getPuzzleLabFromLocalStorage = (allowRedirect: boolean): Promise<st
 			window.removeEventListener('message', listener)
 
 			if (event.data.storageAccessDenied) {
+				// document.requestStorageAccess() errored in the frame, we need to
+				// redirect through the helper to get data.
 				if (!allowRedirect) {
 					console.warn('Storage access denied, but a redirect would be bad for UX')
 					return resolve('')
@@ -38,3 +51,36 @@ export const getPuzzleLabFromLocalStorage = (allowRedirect: boolean): Promise<st
 	iframe.style.display = 'none'
 	document.body.appendChild(iframe)
 })
+
+export const useNeedsManualMigration = (): Signal<boolean> => {
+	const needsManualMigration = useSignal(false)
+
+	useEffect(() => {
+		let iframe: HTMLIFrameElement
+		const listener = (event: MessageEvent<unknown>) => {
+			if (event.data
+			&& typeof event.data === 'object'
+			&& 'sprig' in event.data 
+			&& event.data.sprig === true
+			&& 'storageAccessDenied' in event.data
+			&& typeof event.data.storageAccessDenied === 'boolean'
+			) {
+				if (iframe) iframe.remove()
+				window.removeEventListener('message', listener)
+				needsManualMigration.value = event.data.storageAccessDenied
+			}
+		}
+		window.addEventListener('message', listener)
+		iframe = document.createElement('iframe')
+		iframe.src = helperUrl
+		iframe.style.display = 'none'
+		document.body.appendChild(iframe)
+
+		return () => {
+			if (iframe) iframe.remove()
+			window.removeEventListener('message', listener)
+		}
+	}, [])
+
+	return needsManualMigration
+}
