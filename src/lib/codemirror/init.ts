d44ec86720465fc4995dee219e03952e369838b6
Graham Darcey <graham@hackclub.com>
2023-03-14 13:45:50 -0400

    Do not initialize the text in the editor using an undoable transaction

--- src/lib/codemirror/init.ts
+++ src/lib/codemirror/init.ts
@@ -9,8 +9,9 @@ import { effect, signal } from '@preact/signals'
 import { h, render } from 'preact'
 import SearchBox from '../../components/search-box'
 
-export function createEditorState(onUpdate = () => {}, onRunShortcut = () => {}): EditorState {
+export function createEditorState(initialCode : string , onUpdate = () => {}, onRunShortcut = () => {}): EditorState {
 	return EditorState.create({
+		doc:initialCode,
 		extensions: [
 			lineNumbers(),
 			highlightActiveLineGutter(),
