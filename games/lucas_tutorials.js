02f7b8b51c3d0f0dcdba2ecf0ba72f8722e21822
Lucas <88567622+LucasHT22@users.noreply.github.com>
2023-03-18 23:37:27 -0300

    Create lucas_tutorials.js

---
+++ games/lucas_tutorials.js
@@ -0,0 +1,8 @@
+/*
+Never gonna give you up
+Never gonna let you down
+Never gonna turn around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+*/
