c9d353c6f52404301da24f8e525c86a534451751
Alex Sirota <67526318+ajs256@users.noreply.github.com>
2023-05-11 16:02:18 -0700

    One more addition

--- src/lib/engine/1-base/index.ts
+++ src/lib/engine/1-base/index.ts
@@ -262,8 +262,16 @@ export function baseEngine() {
 		return tiles
 	}
 
-	const setSolids = (arr: string[]): void => { gameState.solids = arr }
-	const setPushables = (map: Record<string, string[]>): void => { gameState.pushable = map }
+	const setSolids = (arr: string[]): void => { 
+		if (!Array.isArray(arr)) throw new Error('The sprites passed into setSolids() need to be an array.')
+		gameState.solids = arr 
+	}
+	const setPushables = (map: Record<string, string[]>): void => { 
+		for (const key in map) {
+			_checkLegend(key)
+		}
+		gameState.pushable = map 
+	}
 
 	const api = {
 		setMap, 
