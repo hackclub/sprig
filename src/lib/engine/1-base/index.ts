5f83d5f7bc7e7f579d44ce1a91a8e7a98e623063
Alex Sirota <67526318+ajs256@users.noreply.github.com>
2023-05-11 13:01:55 -0700

    Start adding more error messages
    Related to hackclub#972

--- src/lib/engine/1-base/index.ts
+++ src/lib/engine/1-base/index.ts
@@ -1,7 +1,7 @@
 import { palette } from './palette'
 import type { Text } from './text'
 
-// Tagged tempalate literal factory go brrr
+// Tagged template literal factory go brrr
 const _makeTag = <T>(cb: (string: string) => T) => {
 	return (strings: string[], ...interps: string[]) => {
 		if (typeof strings === 'string') {
@@ -65,7 +65,7 @@ export function baseEngine() {
 
 		set type(newType) {
 			const legendDict = Object.fromEntries(gameState.legend)
-			if (!(newType in legendDict)) throw new Error(`"${newType}" not in legend.`)
+			if (!(newType in legendDict)) throw new Error(`"${newType}" isn\'t in the legend.`)
 			this.remove()
 			addSprite(this._x, this._y, newType)
 		}
@@ -181,11 +181,14 @@ export function baseEngine() {
 
 	const setMap = (string: string): void => {
 		if (!string) throw new Error('Tried to set empty map.')
+
+		if (string.constructor == Object) throw new Error('setMap() takes a string, not a dict.') // https://stackoverflow.com/a/51285298
+		if (Array.isArray(string)) throw new Error('It looks like you passed an array into setMap(). Did you mean to use something like setMap(levels[level]) instead of setMap(levels)?')
 		
 		const rows = string.trim().split("\n").map(x => x.trim())
 		const rowLengths = rows.map(x => x.length)
 		const isRect = _allEqual(rowLengths)
-		if (!isRect) throw new Error('Level must be rect.')
+		if (!isRect) throw new Error('Level must be rectangular.')
 		const w = rows[0]?.length ?? 0
 		const h = rows.length
 		gameState.dimensions.width = w
