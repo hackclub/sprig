57a3285ea2b0ee9d34c01e3ed9c4b8b7056d2e1a
Alex Sirota <67526318+ajs256@users.noreply.github.com>
2023-05-11 15:48:40 -0700

    Add some nicer messages to setLegend()

--- src/lib/engine/2-web/index.ts
+++ src/lib/engine/2-web/index.ts
@@ -76,6 +76,10 @@ export function webEngine(canvas: HTMLCanvasElement) {
 	let animationId = window.requestAnimationFrame(_gameloop)
 
 	const setLegend = (...bitmaps: [string, string][]): void => {
+		if (bitmaps.length == 0) throw new Error('There needs to be at least one sprite in the legend.')
+
+		if (!Array.isArray(bitmaps[0])) throw new Error('The sprites passed into setLegend each need to be in square brackets, like setLegend([player, bitmap`...`]).')
+
 		bitmaps.forEach(([ key ]) => {
 			if (key === '.') throw new Error(`Can't reassign "." bitmap`)
 			if (key.length !== 1) throw new Error(`Bitmaps must have one character names`)
