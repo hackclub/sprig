7944f057865d5c66bdcaef4f1c89f7d16f9abd37
Chirag <chirag1292003@gmail.com>
2023-05-14 19:29:34 +0530

    Updates for drab-bullets
    - [x] More bullets
    - [x] A new powerdown
    - [x] New player
    - [x] New ground
    - [x] New mechanics
    - [x] New scoring
    - [x] New Gameover Screen
    - [x] A whole lot of fun

--- games/drab_bullets.js
+++ games/drab_bullets.js
@@ -1,34 +1,35 @@
-/*
-First time? Check out the tutorial game:
-https://sprig.hackclub.com/gallery/getting_started
-*/
-
 const player = "p"
 const ground = "g"
 const bullet = "b"
+const curse = "c"
+var player_cursed = false
+var life = 3
+var gameover = false
+var score = 0
+var speed = 200
 
 setLegend(
   [ player, bitmap`
 ................
 ................
 ................
-................
-................
-................
-................
-................
-....3333330.....
-...336666330....
-...369669630....
-..33666666333...
-...366999630....
-...336666330....
-....333333......
-......3..3......` ],
+....333333330...
+...33666666630..
+..3366666666630.
+..3666666666630.
+..3699966999630.
+.336696666966333
+333666666666633.
+..3666699966630.
+..3366666666330.
+...33666666330..
+....333333333...
+.....333.333....
+......3...3.....` ],
   [ ground, bitmap`
-DDDDDDDDDDDDDDDD
-DDDDDDDDDDDDDDDD
-DDDDDDDDDDDDDDDD
+CCCCCCCCCCCCCCCC
+CCCCCCCCCCCCCCCC
+DCCDDCCCCDDCCCCC
 DDDDDDDDDDDDDDDD
 DDDDDDDDDDDDDDDD
 DDDDDDDDDDDDDDDD
@@ -59,17 +60,34 @@ DDDDDDDDDDDDDDDD`],
 ................
 ................
 ................`],
+  [ curse, bitmap`
+................
+.....99999......
+....9000009.....
+...900000009....
+..90000000009...
+..00000000000...
+..00330003300...
+..00000000000...
+..00000H00000...
+..0000HHH0000...
+..0000HHH0000...
+...000000000....
+.....00000......
+......000.......
+.......0........
+................`],
 )
 
-// setSolids([player, wall])
+setSolids([player, ground])
 
 let level = 0
 const levels = [
   map`
-.bbbbbbbbbbbb
-b.....b..b...
-....b........
-..b....bb.b..
+.............
+.............
+.............
+.............
 .............
 .............
 .............
@@ -80,28 +98,142 @@ ggggggggggggg`
 ]
 
 setMap(levels[level])
-
-setPushables({
-  [ player ]: [bullet],
-  [ bullet ]: [player],
-  
+onInput("j", () => {
+  player_cursed = false
+  life = 3
+  gameover = false
+  score = 0
+  speed = 200
+  clearText()
+  addText(`${score}`, { 
+        x: width()+2,
+        y: 0,
+        color: color`6`
+  })
+  addText(`Lifes: ${life}`, { 
+      x: 1,
+      y: 0,
+      color: color`3`
 })
-
+})
+//// Movements
 onInput("a", () => {
-  getFirst(player).x -= 1
-  getFirst(bullet).y += 1
+  if (player_cursed === false){
+    getFirst(player).x -= 1
+  }else{
+    getFirst(player).x += 1
+  }
 })
-
 onInput("d", () => {
-  getFirst(player).x += 1
+  if (player_cursed === false){
+    getFirst(player).x += 1
+  }else{
+    getFirst(player).x -= 1
+  }
 })
-
-afterInput(() => {
-  var curr_bullet = getFirst(bullet)
-  if (curr_bullet !== undefined){
-    curr_bullet.y +=1
+////
+//// Intervals
+setInterval(() => {
+  if(gameover === false){
+    let x = Math.floor(Math.random() * width());
+    let y = 0; 
+    addSprite(x, y, bullet)
+  }
+}, 400);
+setInterval(() => {
+  if(gameover === false){
+    let x = Math.floor(Math.random() * (width() - 5));
+    let y = 0; 
+    addSprite(x, y, curse)
+  }
+}, 6000);
+////
+//// Main interval
+setInterval(() => {
+  if(gameover === false){
+    const play = getFirst(player)
+    //// movements and collisions
+    getAll(bullet).forEach(bull => {
+      bull.y += 1;
+      if(bull.x === play.x && bull.y === play.y){
+        life -= 1
+      }
+      else if(bull.y > 9){
+        bull.remove()
+      }
+    });
+    getAll(curse).forEach(cur => {
+      cur.y += 1;
+      if(cur.x === play.x && cur.y === play.y){
+        player_cursed = true
+        setInterval(() => {
+          player_cursed = false
+        }, 5000)
+      }
+      else if(cur.y > 9){
+        cur.remove()
+      }
+    });
+    ////
+    //// Text
+    addText(`Lifes: ${life}`, { 
+      x: 1,
+      y: 0,
+      color: color`3`
+    })
+    if(life === 0){
+      addText(`GAME OVER`, { 
+        x: Math.floor(width()/2),
+        y: Math.floor(height()/2),
+        color: color`3`
+      })
+      addText(`PRESS "J"`, { 
+        x: Math.floor(width()/2),
+        y: Math.floor(height()/2) + 1,
+        color: color`3`
+      })
+      addText(`TO RESTART`, { 
+        x: Math.floor(width()/2),
+        y: Math.floor(height()/2) + 2,
+        color: color`3`
+      })
+      gameover = true
+    }
+    if(life > 0){
+      score += 2
+      addText(`${score}`, { 
+        x: width()+2,
+        y: 0,
+        color: color`6`
+      })
+    }
+    ////
+    //// speeding with score/time
+    if(score === 100){
+      speed -= 20
+    }
+    if(score === 200){
+      speed -=30
+    }
+    if(score === 300){
+      speed -=30
+    }
+    if(score === 400){
+      speed -= 30
+    }
   }
   else{
-    curr_bullet = getFirst(bullet)
+    getAll(bullet, curse).forEach(all => {
+        all.remove()
+    });
   }
-})
+  ////
+}, speed);
+////
+
+
+
+
+
+
+
