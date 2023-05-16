5a140533737cca2b9d6c462dd5ec027873be6e3e
Leonard Maculo <70179858+Omay238@users.noreply.github.com>
2023-05-15 21:41:48 -0600

    Throw console error if glyph doesn't exist. (#1030)
    * Update text.ts
    
    * fix an error caused by my incredible levels of incompetence
    
    * i wish we could use console.error. its simpler

--- src/lib/engine/1-base/text.ts
+++ src/lib/engine/1-base/text.ts
@@ -20,9 +20,12 @@ export function composeText(texts: Text[]): { char: string, color: Rgba }[][] {
 		let y = sy
 		for (const line of content.split('\n')) {
 			let x = sx
-			for (const char of line.split(''))
+			for (const char of line.split('')) {
+				if ("!\"#%&\'()*+,./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\^_`abcdefghijklmnopqrstuvwxyz|~¦§¨©¬®¯°±´¶·¸ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ÙÚÛÜÝÞßàáâãäåæçèéêëìíîïñòóôõö÷ùúûüýþÿĀāĂăĄąĆćĊċČčĎĐđĒēĖėĘęĚěĞğĠġĦħĪīĮįİıŃńŇňŌōŒœŞşŨũŪūŮůŲųŴŵŶŷŸǍǎǏǐǑǒǓǔˆˇ˘˙˚˛˜˝ẀẁẂẃẄẅỲỳ†‡•…‰⁄™∂∅∏∑−√∞∫≈≠≤≥◊".indexOf(char) === -1)
+					throw new Error(`Character ${char} is not in the font. It will be rendered incorrectly.`)
 				if (x <= CHARS_MAX_X && y < CHARS_MAX_Y)
-					grid[y]![x++] = { color: color, char }
+					grid[y]![x++] = {color: color, char}
+			}
 			y++
 		}
 	}
