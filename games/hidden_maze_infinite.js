54c616385e3d5beda52e685ddee788ffbe589948
Dark-Sniper101 <89219580+Dark-Sniper101@users.noreply.github.com>
2023-05-17 10:51:25 -0500

    Update hidden_maze_infinite.js

--- games/hidden_maze_infinite.js
+++ games/hidden_maze_infinite.js
@@ -3,9 +3,128 @@ Welcome to Hidden Maze Infinite (or Hidden Maze 2 if you will), the 2nd game in 
 I would recomend playing the 1st game, its a short introductory for what is to come.
 Controls are the same WASD, don't bother with worrying about getting to the end in this one, there is none, 
 I have carefully constructed a maze generation that will allow you to play for as long as you like.
-Remember, have fun!
+Remeber, have fun!
+
+UPDATE 2.0
+This is the second update to this game, it adds a variable map size, 
+the map types can be changed by using the following buttons:
+
+  J: Easy/Baby maps-5x4
+  I: Normal maps-10x8
+  K: Hard/Chad maps-20x16
+
+Normal maps will be the standard map type when you load in. 
+Also this update incidentally allows you to re-start a map type 
+if you press the change map button for the same map type you're on.
+Ex: Say you're stuck on a normal map nd you want anotherone, 
+    you just press "I" to generate another map.
+    
 */
 
+const player = "p"
+const wall = "w"
+const wall2 = "c"
+const goal = "g"
+
+setLegend(
+  [ player, bitmap`
+................
+......22222.....
+......2FFF2.....
+.....22F6F2.....
+.....2F66F2222..
+...222F666F202..
+...200F060F202..
+...202F666F002..
+...202F660F222..
+...222F006F2....
+....2F6666F2....
+....2F666F22....
+....22FFF22.....
+....2202022.....
+....2002002.....
+....2222222.....` ],
+  [ goal, bitmap`
+................
+.22222222222222.
+2266666666666622
+26266F666F666262
+26266F6F6F666262
+226666FFF6666622
+.22266666666222.
+...2266666622...
+....22266222....
+......2662......
+......2662......
+.....226622.....
+....22666622....
+...2266666622...
+...2666666662...
+...2222222222...` ],
+  [ wall, bitmap`
+0000000000000000
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0000000000000000
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD` ],
+  [ wall2, bitmap`
+0000000000000000
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+0CCCCCCC0LLLLLLL
+00000000L0000000
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD
+0FFFFFFF0DDDDDDD` ]
+)
+
+setBackground(wall)
+
+setSolids([ player, wall2 ])
+
+
+
+setMap(map`
+..........
+..........
+..........
+..........
+..........
+..........
+..........
+..........`) //edit this for changing map sizes, try to keep the ratio of width:height as 5:4
+
+
+function resetMap(){
+  for (let i = 0; i < width(); i++){
+    for(let j = 0; j < height(); j++){
+      clearTile(i,j);
+    }
+  }
+  addSprite(0,0, player)
+  
+}
+
+resetMap()
 
 class Cell{
   constructor(isWall){
@@ -21,11 +140,17 @@ class XYPos{
   }
     
 }
+
+
+
+
+
+
 //makes myMap
 let myMap;
-myMap = new Array(10);
+myMap = new Array(width());
 for (let a = 0; a < myMap.length; a++) {
-  myMap[a] = new Array(8);
+  myMap[a] = new Array(height());
   for (let b = 0; b < myMap[a].length; b++) {
     myMap[a][b] = new Cell(true);
   }
@@ -156,95 +281,7 @@ function updateMap(){
 
 
 
-const player = "p"
-const wall = "w"
-const wall2 = "c"
-const goal = "g"
 
-setLegend(
-  [ player, bitmap`
-................
-......22222.....
-......2FFF2.....
-.....22F6F2.....
-.....2F66F2222..
-...222F666F202..
-...200F060F202..
-...202F666F002..
-...202F660F222..
-...222F006F2....
-....2F6666F2....
-....2F666F22....
-....22FFF22.....
-....2202022.....
-....2002002.....
-....2222222.....` ],
-  [ goal, bitmap`
-................
-.22222222222222.
-2266666666666622
-26266F666F666262
-26266F6F6F666262
-226666FFF6666622
-.22266666666222.
-...2266666622...
-....22266222....
-......2662......
-......2662......
-.....226622.....
-....22666622....
-...2266666622...
-...2666666662...
-...2222222222...` ],
-  [ wall, bitmap`
-0000000000000000
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0000000000000000
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD` ],
-  [ wall2, bitmap`
-0000000000000000
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-0CCCCCCC0LLLLLLL
-00000000L0000000
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD
-0FFFFFFF0DDDDDDD` ]
-)
-
-setBackground(wall)
-
-setSolids([ player, wall2 ])
-
-setMap(map`
-p.........
-..........
-..........
-..........
-..........
-..........
-..........
-..........`)
 //changes map
 
 changeWalls();
@@ -270,21 +307,86 @@ onInput("d", () => {
     getFirst(player).x += 1
 })
 
+onInput("j", () => { //baby
+  setMap(map`
+.....
+.....
+.....
+.....`);
+  resetMap();
+    myMap = new Array(width());
+    for (let a = 0; a < myMap.length; a++) {
+      myMap[a] = new Array(height());
+      for (let b = 0; b < myMap[a].length; b++) {
+        myMap[a][b] = new Cell(true);
+      }
+    }
+    changeWalls()
+    updateMap()
+})
+
+onInput("i", () => {//normal
+  setMap(map`
+..........
+..........
+..........
+..........
+..........
+..........
+..........
+..........`);
+  resetMap();
+    myMap = new Array(width());
+    for (let a = 0; a < myMap.length; a++) {
+      myMap[a] = new Array(height());
+      for (let b = 0; b < myMap[a].length; b++) {
+        myMap[a][b] = new Cell(true);
+      }
+    }
+    changeWalls()
+    updateMap()
+})
+
+
+onInput("l", () => { //chad
+  setMap(map`
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................`);
+  resetMap();
+    myMap = new Array(width());
+    for (let a = 0; a < myMap.length; a++) {
+      myMap[a] = new Array(height());
+      for (let b = 0; b < myMap[a].length; b++) {
+        myMap[a][b] = new Cell(true);
+      }
+    }
+    changeWalls()
+    updateMap()
+})
+
+
+
 afterInput(() => {
   if (getFirst(goal).y == getFirst(player).y &&
       getFirst(goal).x == getFirst(player).x) {
-    setMap(map`
-p.........
-..........
-..........
-..........
-..........
-..........
-..........
-..........`)
-    myMap = new Array(10);
+    resetMap();
+    myMap = new Array(width());
     for (let a = 0; a < myMap.length; a++) {
-      myMap[a] = new Array(8);
+      myMap[a] = new Array(height());
       for (let b = 0; b < myMap[a].length; b++) {
         myMap[a][b] = new Cell(true);
       }
