815cab2189c903e395130f6e2d7df2326dbb5333
Anshuman M <72782632+m-anshuman2166@users.noreply.github.com>
2023-05-17 18:44:50 +0530

    Fix score increment even after game is over

--- games/Maths_Speedrun.js
+++ games/Maths_Speedrun.js
@@ -5,18 +5,18 @@
              D for Multiplication
              S to Reset the game
 */
-
 const player = "p"
-let score=0;
+let score = 0;
 
 var no1 = 0;
 var no2 = 0;
 
 var looping = true;
+var isOver = false;
 var res = 0;
 
 setLegend(
-  [ player, bitmap`
+    [player, bitmap`
 ................
 ................
 ................
@@ -32,14 +32,14 @@ setLegend(
 ................
 ................
 ................
-................` ]
+................`]
 )
 
 setSolids([])
 
 let level = 0
 const levels = [
-  map`
+    map`
 ....
 ..p.
 ....`
@@ -47,132 +47,136 @@ const levels = [
 
 function gameOver() {
     looping = false;
+    isOver = true;
     clearText()
-    addText("Gameover", { 
-          x: 5,
-          y: 1,
-          color: color`3`
-        })
-    addText("S = Reset" , { 
-             x: 1,
-             y: 9,
-             color: color`9`
-        })
-  addText("Score:"+score, { 
+    addText("Gameover", {
+        x: 5,
+        y: 1,
+        color: color`3`
+    })
+    addText("S = Reset", {
+        x: 1,
+        y: 9,
+        color: color`9`
+    })
+    addText("Score:" + score, {
         x: 3,
         y: 3,
         color: color`3`
-      })
-  setTimeout(function() {
-    clearText()
-    addText("Gameover", { 
-          x: 5,
-          y: 1,
-          color: color`3`
+    })
+    setTimeout(function() {
+        clearText()
+        addText("Gameover", {
+            x: 5,
+            y: 1,
+            color: color`3`
         })
-    addText("S = Reset" , { 
-             x: 1,
-             y: 9,
-             color: color`9`
+        addText("S = Reset", {
+            x: 1,
+            y: 9,
+            color: color`9`
         })
-    addText("Score:"+score, { 
-        x: 3,
-        y: 3,
-        color: color`3`
-      })
-  }, 1400);
-  
+        addText("Score:" + score, {
+            x: 3,
+            y: 3,
+            color: color`3`
+        })
+    }, 1400);
+
 }
 
 function reset() {
-   clearText()
-  looping = true;
-  loop(2);
+    clearText()
+    looping = true;
+    isOver = false;
+    loop(2);
 
 }
 setMap(levels[level])
 
 setPushables({
-  [ player ]: []
+    [player]: []
 })
 
 
 function setres(x) {
-  res=x;
+    res = x;
 }
 
 
 
 function loop(count) {
-  if(looping) {
-    setTimeout(function() {
-      if(looping) {
-        clearText();
-  
-        no1 = Math.floor(Math.random() * count) + 1;
-        no2 = Math.floor(Math.random() * count) + 1;
-  
-        setres(0)
-        var op = Math.floor(Math.random() * 2) + 1;
-        if(op == 1) {
-          setres(no1 * no2);
-        } else {
-          setres(no1 + no2);
-  
-        }
-        addText(no1 +"_?_"+ no2 + "=" + res + "" , { 
-             x: 6,
-             y: 4,
-             color: color`4`
-        })
-  
-        addText("Score:"+score, { 
-          x: 3,
-          y: 2,
-          color: color`3`
-        })
-        addText("A = Addition" , { 
-             x: 1,
-             y: 7,
-             color: color`9`
-        })
-          
-        addText("D = Multiplication" , { 
-             x: 1,
-             y: 9,
-             color: color`9`
-        })
-        
-        loop(count + 1);
+    if (looping) {
+        setTimeout(function() {
+            if (looping) {
+                clearText();
 
-      }
-    }, 1000);
-  
-}
+                no1 = Math.floor(Math.random() * count) + 1;
+                no2 = Math.floor(Math.random() * count) + 1;
+
+                setres(0)
+                var op = Math.floor(Math.random() * 2) + 1;
+                if (op == 1) {
+                    setres(no1 * no2);
+                } else {
+                    setres(no1 + no2);
+
+                }
+                addText(no1 + "_?_" + no2 + "=" + res + "", {
+                    x: 6,
+                    y: 4,
+                    color: color`4`
+                })
+
+                addText("Score:" + score, {
+                    x: 3,
+                    y: 2,
+                    color: color`3`
+                })
+                addText("A = Addition", {
+                    x: 1,
+                    y: 7,
+                    color: color`9`
+                })
+
+                addText("D = Multiplication", {
+                    x: 1,
+                    y: 9,
+                    color: color`9`
+                })
+
+                loop(count + 1);
+
+            }
+        }, 1000);
+
+    }
 }
 
 onInput("a", () => {
-  var rrr = (no1 + no2)
+    if (!isOver) {
+        var rrr = (no1 + no2)
 
-  if(rrr == res) {
-    score++;
-  } else {
-    gameOver();
-  }
-  
+        if (rrr == res) {
+            score++;
+        } else {
+            gameOver();
+        }
+    }
 })
 onInput("d", () => {
-  var rrr = (no1 * no2)
-  if(rrr == res) {
-    score++;
-  } else {
-    gameOver();
-    
-  }
-  
+    if (!isOver) {
+        var rrr = (no1 * no2)
+        if (rrr == res) {
+            score++;
+        } else {
+            gameOver();
+
+        }
+    }
 })
 onInput("s", () => {
-  reset();
-  
+    reset();
+
 })
 loop(2);
