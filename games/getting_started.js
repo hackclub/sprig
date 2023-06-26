63e250c14890e31a5aa319de31b42a97569c00b1
Sam Liu <hello@samliu.dev>
2023-06-25 23:03:04 -0400

    Remove tutorial from js file

--- games/getting_started.js
+++ games/getting_started.js
@@ -3,60 +3,6 @@
 @author: leo, edits: samliu
 */
 
-/*
- ______    _______  _______  ______     _______  __   __  ___   _______ 
-|    _ |  |       ||   _   ||      |   |       ||  | |  ||   | |       |
-|   | ||  |    ___||  |_|  ||  _    |  |_     _||  |_|  ||   | |  _____|
-|   |_||_ |   |___ |       || | |   |    |   |  |       ||   | | |_____ 
-|    __  ||    ___||       || |_|   |    |   |  |       ||   | |_____  |
-|   |  | ||   |___ |   _   ||       |    |   |  |   _   ||   |  _____| |
-|___|  |_||_______||__| |__||______|     |___|  |__| |__||___| |_______|
-
-Welcome to Sprig!!!
-
-Hit "run" to execute the code and
-start the game (you can also press shift+enter)
-Click the "Show Help" to discover your toolkit
-
-The objective is to push the purple boxes onto the green goals
-Press j to reset the current level
-
-To solve each step, you'll have to edit the code
-The code for this game starts below this comment (the brown text)
-
---- Step 1 ---
-Add controls to move up and left, use "w" and "a" as inputs
- * Tip: Add 2 additional "onInput" functions
-
---- Step 2 ---
-Make the purple blocks pushable
- * Tip: Edit "setPushables"
-
---- Step 3---
-Add some new levels.
- * Tip: Make a new map in "levels"
-
---- Step 4 ---
-Allow the player to push two or more boxes in a row
- * Tip: Allow boxes to push boxes
-
---- Step 5---
-Add sound effects when you move.
- * Tip: Create a "tune" and use "playTune"
-
---- Step 6 ---
-Solve the game you just created!
-
---- I'm done, now what? --- 
-Make your own game! Try
- - adding two players
- - leaving a trail as you move
- - having different blocks and goal types
- - come up with your own mechanic!
-*/
-
-// START OF GAME CODE
-
 // define the sprites in our game
 const player = "p";
 const box = "b";
