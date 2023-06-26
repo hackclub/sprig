219e4aed3c48c10c17a952773d483fbf6857d874
Sam Liu <hello@samliu.dev>
2023-06-25 21:30:54 -0400

    Clarification in Getting Started tutorial

--- games/getting_started.js
+++ games/getting_started.js
@@ -1,6 +1,6 @@
 /*
 @title: getting_started
-@author: leo
+@author: leo, edits: samliu
 */
 
 /*
@@ -12,80 +12,58 @@
 |   |  | ||   |___ |   _   ||       |    |   |  |   _   ||   |  _____| |
 |___|  |_||_______||__| |__||______|     |___|  |__| |__||___| |_______|
 
-Instructions:
-
 Welcome to Sprig!!!
 
 Hit "run" to execute the code and
-start the game (you can also press shift+enter).
+start the game (you can also press shift+enter)
+Click the "Show Help" to discover your toolkit
 
-Click the "Show Help" to discover your toolkit.
+The objective is to push the purple boxes onto the green goals
+Press j to reset the current level
 
-The objective is to push the purple boxes onto the green goals.
-Press j to reset the current level.
+To solve each step, you'll have to edit the code
+The code for this game starts below this comment (the brown text)
 
-To beat each level you'll have to edit the code.
-
-The code for this game starts below this comment (the brown text).
-
---------
-Level 1
---------
-
-Make the purple block pushable. 
-
---------
-Level 2
---------
-
+--- Step 1 ---
 Add controls to move up and left, use "w" and "a" as inputs
+ * Tip: Add 2 additional "onInput" functions
 
-Tip: 
-Do you find it annoying restarting at level 0?
-Try adjusting the starting level.
+--- Step 2 ---
+Make the purple blocks pushable
+ * Tip: Edit "setPushables"
 
---------
-Level 3
---------
+--- Step 3---
+Add some new levels.
+ * Tip: Make a new map in "levels"
 
-Edit the map.
+--- Step 4 ---
+Allow the player to push two or more boxes in a row
+ * Tip: Allow boxes to push boxes
 
---------
-Level 4
---------
-
-Make boxes push boxes.
-
---------
-Level 5
---------
-
+--- Step 5---
 Add sound effects when you move.
+ * Tip: Create a "tune" and use "playTune"
 
---------
-Level 6
---------
+--- Step 6 ---
+Solve the game you just created!
 
-Solve the puzzle!
-
---------
-END
---------
-
+--- I'm done, now what? --- 
 Make your own game! Try
  - adding two players
  - leaving a trail as you move
  - having different blocks and goal types
  - come up with your own mechanic!
-
 */
 
+// START OF GAME CODE
 
+// define the sprites in our game
 const player = "p";
 const box = "b";
 const goal = "g";
 const wall = "w";
 
+// assign bitmap art to each sprite
 setLegend(
   [ player, bitmap`
 ................
@@ -157,7 +135,8 @@ setLegend(
 0000000000000000`]
 );
 
-let level = 0;
+// create game levels
+let level = 0; // this tracks the level we are on
 const levels = [
   map`
 p.bg`,
@@ -186,35 +165,38 @@ p.w.
 ..bg`
 ];
 
+// set the map displayed to the current level
 const currentLevel = levels[level];
 setMap(currentLevel);
 
-setSolids([ player, box, wall ]);
+setSolids([ player, box, wall ]); // these blocks cannot be pushed by others
 
+// allow certain sprites to push certain other sprites
 setPushables({
   [player]: []
 });
 
-// START - PLAYER MOVEMENT CONTROLS
-
+// input for player movement control
 onInput("s", () => {
-  getFirst(player).y += 1;
+  getFirst(player).y += 1; // positive y is downwards
 });
 
 onInput("d", () => {
   getFirst(player).x += 1;
 });
 
-// END - PLAYER MOVEMENT CONTROLS
-
+// input to reset level
 onInput("j", () => {
-  const currentLevel = levels[level];
+  const currentLevel = levels[level]; // get the original map of the level
+
+  // make sure the level exists before we load it
   if (currentLevel !== undefined) {
     clearText("");
     setMap(currentLevel);
   }
 });
 
+// These get run after every input
 afterInput(() => {
   // count the number of tiles with goals
   const targetNumber = tilesWith(goal).length;
@@ -222,6 +204,8 @@ afterInput(() => {
   // count the number of tiles with goals and boxes
   const numberCovered = tilesWith(goal, box).length;
 
+  // if the number of goals is the same as the number of goals covered
+  // all goals are covered and we can go to the next level
   if (numberCovered === targetNumber) {
     // increase the current level number
     level = level + 1;
@@ -229,6 +213,8 @@ afterInput(() => {
     const currentLevel = levels[level];
 
     // make sure the level exists and if so set the map
+    // otherwise, we have finished the last level, there is no level
+    // after the last level
     if (currentLevel !== undefined) {
       setMap(currentLevel);
     } else {
