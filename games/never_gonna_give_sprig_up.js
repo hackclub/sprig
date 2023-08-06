34f8c0080450dee5a9f989ca47969f9bf8b02d4b
Lucas <88567622+LucasHT22@users.noreply.github.com>
2023-08-05 23:00:18 -0300

    Create never_gonna_give_sprig_up.js

---
+++ games/never_gonna_give_sprig_up.js
@@ -0,0 +1,56 @@
+/*
+We're no strangers to Sprig
+You know the rules and so do I (do I)
+A full commitment's what I'm thinking of
+You wouldn't get this from any other guy
+I just wanna tell you how I'm feeling
+Gotta make you understand
+Never gonna give Sprig up
+Never gonna let Sprig down
+Never gonna run around and desert Sprig
+Never gonna make Sprig cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt Sprig
+We've known Sprig for so long
+Your heart's been aching, but you're too shy to say it (say it)
+Inside, we both know what's been going on (going on)
+We know the game and we're gonna play it
+And if you ask me how I'm feeling
+Don't tell me you're too blind to see
+Never gonna give Sprig up
+Never gonna let Sprig down
+Never gonna run around and desert Sprig
+Never gonna make Sprig cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt Sprig
+Never gonna give Sprig up
+Never gonna let Sprig down
+Never gonna run around and desert Sprig
+Never gonna make Sprig cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt Sprig
+We've known Sprig for so long
+Your heart's been aching, but you're too shy to say it (to say it)
+Inside, we both know what's been going on (going on)
+We know the game and we're gonna play it
+I just wanna tell you how I'm feeling
+Gotta make you understand
+Never gonna give Sprig up
+Never gonna let Sprig down
+Never gonna run around and desert Sprig
+Never gonna make Sprig cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt Sprig
+Never gonna give Sprig up
+Never gonna let Sprig down
+Never gonna run around and desert Sprig
+Never gonna make Sprig cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt Sprig
+Never gonna give Sprig up
+Never gonna let Sprig down
+Never gonna run around and desert Sprig
+Never gonna make Sprig cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt Sprig
+*/
