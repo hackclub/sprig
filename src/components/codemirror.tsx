b591a04578a9151acd5972d483df77be77846166
JosiasAurel <ndjosiasaurel@gmail.com>
2023-10-19 13:56:35 +0100

    demo change colorscheme

--- src/components/codemirror.tsx
+++ src/components/codemirror.tsx
@@ -1,7 +1,9 @@
-import { createEditorState } from '../lib/codemirror/init'
-import { useEffect, useRef } from 'preact/hooks'
-import { EditorView } from '@codemirror/view'
+import { createEditorState, initialExtensions } from '../lib/codemirror/init'
+import { useEffect, useRef, useState } from 'preact/hooks'
+import { StateEffect } from '@codemirror/state'
 import styles from './codemirror.module.css'
+import { oneDark } from '@codemirror/theme-one-dark'
+import { EditorView } from '@codemirror/view'
 
 interface CodeMirrorProps {
 	class?: string | undefined
@@ -16,24 +18,45 @@ export default function CodeMirror(props: CodeMirrorProps) {
 
 	// Alert the parent to code changes (not reactive)
 	const onCodeChangeRef = useRef(props.onCodeChange)
-	useEffect(() => { onCodeChangeRef.current = props.onCodeChange }, [ props.onCodeChange ])
+	useEffect(() => { onCodeChangeRef.current = props.onCodeChange }, [props.onCodeChange])
 
 	// Run button
 	const onRunShortcutRef = useRef(props.onRunShortcut)
-	useEffect(() => { onRunShortcutRef.current = props.onRunShortcut }, [ props.onRunShortcut ])
+	useEffect(() => { onRunShortcutRef.current = props.onRunShortcut }, [props.onRunShortcut])
 
 	useEffect(() => {
 		if (!parent.current) throw new Error('Oh golly! The editor parent ref is null')
 		let lastCode: string = props.initialCode ?? ''
+
 		const editor = new EditorView({
 			state: createEditorState(props.initialCode ? props.initialCode : '', () => {
 				if (editor.state.doc.toString() === lastCode) return
 				lastCode = editor.state.doc.toString()
 				onCodeChangeRef.current?.()
 			}, () => onRunShortcutRef.current?.()),
-			parent: parent.current
+			parent: parent.current,
 		})
-		
+
+		// serves to restore config before dark mode was added
+		const restoreInitialConfig = () => initialExtensions(() => {
+				if (editor.state.doc.toString() === lastCode) return
+				lastCode = editor.state.doc.toString()
+				onCodeChangeRef.current?.()
+			}, () => onRunShortcutRef.current?.());
+
+		// adds dark mode to the editor
+		setTimeout(() => {
+			editor.dispatch({
+				effects: StateEffect.appendConfig.of(oneDark)
+			});
+		}, 2000);
+
+		setTimeout(() => {
+			editor.dispatch({
+				effects: StateEffect.reconfigure.of(restoreInitialConfig())
+			});
+		}, 5000);
+
 		props.onEditorView?.(editor)
 	}, [])
 
