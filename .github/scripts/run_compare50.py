34933b3a045ea9a66484cfb0335510e9e6230164
NOVA52 <GamingCraft52@gmail.com>
2023-10-27 00:53:48 +0200

    Update run_compare50.py

--- .github/scripts/run_compare50.py
+++ .github/scripts/run_compare50.py
@@ -2,12 +2,6 @@ import subprocess
 import glob
 
 trimmed_files = glob.glob('temp/*.js')
-print("Files to compare:", trimmed_files)
+result = subprocess.run(['compare50', *trimmed_files, 'sprig/games/'], capture_output=True, text=True)
 
-try:
-    result = subprocess.run(['compare50', *trimmed_files, 'sprig/games/'], capture_output=True, text=True, timeout=600)
-    print(result.stdout)
-    if result.stderr:
-        print("Error:", result.stderr)
-except subprocess.TimeoutExpired:
-    print("compare50 took too long to run and was terminated.")
+print(result.stdout)
