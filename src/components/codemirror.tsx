ac0857521c427f2f4d2c36b35b903bef2b16dc71
Lexi Mattick <lexi.mattick@gmail.com>
2023-11-20 11:48:40 -0500

    Add a space after a comma

--- src/components/codemirror.tsx
+++ src/components/codemirror.tsx
@@ -63,7 +63,7 @@ export default function CodeMirror(props: CodeMirrorProps) {
 
 	useEffect(() => {
 		setEditorTheme();
-	}, [isDark.value,editorRef]);
+	}, [isDark.value, editorRef]);
 
 
 	return (
