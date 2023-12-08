0cff07e2314bd9793c6205b36e551f2776f1d76c
AstralTurtle <jamesacs329@gmail.com>
2023-12-08 15:30:04 -0500

    added score and press I to restart

--- games/bomb_dodge.js
+++ games/bomb_dodge.js
@@ -2,6 +2,7 @@
 First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 */
+let score = 0;
 let sounds = [
   tune`
 500,
@@ -12,6 +13,16 @@ let sounds = [
 500: G5/500,
 15000`
 ]
+
+const levels = [
+  map`
+........
+...b....
+...p....
+........
+........
+........`
+]
 let playback;
 const player = "p"
 const bomb = "b";
@@ -20,37 +31,6 @@ var gameRunning = true;
 let nextTime = 5;
 let difficulty = 2.0;
 let soundID = 0;
-var gameLoop = setInterval(() => {
-  clearExplosion();
-  nextTime--;
-  //   addText("" + difficulty,{
-  //   x: 0,
-  //   y: height(),
-  //   color: color`5` 
-  // })
-  addText("Explode in: " + Math.ceil(nextTime),{
-      x: 0,
-      y: 0,
-      color: color`H`
-    })
-
-  if (nextTime <= 0){
-    explode();
-    difficulty += (width() / 100);
-    spawnBombs();
-    nextTime = 7 - difficulty;
-    soundID = 1;
-  }
-  if (playback != null){
-  playback.end();
-  }
-  playback = playTune(sounds[soundID]);
-  soundID = 0;
-
-    
-},1000)
-
-
 
 
 setLegend(
@@ -109,18 +89,10 @@ setLegend(
 
 setSolids([player,bomb,explosion])
 
-let level = 0
-const levels = [
-  map`
-........
-...b....
-...p....
-........
-........
-........`
-]
 
-setMap(levels[level])
+
+
+
 
 setPushables({
   [ player ]: [bomb]
@@ -145,12 +117,13 @@ onInput("d", () => {
 })
 
 onInput("i", () => {
-  explode();
-})
-
-afterInput(() => {
-  
-})
+  if (!gameRunning){
+    clearText();
+    gameLoop = startGame();
+    gameRunning = true;
+  }
+} 
+  )
 
 function explode(){
   getAll(bomb).forEach((bom) => {
@@ -192,6 +165,48 @@ function clearExplosion(){
   getAll(explosion).forEach((exp) => {
     clearTile(exp.x,exp.y)
   } )
+}
+
+var gameLoop = startGame();
+
+
+function startGame(){
+  setMap(levels[0])
+  return setInterval(() => {
+  clearExplosion();
+  nextTime--;
+  //   addText("" + difficulty,{
+  //   x: 0,
+  //   y: height(),
+  //   color: color`5` 
+  // })
+  addText("Explode in: " + Math.ceil(nextTime),{
+      x: 0,
+      y: 0,
+      color: color`H`
+    })
+  addText("Score: " + score,{
+      x: 0,
+      y: 15,
+      color: color`4`
+    })
+
+  if (nextTime <= 0){
+    explode();
+    difficulty += (width() / 100);
+    spawnBombs();
+    nextTime = 7 - difficulty;
+    soundID = 1;
+    score++;
+  }
+  if (playback != null){
+  playback.end();
+  }
+  playback = playTune(sounds[soundID]);
+  soundID = 0;
+  
+    
+},1000)
 }
 
                     
