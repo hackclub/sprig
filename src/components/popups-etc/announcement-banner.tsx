494313182906fe0323f92646235f9ca0700847e1
Sheepy3 <109123711+Sheepy3@users.noreply.github.com>
2023-12-14 15:51:30 -0500

    Update announcement-banner.tsx

--- src/components/popups-etc/announcement-banner.tsx
+++ src/components/popups-etc/announcement-banner.tsx
@@ -5,7 +5,7 @@ export default function AnnouncementBanner() {
 	return (
 		<div class={styles.banner}>
 			<a href="/drop">
-				<img src="https://cloud-bqaqkqg1k-hack-club-bot.vercel.app/0snoman.png" alt="Lil Snowguy"/>
+				<img src="https://cloud-bqaqkqg1k-hack-club-bot.vercel.app/0snoman.png" alt="Lil Snowguy" width="128" height="150" />
 				<p>
 					SHAWN X SPRIG winter case drops dec-15! click here to learn
 					more
