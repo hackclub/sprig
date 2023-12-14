c8e39b7973a6ec7d9b2a14cc83cf107a98c3a362
Sheepy3 <109123711+Sheepy3@users.noreply.github.com>
2023-12-14 16:01:06 -0500

    Update announcement-banner.tsx

--- src/components/popups-etc/announcement-banner.tsx
+++ src/components/popups-etc/announcement-banner.tsx
@@ -1,14 +1,13 @@
 import styles from "./announcement-banner.module.css";
-import { FaGamepad } from "react-icons/fa";
 
 export default function AnnouncementBanner() {
 	return (
 		<div class={styles.banner}>
 			<a href="/drop">
-				<img src="https://cloud-bqaqkqg1k-hack-club-bot.vercel.app/0snoman.png" alt="Lil Snowguy" width="128" height="150" />
+				<img src="https://cloud-bqaqkqg1k-hack-club-bot.vercel.app/0snoman.png" alt="Lil Snowguy" width="64" height="75" />
 				<p>
 					SHAWN X SPRIG winter case drops dec-15! click here to learn
-					more
+					more about how you can get one!
 				</p>
 			</a>
 		</div>
