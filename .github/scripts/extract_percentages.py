755f1b396651159680438702afb731a77b1ee640
NOVA52 <GamingCraft52@gmail.com>
2023-12-16 19:21:34 +0100

    filter plagiarism highest to lowest

--- .github/scripts/extract_percentages.py
+++ .github/scripts/extract_percentages.py
@@ -7,18 +7,27 @@ def extract_similarity_percentage(html_file):
         soup = BeautifulSoup(file, 'html.parser')
         file_name_tag = soup.select_one("#textright > div > h4")
         if file_name_tag:
-            percentage = file_name_tag.find("span", class_="text-secondary small").text.strip("()%")
-            return percentage
+            percentage_text = file_name_tag.find("span", class_="text-secondary small").text.strip("()%")
+            return int(percentage_text)
         else:
             return None
 
-def process_html_files(directory):
+def process_html_files(directory, threshold=10):
+    results = {}
     for filename in os.listdir(directory):
         if filename.endswith(".html"):
             file_path = os.path.join(directory, filename)
             percentage = extract_similarity_percentage(file_path)
-            if percentage:
-                print(f"{filename.replace('.html', '.js')}: {percentage}%")
+            if percentage is not None:
+                results[filename.replace('.html', '.js')] = percentage
+
+    filtered_sorted_results = sorted(
+        ((file, percent) for file, percent in results.items() if percent >= threshold),
+        key=lambda x: x[1], reverse=True
+    )
+
+    for file, percent in filtered_sorted_results:
+        print(f"{file}: {percent}%")
 
 def main():
     if len(sys.argv) != 2:
