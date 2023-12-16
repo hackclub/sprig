9e79a57d8d4743054a5e39560c49699163adb7e8
NOVA52 <GamingCraft52@gmail.com>
2023-12-16 16:10:01 +0100

    fix

--- .github/scripts/plagiarism_check.py
+++ .github/scripts/plagiarism_check.py
@@ -2,18 +2,19 @@ import sys
 import subprocess
 import os
 import glob
+import shutil
 
-def run_compare50(single_file, directory, output_base_dir):
+def run_compare50(single_file, directory, output_dir, saved_dir_base):
     try:
-        
-        all_js_files = glob.glob(os.path.join(directory, "*.js"))
+        directory_abs = os.path.abspath(directory)
+
+        all_js_files = glob.glob(os.path.join(directory_abs, "*.js"))
+        counter = 1
 
         for file in all_js_files:
-            if os.path.abspath(file) == single_file:
+            if os.path.abspath(file) == os.path.abspath(single_file):
                 continue
 
-            output_dir = os.path.join(output_base_dir, os.path.basename(file).replace('.js', ''))
-            
             command = [
                 "compare50",
                 single_file,
@@ -25,7 +26,17 @@ def run_compare50(single_file, directory, output_base_dir):
 
             print("Running Compare50 command:", " ".join(command))
             subprocess.run(command, check=True)
-            print(f"Compare50 results for {file} are saved in {output_dir}")
+
+            saved_dir = os.path.join(saved_dir_base, str(counter))
+            if not os.path.exists(saved_dir):
+                os.makedirs(saved_dir)
+            if os.path.exists(output_dir):
+                shutil.move(output_dir, saved_dir)
+                print(f"Compare50 results for {file} are saved in {saved_dir}")
+                counter += 1
+
+            if os.path.exists(output_dir):
+                shutil.rmtree(output_dir)
 
     except subprocess.CalledProcessError as e:
         print("Error in running Compare50:", e)
@@ -33,15 +44,16 @@ def run_compare50(single_file, directory, output_base_dir):
         print(f"An error occurred: {e}")
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: python plagiarism_check.py <single_file> <directory> <output_base_dir>")
+    if len(sys.argv) != 5:
+        print("Usage: python plagiarism_check.py <single_file> <directory> <output_dir> <saved_dir_base>")
         sys.exit(1)
 
     single_file = sys.argv[1]
     directory = sys.argv[2]
-    output_base_dir = sys.argv[3]
+    output_dir = sys.argv[3]
+    saved_dir_base = sys.argv[4]
 
-    run_compare50(single_file, directory, output_base_dir)
+    run_compare50(single_file, directory, output_dir, saved_dir_base)
 
 if __name__ == "__main__":
     main()
