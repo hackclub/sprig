aa768918bccb8ef84e872ea51208c1ee4ffc08ab
kevindoeschemistry <37723297+kevindoeschemistry@users.noreply.github.com>
2023-12-16 15:37:04 -0800

    Add files via upload

---
+++ games/whispering_raticate.js
@@ -0,0 +1,49 @@
+/*
+First time? Check out the tutorial game:
+https://sprig.hackclub.com/gallery/getting_started
+*/
+
+const player = "p"
+
+setLegend(
+  [ player, bitmap`
+................
+................
+.......000......
+.......0.0......
+......0..0......
+......0...0.0...
+....0003.30.0...
+....0.0...000...
+....0.05550.....
+......0...0.....
+.....0....0.....
+.....0...0......
+......000.......
+......0.0.......
+.....00.00......
+................` ]
+)
+
+setSolids([])
+
+let level = 0
+const levels = [
+  map`
+p.
+..`
+]
+
+setMap(levels[level])
+
+setPushables({
+  [ player ]: []
+})
+
+onInput("s", () => {
+  getFirst(player).y += 1
+})
+
+afterInput(() => {
+  
+})
