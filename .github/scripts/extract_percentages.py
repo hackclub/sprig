ed096cf8f32069036ea534fbcf8b4a757678db10
Shubham Panth <GamingCraft52@gmail.com>
2023-12-17 18:22:18 +0100

    Implemented Automate Plagiarism / Copy Detection (#1285)
    * Implemented Automate Plagiarism / Copy Detection #1177
    
    * Refine PR Workflow Trigger
    
    As suggested by @SamDev-7 in issue #1177 , the filter has been implemented to trigger the PR only when changes occur within the 'games' folder.
    
    * Refine PR Workflow Trigger
    
    As suggested by @SamDev-7 in issue #1177 , the filter has been implemented to trigger the PR only when changes occur within the 'games' folder.
    
    * Auto format + Added Support To Prettier
    
    As @grymmy commented on #1177  , have added support to Prettier and formatted document.
    
    * Implemented fast-diff on PlagarismChecker
    
    as @grymmy talked on #1177 , we are now using package fast-diff to check for Plagiarism. and we also added those packages.
    
    * install dependencies on workflow
    
    * Fix PreprocessCode
    
    * Few fix and try catch
    
    * adding logs to understand the problem
    
    * added async and await
    
    * more async
    
    * Improved PlagiarismChecker
    
    Improved time by only filtering original code, the old one was trimming from gallery code to.
    
    Improved CalculateSimilarity to ignore whitespace and now also show context on what is being flagged (can be used both by sprig reviewer and me for debug)
    
    improved the output file for more context
    
    * Adding Logs For Debbuging
    
    * Added Context
    
    Added a log that shows both % + context meaning/word so we can see which one is being flagged wrong.
    
    * Testing Lowering Threshold
    
    * few improve
    
    * more filter
    
    * more filter?
    
    * Removed fast-diff | Implement line by line check
    
    * Impliment line by line system
    
    * Remove fast-diff , fix string error
    
    * debug
    
    * preprocessCode fixed!
    
    * moving to 0% for debug purpose
    
    * add fast-levenshtein for complex plagarsim
    
    * Implement token based plagarsim
    
    * Implementing Compare50
    
    * checking path dir
    
    * path issue
    
    * compare50 auto creates , making this crash! fixed!
    
    * adding more filter
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * test
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * fix
    
    * test
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * Update plagiarism_check.py
    
    * removing dir before running new one
    
    * add passes to only get information needed
    
    * Extracting usefull data from compare50
    
    * added log as there is no output
    
    * extract data from html
    
    * Update check_plagiarism.yml
    
    * filter plagiarism highest to lowest
    
    * post result after getting result
    
    * name syntax error
    
    * show only filtered result on comment
    
    * Implemented Automate Plagiarism / Copy Detection #1177
    
    * Cleaning PR
    
    * Remove Prettier
    
    * Remove Prettier from yarn.lock

---
+++ .github/scripts/extract_percentages.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import os
+import sys
+
+def extract_similarity_percentage(html_file):
+    with open(html_file, 'r', encoding='utf-8') as file:
+        soup = BeautifulSoup(file, 'html.parser')
+        file_name_tag = soup.select_one("#textright > div > h4")
+        if file_name_tag:
+            percentage_text = file_name_tag.find("span", class_="text-secondary small").text.strip("()%")
+            return int(percentage_text)
+        else:
+            return None
+
+def process_html_files(directory, threshold=10):
+    results = {}
+    for filename in os.listdir(directory):
+        if filename.endswith(".html"):
+            file_path = os.path.join(directory, filename)
+            percentage = extract_similarity_percentage(file_path)
+            if percentage is not None:
+                results[filename.replace('.html', '.js')] = percentage
+
+    filtered_sorted_results = sorted(
+        ((file, percent) for file, percent in results.items() if percent >= threshold),
+        key=lambda x: x[1], reverse=True
+    )
+
+    with open('plagiarism_results.txt', 'w') as output_file:
+        output_file.write("\nFiltered and Sorted Results (Above 10%):\n")
+        for file, percent in filtered_sorted_results:
+            output_file.write(f"{file}: {percent}%\n")
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python extract_percentages.py <saved_dir_path>")
+        sys.exit(1)
+
+    saved_dir_path = sys.argv[1]
+    process_html_files(saved_dir_path)
+
+if __name__ == "__main__":
+    main()
