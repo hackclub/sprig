3a6173ba460c12237d6192af3cf2456f70b0b47b
DevIos01 <GamingCraft52@gmail.com>
2023-12-19 17:19:55 +0100

    Implemented Better Logs For Plagiarism Checker.

--- .github/scripts/plagiarism_check.py
+++ .github/scripts/plagiarism_check.py
@@ -3,11 +3,17 @@ import subprocess
 import os
 import glob
 import shutil
+import time
+
+def log(message):
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{timestamp}] {message}")
 
 def run_compare50(single_file, directory, output_dir, saved_dir_base):
     try:
         if not os.path.exists(saved_dir_base):
             os.makedirs(saved_dir_base)
+            log("Created base directory for saved files.")
 
         all_js_files = glob.glob(os.path.join(directory, "*.js"))
         total_files = len(all_js_files)
@@ -16,11 +22,13 @@ def run_compare50(single_file, directory, output_dir, saved_dir_base):
         for file in all_js_files:
             current_file_number += 1
             if os.path.abspath(file) == os.path.abspath(single_file):
+                log(f"Skipping comparison for the same file: {file}")
                 continue
 
-            print(f"Processing file {current_file_number} of {total_files}: {file}")
+            log(f"Processing file {current_file_number} of {total_files}: {file}")
             if os.path.exists(output_dir):
                 shutil.rmtree(output_dir)
+                log(f"Cleaned existing output directory: {output_dir}")
             
             command = [
                 "compare50",
@@ -32,23 +40,28 @@ def run_compare50(single_file, directory, output_dir, saved_dir_base):
             ]
 
             command_str = ' '.join(command)
+            log(f"Running command: {command_str}")
             subprocess.run(command_str, shell=True, check=True)
+            log("Compare50 command executed successfully.")
 
             match_file = os.path.join(output_dir, "match_1.html")
 
             if os.path.exists(match_file):
                 new_filename = os.path.basename(file).replace('.js', '.html')
                 saved_file_path = os.path.join(saved_dir_base, new_filename)
-                print(f"Moving {match_file} to {saved_file_path}")
+                log(f"Match found. Moving {match_file} to {saved_file_path}")
                 shutil.move(match_file, saved_file_path)
+            else:
+                log(f"No match found for file: {file}")
 
     except subprocess.CalledProcessError as e:
-        print("Error in running Compare50:", e)
+        log(f"Error in running Compare50: {e}")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        log(f"An error occurred: {e}")
 
 def main():
     if len(sys.argv) != 5:
+        log("Incorrect number of arguments provided.")
         print("Usage: python plagiarism_check.py <single_file> <directory> <output_dir> <saved_dir_base>")
         sys.exit(1)
 
@@ -57,17 +70,18 @@ def main():
     output_dir = sys.argv[3]
     saved_dir_base = sys.argv[4]
 
-    print(f"Received arguments:")
-    print(f"Single file: {single_file}")
-    print(f"Directory: {directory}")
-    print(f"Output directory: {output_dir}")
-    print(f"Saved directory base: {saved_dir_base}")
+    log(f"Starting plagiarism check with the following arguments:")
+    log(f"Single file: {single_file}")
+    log(f"Directory: {directory}")
+    log(f"Output directory: {output_dir}")
+    log(f"Saved directory base: {saved_dir_base}")
 
-    print(f"All files in directory '{directory}':")
+    log(f"Listing all JavaScript files in directory '{directory}':")
     for f in glob.glob(os.path.join(directory, "*.js")):
-        print(f)
+        log(f)
 
     run_compare50(single_file, directory, output_dir, saved_dir_base)
+    log("Plagiarism check completed.")
 
 if __name__ == "__main__":
     main()
