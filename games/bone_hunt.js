bd7f266bfa417bce0433adc04dad7b537017a653
Lucas <88567622+LucasHT22@users.noreply.github.com>
2024-01-15 22:27:31 -0300

    add metadata

--- games/bone_hunt.js
+++ games/bone_hunt.js
@@ -2,11 +2,11 @@
 First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
-@title: 
-@author: 
-@tags: []
+@title: bone_hunt
+@author: Andrew Cromar
+@tags: ["puzzle"]
 @img: ""
-@addedOn: 2024-00-00
+@addedOn: 2024-01-15
 */
 
 const player = "p";
