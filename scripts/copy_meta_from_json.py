1a42a50c031ff19952d3b8e40221cd3b9720db9a
GalaxyGamingBoy <xrteach@gmail.com>
2024-01-18 21:51:21 +0000

    docs: Comments

--- scripts/copy_meta_from_json.py
+++ scripts/copy_meta_from_json.py
@@ -1,7 +1,15 @@
+"""
+A python script to copy the metadata from the metadata.json to the actual JS files
+"""
+
 import re, json
 
 # Test if the necessery tags exist
 def test_metadata(game):
+    """
+    Tests each game that is passed to see if a field is found & if yes how many occurences it found
+    """
+
     game_lines = []
     found = {"title": False, "author": False, "img": False, "tags": False, "addedOn": False}
     occurences = {"title": 0, "author": 0, "img": 0, "tags": 0, "addedOn": 0}
@@ -12,6 +20,10 @@ def test_metadata(game):
     
     # Check if tags exist
     for i, l in enumerate(game_lines):
+        """
+        Search each line for any tag using regex, if yes count an occurence and increment found
+        """
+
         if re.findall("@title: (.+)", l):
             found["title"] = True
             occurences["title"] = len(re.findall("@title: (.+)", l))
@@ -34,10 +46,15 @@ def test_metadata(game):
 
 # Replace the game meta
 def embed_metadata(game_metadata, test_results):
+    """
+    Embeds the game metadata according to what is missing from the test result
+    """
+
     game_lines = []
     with open(f"./games/{game_metadata["filename"]}.js") as f:
         game_lines = f.readlines()
     
+    # If no tag is found, the add a new comment at the start of the file
     if (test_results["title"] == False and test_results["author"] == False and test_results["img"] == False and test_results["tags"] == False and test_results["addedOn"] == False):
         game_lines.insert(0, "*/\n\n")
         game_lines.insert(0, "@addedOn: " + game_metadata["addedOn"] + "\n")
@@ -48,8 +65,10 @@ def embed_metadata(game_metadata, test_results):
         game_lines.insert(0, "/*\n")
 
         print(f"[EMBED] {game_metadata["filename"]}: All Metadata Embed")
+    # If all metadata is found, skip
     elif (test_results["title"] == True and test_results["author"] == True and test_results["img"] == True and test_results["tags"] == True and test_results["addedOn"] == True):
         print(f"[EMBED] {game_metadata["filename"]}: No Metadata Embed")
+    # If any of the metadata is missing, insert it into the 2nd line
     else:
         if(test_results["addedOn"] == False): game_lines.insert(1, "@addedOn: " + game_metadata["addedOn"] + "\n")
         if(test_results["img"] == False): game_lines.insert(1, "@img: " + (game_metadata["img"] if game_metadata["img"] else "\"\"") + "\n")
@@ -62,6 +81,7 @@ def embed_metadata(game_metadata, test_results):
     with open(f"./games/{game_metadata["filename"]}.js", "w") as f:
         f.writelines(game_lines)
 
+# Save changes
 with open("./games/metadata.json") as f:
     metadata = json.load(f)
     for game_metadata in metadata:
