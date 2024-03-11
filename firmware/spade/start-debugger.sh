e773df9269a92d4ab455d7dcb61b69f333db5c63
sheepy3 <sheepy680@gmail.com>
2024-03-11 12:34:43 -0400

    start debugger now support linucks

--- firmware/spade/start-debugger.sh
+++ firmware/spade/start-debugger.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+a=`uname`
+if [[ "$a" == "Linux" ]]; then
+	 /usr/bin/gdb-multiarch -ex 'target remote :3333' ./spade.elf 
+else
+	 /opt/homebrew/bin/arm-none-eabi-gdb -ex 'target remote :3333' ./spade.elf 
+fi
 
-/opt/homebrew/bin/arm-none-eabi-gdb -ex 'target remote :3333' ./spade.elf 
+
+
