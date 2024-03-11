2a81ea40f83df90d48a9d195579ded1633427592
sheepy3 <sheepy680@gmail.com>
2024-03-11 14:41:27 -0400

    updated log to work on linux

--- firmware/gardenshed/log.sh
+++ firmware/gardenshed/log.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
-pkill SCREEN
-screen /dev/`ls /dev/ | grep usb | tail -n 1` 115200
+a=`uname`
+if [[ "$a" == "Linux" ]]; then
+    pkill screen
+	 screen /dev/`ls /dev/ | grep ttyACM | tail -n 1` 115200 
+else
+    pkill SCREEN
+	 screen /dev/`ls /dev/ | grep usb | tail -n 1` 115200
+fi
