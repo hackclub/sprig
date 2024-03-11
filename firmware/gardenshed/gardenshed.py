c106e7183cca933034135de40a5e0cecb86277ad
sheepy3 <sheepy680@gmail.com>
2024-03-11 18:05:03 -0400

    gardenshed basic functionality

--- firmware/gardenshed/gardenshed.py
+++ firmware/gardenshed/gardenshed.py
@@ -2,11 +2,28 @@ import argparse
 import subprocess
 import os
 
+def runscript(script):
+    firmware = "spade"  
+    current_dir = os.path.dirname(os.path.realpath(__file__)) # Grabs path to gardenshed directory
+    bash_script_path_gardenshed = os.path.join(current_dir, script)
+    os.chdir(os.path.join(current_dir, "..", firmware))
+    subprocess.run(["bash", os.path.join("..", "gardenshed", script)])
+
 parser = argparse.ArgumentParser()
-parser.add_argument("echo")
+parser.add_argument("command",type=str, help="log, enable-gdb,")
+parser.add_argument("-f","--flash", help="flash device after completing action",action="store_true")
+
 args = parser.parse_args()
-print(args.echo)
+
+#print(args.echo)
+
+runscript(args.command+".sh")
+
+if args.flash:
+    runscript("reflash.sh")
 
 
 
+
+ 
 
