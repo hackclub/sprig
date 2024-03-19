cdf9937c14939063e0a17c1daafc35771ed61267
sheepy3 <sheepy680@gmail.com>
2024-03-19 13:36:44 -0400

    moved gardenshed to scripts and updated code appropriately

--- firmware/gardenshed/log.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-a=`uname`
-if [[ "$a" == "Linux" ]]; then
-    pkill screen
-	 screen /dev/`ls /dev/ | grep ttyACM | tail -n 1` 115200 
-else
-    pkill SCREEN
-	 screen /dev/`ls /dev/ | grep usb | tail -n 1` 115200
-fi
