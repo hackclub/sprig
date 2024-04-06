9d8b0beeaf31186a922229f28d586fd69085c608
GalaxyGamingBoy <xrteach@gmail.com>
2024-04-06 16:28:55 +0000

    revert formating

--- games/dino-game.js
+++ games/dino-game.js
@@ -99,7 +99,7 @@ setLegend(
 000.0000000.0000
 ..........000...`]
 )
-
+  
 var runmap = map`
 ........
 dggggggg`
@@ -125,116 +125,116 @@ var score = 0
 var waiting = false
 
 
-function do_jump() {
-  if (jump == 1) {
-    return
-  } else {
-    if (top_map[0] == raptor) {
-      game_over()
+function do_jump(){
+    if(jump == 1){
+        return
+    }else{
+        if(top_map[0] == raptor){
+            game_over()
+        }
+        if(getFirst(dino).y){
+          getFirst(dino).y -= 1
+          jump = 1
+          jump_timer = setTimeout(end_jump, jump_time);
+        }
     }
-    if (getFirst(dino).y) {
-      getFirst(dino).y -= 1
-      jump = 1
-      jump_timer = setTimeout(end_jump, jump_time);
-    }
-  }
 }
-
-function end_jump() {
-  if (bot_map[0] != ground) {
-    game_over()
-  }
-  if (getFirst(dino).y == 0) { // ==0 bc otherwise it'll be false
-    getFirst(dino).y += 1
-    jump = 0
-  }
+  
+function end_jump(){
+        if(bot_map[0] != ground){
+            game_over()
+        }
+        if(getFirst(dino).y == 0){ // ==0 bc otherwise it'll be false
+          getFirst(dino).y += 1
+          jump = 0
+        }
 }
-
+  
 onInput("w", () => {
-  if (playing == 0 && !waiting) {
-    playing = 1
-    addText("                   ", {
-      x: 0,
-      y: 6,
-      color: color`0`
-    });
-    top_map = "........"
-    bot_map = "gggggggg"
-    runmap = top_map + "\n" + bot_map
-    setMap(runmap)
-    addSprite(0, 1, dino)
-    jump = 0
-    tick()
-  } else if (!waiting) {
-    do_jump()
-  }
+    if(playing == 0 && !waiting){
+      playing = 1
+      addText("                   ", {
+          x: 0,
+          y: 6,
+          color: color`0`
+      });
+      top_map = "........"
+      bot_map = "gggggggg"  
+      runmap = top_map + "\n" + bot_map
+      setMap(runmap)
+      addSprite(0, 1, dino)
+      jump = 0
+      tick()
+    }else if(!waiting){
+      do_jump()
+    } 
 })
 
-function tick() {
-  top_next = "."
-  switch (last) {
-    case ground:
-      var options = [ground, ground, ground, ground, ground, ground, cactus1, cactus2, raptor]
-      next = options[Math.floor(Math.random() * options.length)]
-      last = next
-      if (next == raptor) {
-        top_next = raptor
-        next = ground
-      }
-      break;
-    default:
-      next = ground
-      last = next
-      break;
-  }
-  top_map = top_map.slice(1) + top_next
-  bot_map = bot_map.slice(1) + next
-  runmap = top_map + "\n" + bot_map
-  setMap(runmap)
-  if (jump == 0) {
-    if (bot_map[0] != ground) {
-      game_over()
-      return
+function tick(){
+    top_next = "."
+    switch (last) {
+        case ground:
+            var options = [ground, ground, ground, ground, ground, ground, cactus1, cactus2, raptor]
+            next = options[Math.floor(Math.random() * options.length)]
+            last = next
+            if(next == raptor){
+                top_next = raptor
+                next = ground
+            }
+            break;
+        default:
+            next = ground
+            last = next
+            break;
     }
-    addSprite(0, 1, dino)
-  } else {
-    if (top_map[0] == raptor) {
-      game_over()
-      return
+    top_map = top_map.slice(1) + top_next
+    bot_map = bot_map.slice(1) + next
+    runmap = top_map + "\n" + bot_map
+    setMap(runmap)
+    if(jump == 0){
+        if(bot_map[0] != ground){
+          game_over()
+          return
+        }
+        addSprite(0, 1, dino)
+    }else{
+        if(top_map[0] == raptor){
+          game_over()
+          return
+        }
+        addSprite(0, 0, dino)
     }
-    addSprite(0, 0, dino)
-  }
-  if (time > min_time) {
-    time -= 10
-    jump_time -= 10
-  }
-  score += const_time - time
-  addText(String(1000000 + score).slice(1), {
-    x: 14,
-    y: 6,
-    color: color`0`
-  });
-  tick_timer = setTimeout(tick, time)
+    if(time > min_time){
+      time -= 10
+      jump_time -= 10
+    }
+    score += const_time - time
+    addText(String(1000000+score).slice(1), {
+        x: 14,
+        y: 6,
+        color: color`0`
+    });
+    tick_timer = setTimeout(tick, time)
 }
 
-function game_over() {
-  clearTimeout(tick_timer)
-  clearTimeout(jump_timer)
-  playing = 0
-  score = 0
-  time = const_time
-  jump_time = const_jump_time
-  addText("Game Over", {
-    x: 0,
-    y: 6,
-    color: color`0`
-  });
-  waiting = true
-  setTimeout(() => { waiting = false }, 2000)
+function game_over(){
+    clearTimeout(tick_timer)
+    clearTimeout(jump_timer)
+    playing = 0
+    score = 0
+    time = const_time
+    jump_time = const_jump_time
+    addText("Game Over", {
+        x: 0,
+        y: 6,
+        color: color`0`
+    });
+    waiting = true
+    setTimeout(()=>{waiting = false}, 2000)
 }
 
 addText("Press W to start", {
-  x: 0,
-  y: 6,
-  color: color`0`
+    x: 0,
+    y: 6,
+    color: color`0`
 });
