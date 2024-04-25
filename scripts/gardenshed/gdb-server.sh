a3fa27a3f08a7e07fdf50a82e653d0fbe2fb8e94
Cheru Berhanu <cheru@cheru.dev>
2024-04-25 14:36:06 -0400

    Update gdb-server.sh

--- scripts/gardenshed/gdb-server.sh
+++ scripts/gardenshed/gdb-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "bindto 0.0.0.0" -c "program ./spade.elf verify reset"
+sudo openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program ./spade.elf verify reset"
