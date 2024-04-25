cae567cb6f87cb0adc8b16480444edb1ccdc2d00
Cheru Berhanu <cheru@cheru.dev>
2024-04-25 14:22:15 -0400

    run gdb client in docker instead of host this gives gdb access to the full source tree for debugging

--- scripts/gardenshed/gdb-client.sh
+++ scripts/gardenshed/gdb-client.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
-a=`uname`
-if [[ "$a" == "Linux" ]]; then
-	 /usr/bin/gdb-multiarch -ex 'target remote :3333' ./spade.elf 
+
+dependencies=("docker" "echo" "whoami" "grep")
+
+for dep in "${dependencies[@]}"; do
+    if ! command -v $dep > /dev/null; then
+        printf "%s not found, please install %s\n" "$dep" "$dep"
+        exit 1
+    fi
+done
+
+echo "Dependencies found successfully"
+
+# Ensure user in docker group if on linux
+if id -nG $(whoami) | grep -qw "docker"; then
+    echo User in docker group, continuing
 else
-	 /opt/homebrew/bin/arm-none-eabi-gdb -ex 'target remote :3333' ./spade.elf 
+    if [[ $OSTYPE != *"linux"* ]]; then
+        echo "Platform is not linux, skipping docker group check"
+    else
+        echo User $(whoami) not in the docker group. Please add user $(whoami) to the docker group and try again
+        exit 1
+    fi
 fi
 
+IP=$(docker network inspect bridge -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}')
 
-
+case $1 in
+    "--log") docker build ./docker | tee dockerBuildLog.txt
+             docker run -it --security-opt label:disable --rm --volume `pwd`:/root/spade $(docker images | awk '{print $3}' | awk 'NR==2') /usr/bin/gdb-multiarch -ex "target remote $IP:3333" /root/spade/spade.elf | tee -a dockerBuildLog.txt ;;
+    *) docker build ./docker
+       docker run -it --security-opt label:disable --rm --volume `pwd`:/root/spade $(docker images | awk '{print $3}' | awk 'NR==2') /usr/bin/gdb-multiarch -ex "target remote $IP:3333" /root/spade/spade.elf ;;
+esac
