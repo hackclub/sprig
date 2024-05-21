181c9112238f9f99468eccb6e35a5ffba6bacc2d
graham <graham@hackclub.com>
2024-05-20 20:18:01 -0400

    Update MusicGarden.js

--- games/MusicGarden.js
+++ games/MusicGarden.js
@@ -1,12 +1,9 @@
 /*
-First time? Check out the tutorial game:
-https://sprig.hackclub.com/gallery/getting_started
-
-@title: 
-@author: 
+@title: Music Garden
+@author: kubits
 @tags: []
 @img: ""
-@addedOn: 2024-00-00
+@addedOn: 2024-05-20
 */
 
 /* 
