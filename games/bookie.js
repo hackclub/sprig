0982ab949fe025692ee4779ff93fdd113769cfa7
leomcelroy <leomcelroy@gmail.com>
2024-05-20 23:23:28 -0400

    Update bookie.js

--- games/bookie.js
+++ games/bookie.js
@@ -1,12 +1,9 @@
 /*
-First time? Check out the tutorial game:
-https://sprig.hackclub.com/gallery/getting_started
-
-@title: 
-@author: 
+@title: Fruit Picking
+@author: Emma
 @tags: []
 @img: ""
-@addedOn: 2024-00-00
+@addedOn: 2024-05-20
 */
 
 const player = "p"
