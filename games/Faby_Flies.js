837e2d6481d1b580927dcc08c1fbde661557ceea
Eny <69588272+EnyCode@users.noreply.github.com>
2024-06-22 12:43:52 +0100

    Update Faby_Flies.js

--- games/Faby_Flies.js
+++ games/Faby_Flies.js
@@ -2,10 +2,10 @@
 First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
-@title: 
-@author: 
-@tags: []
-@addedOn: 2024-00-00
+@title: Faby Flies
+@author: Eny 
+@tags: ['classic', 'endless']
+@addedOn: 2024-06-22
 */
 
 const player = "p"
