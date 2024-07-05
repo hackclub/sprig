16f8206c6d80817f5316ef71feef7d3cc951f4c0
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-05 16:56:49 -0400

    Update to v1.1

--- games/Slimer.js
+++ games/Slimer.js
@@ -5,6 +5,7 @@ Slimer
 @tags: []
 @addedOn: 2024-07-04
 */
+
 const player = "p"
 const goal = "g"
 const winnertext = "w"
@@ -21,6 +22,9 @@ const gkeyDoor = "q"
 const crate = "c"
 const crateButton = "j"
 const crateWall = "v"
+
+const doubleStepPowerup = "n"
+const depower = "z"
 
 const obstacles = [wall, bkeyDoor, rkeyDoor, gkeyDoor, crateWall]
 
@@ -59,23 +63,6 @@ setLegend(
 ..666444444666..
 ...6666666666...
 ....66666666....`], 
-  [ winnertext, bitmap`
-................
-.0.....0........
-.0.....0.0......
-.0.....0...000..
-.0..0..0.0.0.0..
-..00.00..0.0.0..
-................
-................
-................
-................
-................
-................
-................
-................
-................
-................`],
   [ wall, bitmap`
 FFFFFFFFFFFFFFFF
 FFFFFFFFFFFFFFFF
@@ -245,8 +232,43 @@ FFFFFFFFFFFFFFFF`],
 ...9999999999...
 ...9999999999...
 ...9999999999...
-...9999999999...`]
-  )
+...9999999999...`],
+  [ doubleStepPowerup, bitmap`
+................
+................
+................
+........6.......
+........6.......
+.......66.......
+.......66.......
+......6666......
+..66666666666...
+...6666666666...
+....6666666.....
+....666666......
+...66666666.....
+..666...666.....
+..........6.....
+................`],
+  [ depower, bitmap`
+....33333333....
+...3333333333...
+..233333333332..
+.33233333333233.
+3333233333323333
+3333323333233333
+3333332332333333
+3333333223333333
+3333333223333333
+3333332332333333
+3333323333233333
+3333233333323333
+.33233333333233.
+..233333333332..
+...3333333333...
+....33333333....`]
+)
+
 
 setSolids([...obstacles])
 
@@ -339,11 +361,64 @@ gdqc...kc.
 .dqc....c.
 .dqccccccb
 .dqc....c.
-.dqc.p..c.`
+.dqc.p..c.`,
+    map`
+..abjd..vn
+..a..d..v.
+..a..d..v.
+..a..dc.v.
+g.ap.d..v.`,
+    map`
+gd.......v....
+.d.......v....
+.d.......v.n..
+.d.......v....
+.dp..c...v....
+.d..aaa..v....
+.d..aba.jv....
+.d..aaa..v....
+.d.......v....
+.d.......v....`,
+  map`
+gaq.......v..dn
+.aq.......v.zd.
+.aq...c...v..d.
+.aq.......v..d.
+.aq.....j.v..d.
+.aq.......v.bd.
+.aq.......v..d.
+.aq.......v..d.
+.aq.......v..d.
+.aq....p..v..dk`,
+  map`
+g..ad.anq....
+aaaad.a.q...b
+....d.a.q.aaa
+....d.a.q....
+....d.a.q....
+....d.a.q....
+....dka.q....
+.p..d.a.q..p.`,
+  map`
+...asb...dn
+.c.as....d.
+...as....d.
+jp.as....d.
+aaaas...pd.
+.aavs.aaaaa
+gaavs.a....`,
+  map`
+pa..v.ar.ap
+.a..v.a..a.
+.s..v.a....
+.s..v.a.caj
+.s..v.a....
+.a..v.a..a.
+pa..vga..ap`
   ]
 
 const win = map`
-..w..
+.....
 ..p..
 .....
 .....`
@@ -359,29 +434,34 @@ onInput("w", () => movePlayer(0, -1))
 onInput("s", () => movePlayer(0, 1))
 onInput("a", () => movePlayer(-1, 0))
 onInput("d", () => movePlayer(1, 0))
-
+var activePowerup = "None"
 afterInput(() =>{
   if(!getFirst(player) || !getFirst(goal)) return;
   if(getFirst(player).x === getFirst(goal).x && getFirst(player).y == getFirst(goal).y){
     level++
+    activePowerup = "None"
     if (level < levels.length) {
       setMap(levels[level])
     } else {
       setMap(win)
     }
   }
-  console.log(Date.now() - d)
 })
 
 function movePlayer(dx, dy) {
-  console.log(dx, dy)
-    const playerSprite = getFirst(player)
-    const targetX = playerSprite.x + dx
-    const targetY = playerSprite.y + dy
+  if(activePowerup == "DoubleMove"){ 
+
+    dx = dx * 2
+    dy = dy * 2
+  }
+
+  getAll(player).forEach((playerSprite) => {
+    var targetX = playerSprite.x + dx
+    var targetY = playerSprite.y + dy
     
     if (!isObstacle(targetX, targetY)) {
         const targetTileSprites = getTile(targetX, targetY)
-  
+
         const crateIndex = targetTileSprites.findIndex(sprite => sprite.type === crate)
         
         if (crateIndex !== -1) {
@@ -396,19 +476,34 @@ function movePlayer(dx, dy) {
 
             const button = getTile(targetTileSprites[crateIndex].x, targetTileSprites[crateIndex].y)
             if(button.some(sprite => sprite.type == crateButton)){
-                targetTileSprites[crateIndex].remove()
+                //targetTileSprites[crateIndex].remove()
                 getAll(crateWall).forEach(wall => {
-                    wall.remove()
+                  wall.remove()
                 })
             }
           }
         } else {
-          if (!isObstacle(targetX, targetY)) {
+          if(!isObstacle(targetX, targetY)) {
+            if(getTile(targetX, targetY).find(sprite => sprite.type === "n")){
+              activePowerup = "DoubleMove"
+              console.log("set powerup")
+              getTile(targetX, targetY).find(sprite => sprite.type === "n").remove()
+            } else if (getTile(targetX, targetY).find(sprite => sprite.type === depower)){
+              getTile(targetX, targetY).find(sprite => sprite.type === depower).remove()
+              activePowerup = "None"
+            }
+
+            if(targetX < 0) targetX = 0
+            if(targetX > width()) targetX = width()
+
+            if(targetY > height()) targetY = height()
+            if(targetY < 0) targetY = 0
+
             playerSprite.x = targetX
             playerSprite.y = targetY
           }
         }
-  
+
       const blueKey = getTile(targetX, targetY).find(sprite => sprite.type === bkey)
       const redKey = getTile(targetX, targetY).find(sprite => sprite.type === rkey)
       const greenKey = getTile(targetX, targetY).find(sprite => sprite.type == gkey)
@@ -437,7 +532,9 @@ function movePlayer(dx, dy) {
         })
       }
     }
-  }
+  })
+  
+}
   
 
 function isObstacle(x, y) {
