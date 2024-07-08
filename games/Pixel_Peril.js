554a0f0151abc3040de1067f38440554bd60714a
Marios Mitsios <xrteach@gmail.com>
2024-07-08 13:28:05 -0400

    Fix metadata in Pixel_Peril.js

--- games/Pixel_Peril.js
+++ games/Pixel_Peril.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Pixel Peril
-@author: 
+@author: Outdatedcandy92
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-07-08
 */
 
 const player = "p"
