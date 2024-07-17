806816f741eb7d5fe14e7c372035d7253d17f2a8
Ayaan Grover <ayaangrover@gmail.com>
2024-07-16 20:37:50 -0700

    Update candyshopper.js

--- games/candyshopper.js
+++ games/candyshopper.js
@@ -2,7 +2,7 @@
 @title: candyshopper
 @author: ayaangrover
 @tags: ["puzzle"]
-@addedOn: 2024-07-15
+@addedOn: 2024-07-16
 */
 
 const player = "p"
@@ -11,6 +11,58 @@ const chocolate = "c"
 const candycane = "a"
 const goal = "g";
 const wall = "w";
+const badjuice = "b";
+const stepForwardTune = tune`
+127.29844413012731,
+42.432814710042436: C4^42.432814710042436,
+1188.1188118811883`; 
+const resetTune = tune`
+161.29032258064515,
+161.29032258064515: E5/161.29032258064515,
+161.29032258064515: D5/161.29032258064515,
+161.29032258064515: C5/161.29032258064515,
+161.29032258064515: B4/161.29032258064515,
+161.29032258064515: A4/161.29032258064515,
+161.29032258064515: A4/161.29032258064515,
+161.29032258064515: A4/161.29032258064515,
+3870.967741935484`; 
+const winTune = tune`
+100.67114093959732: C4/100.67114093959732,
+100.67114093959732: F4/100.67114093959732,
+100.67114093959732: B4/100.67114093959732,
+100.67114093959732: E5/100.67114093959732,
+100.67114093959732: A5/100.67114093959732,
+100.67114093959732: A5/100.67114093959732,
+100.67114093959732: A5/100.67114093959732,
+100.67114093959732: G5/100.67114093959732,
+100.67114093959732: F5/100.67114093959732,
+100.67114093959732: E5/100.67114093959732,
+100.67114093959732: C4/100.67114093959732,
+100.67114093959732: F4/100.67114093959732,
+100.67114093959732: B4/100.67114093959732,
+100.67114093959732: E5/100.67114093959732,
+100.67114093959732: A5/100.67114093959732,
+100.67114093959732: A5/100.67114093959732,
+100.67114093959732: A5/100.67114093959732,
+100.67114093959732: G5/100.67114093959732,
+100.67114093959732: F5/100.67114093959732,
+100.67114093959732: E5/100.67114093959732,
+100.67114093959732: D5/100.67114093959732,
+1107.3825503355706`; 
+const levelTune = tune`
+53.475935828877006: A4-53.475935828877006,
+53.475935828877006: G4-53.475935828877006,
+53.475935828877006: A4-53.475935828877006,
+53.475935828877006: B4-53.475935828877006,
+53.475935828877006: C5-53.475935828877006,
+53.475935828877006: D5-53.475935828877006,
+53.475935828877006: E5-53.475935828877006,
+53.475935828877006: F5-53.475935828877006,
+53.475935828877006: G5-53.475935828877006,
+53.475935828877006: A5-53.475935828877006,
+53.475935828877006: B5-53.475935828877006,
+1122.9946524064171`; 
+
 
 setLegend(
   /* [ player, bitmap`
@@ -33,13 +85,13 @@ setLegend(
   [player, bitmap`
 ................
 ....00000000....
-..000......000..
-.00..........00.
-.0..0......0..0.
-.0....0..0....0.
-.0....0000....0.
-.00..........00.
-..000......000..
+..000222222000..
+.00222222222200.
+.02202222220220.
+.02222022022220.
+.02222000022220.
+.00222222222200.
+..000222222000..
 ....00000000....
 ....0......0....
 ....0......0....
@@ -81,6 +133,23 @@ setLegend(
 ....3333333.....
 ....3333333.....
 ....3333333.....`],
+  [badjuice, bitmap`
+................
+....1111........
+.......1........
+.......1........
+...33333333333..
+...32222222223..
+...32232223223..
+...32223232223..
+...32222322223..
+...32332223323..
+...32332223323..
+...32222222223..
+...32322222323..
+...32333333323..
+...32222222223..
+...33333333333..`],
   [candycane, bitmap`
 ................
 ................
@@ -138,11 +207,11 @@ LLLLLLLLLLLLLLL.
 let level = 0
 const levels = [
   map`
-p..
-..j
-..g`,
+p...
+...j
+b..g`,
   map`
-p..jg
+pb.jg
 .....
 j.c..
 .a...
@@ -151,46 +220,40 @@ g.g.g`,
 p..
 ww.
 ...
-.ww
+.wb
 ...
-ww.
+bw.
 ...
-.ww
+.wb
 ...
-ww.
+bw.
 gj.
-www`,
+wwb`,
   map`
-.p.
+bpb
 ...
 acj
 ggg`,
   map`
-w.p..
-wj.a.
-gj.cg
-wgwgw
-wwwww`,
+wp..bw
+wj.aww
+gj.cgw
+wgwgww
+wwwwww`,
   map`
 ...............gw
-.a.............ww
+.a............bww
 ..a............gw
 ...a..........gww
 ....a.........pgw
 .....a........gww
-......a........gw
-.......a.......ww
+......ab.......gw
+.......a......bww
 ...............gw`,
   map`
-p.g..
-.....
-.g..
-.c..a
-.....`,
-  map`
-p..w......www...w.w
-ww.wwww.w..ww.w.w.w
-....w...w.www.w.w.w
+p..wwwwwwwwwwwwwwww
+ww.wwww.w..ww...www
+....w...w.www.w.www
 .ww...w.w.www.w...w
 .www.ww.wwwww.w.w.w
 .w.w..w...w...w.w.w
@@ -198,29 +261,13 @@ ww.wwww.w..ww.w.w.w
 .w.w.w..w...w.w.w.w
 .w.w.w.wwwwww.w.w.w
 wwww.w.w...ww.w.w.w
-.....w...w.ww.www.w
-.wwwww.w.w.ww.www.w
-.www.w.w.wwww.www.w
+.....w...w.w..w.w.w
+.wwwww.w.w.ww.w.www
+.www.w.w.wwww.w...w
 .....w.w...ww.www.w
 wwwwww.www.w..w...w
 w......w.w.wwww.www
-wwwwww...w....w.jg.`,
-  map`
-..wwgwgwgwgwgwgwgwgwg
-pa.wa.j.j.j.j.j.j.j.j
-w..w.w.w.w.w.w.w.w.w.
-w..w.w.w.w.w.w.w.w.w.
-g..w.................
-w..ww.wwwwwwwwwwwwwww
-w..ww.wgwgwgwgwgwgwgw
-w..ww.wcwcwcwcwcwcwcw
-w..ww.w.w.w.w.w.w.w.w
-w..ww.w.w.w.w.w.w.w.w
-w..ww.w.w.w.w.w.w.w.w
-w..ww.w.w.w.w.w.w.w.w
-w..ww.w.w.w.w.w.w.w.w
-wa.ww.w.w.w.w.w.w.w.w
-wg..................wdd`
+wwwwww...w....w.jg.`
 ]
 
 const currentLevel = levels[level];
@@ -236,17 +283,24 @@ setPushables({
 })
 
 onInput("w", () => {
-  getFirst(player).y -= 1
-})
+  getFirst(player).y -= 1;
+  playTune(stepForwardTune); 
+});
+
 onInput("s", () => {
-  getFirst(player).y += 1
-})
+  getFirst(player).y += 1;
+  playTune(stepForwardTune); 
+});
+
 onInput("a", () => {
-  getFirst(player).x -= 1
-})
+  getFirst(player).x -= 1;
+  playTune(stepForwardTune); 
+});
+
 onInput("d", () => {
-  getFirst(player).x += 1
-})
+  getFirst(player).x += 1;
+  playTune(stepForwardTune); 
+});
 
 onInput("j", () => {
   const currentLevel = levels[level];
@@ -254,12 +308,25 @@ onInput("j", () => {
   if (currentLevel !== undefined) {
     clearText("");
     setMap(currentLevel);
+    playTune(resetTune); 
   }
 });
 
 afterInput(() => {
+  const playerSprite = getFirst(player);
+  const badJuiceTiles = tilesWith(badjuice);
+
+  // Check if player is touching badjuice
+  const fails = tilesWith(player, badjuice).length + tilesWith(juice, badjuice).length + tilesWith(chocolate, badjuice).length + tilesWith(candycane, badjuice).length ;
+  if (fails>=1) {
+    playTune(resetTune);
+    level = 0;
+    setMap(levels[level]);
+  }
+
   const baskets = tilesWith(goal).length;
 
+  // Check if the player has completed the level
   const successes = tilesWith(goal, juice).length + tilesWith(goal, chocolate).length + tilesWith(goal, candycane).length;
   if (successes === baskets) {
     level = level + 1;
@@ -268,8 +335,10 @@ afterInput(() => {
 
     if (currentLevel !== undefined) {
       setMap(currentLevel);
+      playTune(levelTune);
     } else {
       addText("game over");
+      playTune(winTune);
     }
   }
 });
