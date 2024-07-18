531531faf4e377901e8890c30ec9a76c657a28a7
Marios Mitsios <xrteach@gmail.com>
2024-07-18 15:50:41 -0400

    Update metadata for sokobanlike.js

--- games/sokobanlike.js
+++ games/sokobanlike.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: sokobanlike
-@author: 
-@tags: []
-@addedOn: 2024-00-00
+@author: Neptune1190
+@tags: ["puzzle"]
+@addedOn: 2024-07-18
 */
 
 const player = "p"
