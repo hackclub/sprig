01152a9afb687085267e313e24c6b070ae47252a
JosiasAurel <ndjosiasaurel@gmail.com>
2024-07-18 17:03:04 +0100

    same changes as in prev?

--- games/tictactoe.js
+++ games/tictactoe.js
@@ -1,267 +1,288 @@
 /*
-First time? Check out the tutorial game:
-https://sprig.hackclub.com/gallery/getting_started
+@title: 2 player tic-tac-toe
+@tags: ['classic', 'multiplayer']
+@addedOn: 2022-09-06
+@author: reesericci
 
-@title: TicTacToe
-@author: Benjamin
-@tags: ['tictactoe','tic-tac-toe']
-@addedOn: 2024-07-01
+"a", "s", and "d" are your controls.
+
+You push a combo of those keys to control where to place your piece.
+
+First you use "a", "s", or "d" to control your X position, then do the same thing to place your Y position. You have just placed a piece!
+
+
 */
 
-const player = "p";
-const x = "x";
-const o = "o";
-const background = "b";
-let turn = false;
-let board = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""]
-];
-gameover = false;
-
-setLegend(
-  [player, bitmap`
-................
-................
-................
-................
-................
-.....000000.....
-.....0....0.....
-.....0.00.0.....
-.....0.00.0.....
-.....0....0.....
-.....000000.....
-................
-................
-................
-................
-................`],
-  [x, bitmap`
-................
-................
-................
-................
-................
-.....3....3.....
-......3..3......
-.......33.......
-.......33.......
-......3..3......
-.....3....3.....
-................
-................
-................
-................
-................`],
-  [o, bitmap`
-................
-................
-................
-................
-................
-.....555555.....
-.....5....5.....
-.....5....5.....
-.....5....5.....
-.....5....5.....
-.....555555.....
-................
-................
-................
-................
-................`],
-  [background, bitmap`
-0000000000000000
-0..............0
-0..............0
-0..............0
-0..............0
-0..............0
-0..............0
-0..............0
-0..............0
-0..............0
-0..............0
-0..............0
-0..............0
-0..............0
-0..............0
-0000000000000000`]
-);
-
-
-let level = map`
-...
-.p.
-...`;
-
-setMap(level);
-
-setSolids([x, o]);
-setBackground(background);
-
-function gameOver(winner) {
-  gameover = true
-  if (winner == "x") {
-    addText(`Game over. X won!`, {
-      x: 2,
-      y: 6,
-      color: color`3`
-    })
-  } else if (winner == "o") {
-    addText(`Game over. O won!`, {
-      x: 2,
-      y: 6,
-      color: color`5`
-    })
-  } else {
-    addText(`Game over.`, {
-      x: 4,
-      y: 6,
-      color: color`9`
-    })
-    addText(`It was a draw!`, {
-      x: 3,
-      y: 8,
-      color: color`9`
-    })
+class Tile {
+	constructor(x, y) {
+	  this._x = x;
+	  this._y = y;
+	}
+  
+	get state() {
+		return this.updateState();
+	}
+	
+	get x() {
+	  return this._x;
+	}
+	
+	get y() {
+	  return this._y;
+	}
+  
+	updateState() {
+	  const sprigTile = getTile(this.x,this.y)
+	  if(finished == true) return;
+	  if(sprigTile.length == 1 && sprigTile[0].type == "t") {
+		return null;
+	  } else {
+		return sprigTile[1].type;
+	  }
+	}
+	
+	addX() {
+	  if(finished == true) return
+	  if(!this.state) {
+		addSprite(this.x, this.y, "x");
+		return;
+	  }
+	  throw new Error("taken")
+	}
+  
+	addO() {
+	  if(finished == true) return
+	  if(!this.state) {
+		addSprite(this.x, this.y, "o");
+		return;
+	  }
+	  throw new Error("taken")
+	}
+  
   }
-  addText(`Press J to restart`, {
-    x: 1,
-    y: 10,
-    color: color`D`
+  
+  let finished = false;
+  
+  function checkForWinner(tiles) {
+	for(let i = 0; i < 2; i++) {
+	  let row = tiles[i];
+  
+	  if(row[0].state == null || row[1].state == null || row[2].state == null) continue;
+	  if(
+		row[0].state == row[1].state && 
+		row[0].state == row[2].state &&
+		row[1].state == row[2].state
+	  ) {
+		winner(row[0].state)
+		break;
+	  }
+	  
+	}
+  
+	for(let i = 0; i < 2; i++) {
+	  if(tiles[0][i].state == null || tiles[1][i].state == null || tiles[2][i].state == null) continue;
+	  
+	  if(tiles[0][i].state == tiles[1][i].state &&
+		 tiles[0][i].state == tiles[2][i].state &&
+		 tiles[1][i].state == tiles[2][i].state
+	  ) {
+		winner(tiles[2][i].state)
+		return;
+	  }
+	}
+  
+	function checkLeftDiagonal() {
+	  if(tiles[0][0].state == tiles[1][1].state &&
+		 tiles[0][0].state == tiles[2][2].state &&
+		 tiles[1][1].state == tiles[2][2].state
+	  ) {
+		return tiles[0][0].state;
+	  }
+	}
+  
+	function checkRightDiagonal() {
+	  if(tiles[2][0].state == tiles[1][1].state &&
+		 tiles[2][0].state == tiles[0][2].state &&
+		 tiles[1][1].state == tiles[0][2].state
+	  ) {
+		return tiles[2][0].state;
+	  }
+	}
+	
+	if(!(tiles[0][0].state == null || tiles[1][1].state == null || tiles[2][2].state == null)) { 
+	  const checker = checkLeftDiagonal();
+	  if(checker == "x" || checker == "o") {
+		winner(checker)
+		return;
+	  }
+	}
+  
+	if(!(tiles[2][0].state == null || tiles[1][1].state == null || tiles[0][2].state == null)) { 
+	  const checker = checkRightDiagonal();
+	  if(checker == "x" || checker == "o") {
+		winner(checker)
+		return;
+	  }
+	}
+	  
+  }
+  
+  function winner(type) {
+	// console.log(`${type} wins!`);
+	finished = true;
+	setMap(map`
+  ..........
+  ..........
+  ..........
+  ..........
+  ..........
+  ..........
+  ..........
+  ..........
+  ..........
+  ..........`)
+	addText(`${type} wins!`, {x: 7, y: 8})
+	playTune(tune`
+  1865.2849740932643,
+  155.44041450777203: a5^155.44041450777203 + g5^155.44041450777203 + f5^155.44041450777203 + e5^155.44041450777203 + d5^155.44041450777203,
+  155.44041450777203: a5^155.44041450777203 + d5^155.44041450777203 + c5^155.44041450777203,
+  155.44041450777203: a5^155.44041450777203 + c5^155.44041450777203,
+  155.44041450777203: f5^155.44041450777203 + a5^155.44041450777203 + c5^155.44041450777203,
+  155.44041450777203: f5^155.44041450777203 + a5^155.44041450777203 + c5^155.44041450777203,
+  155.44041450777203: f5^155.44041450777203 + g5^155.44041450777203 + a5^155.44041450777203 + c5^155.44041450777203,
+  155.44041450777203: c5^155.44041450777203,
+  155.44041450777203: c5^155.44041450777203,
+  155.44041450777203: c5^155.44041450777203,
+  155.44041450777203: c5^155.44041450777203 + b5^155.44041450777203,
+  155.44041450777203: c5^155.44041450777203 + b5^155.44041450777203,
+  155.44041450777203: c5^155.44041450777203 + a5^155.44041450777203 + b5^155.44041450777203,
+  155.44041450777203: d5^155.44041450777203 + e5^155.44041450777203 + f5^155.44041450777203 + g5^155.44041450777203 + a5^155.44041450777203,
+  `)
+  }
+  
+  
+  const basetile = "t";
+  const xwins = "k";
+  const owins = "l";
+  
+  setLegend(
+	[ "x", bitmap`
+  ................
+  ................
+  ................
+  ...4........4...
+  ....4......4....
+  .....4....4.....
+  ......4..4......
+  .......44.......
+  .......44.......
+  ......4..4......
+  .....4....4.....
+  ....4......4....
+  ...4........4...
+  ................
+  ................
+  ................`],
+	[ "o",bitmap`
+  ................
+  ................
+  ................
+  ................
+  .....555555.....
+  ....55555555....
+  ....55555555....
+  ....55555555....
+  ....55555555....
+  ....55555555....
+  ....55555555....
+  .....555555.....
+  ................
+  ................
+  ................
+  ................`],
+	[ basetile, bitmap`
+  0000000000000000
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0..............0
+  0000000000000000`],
+  );
+  
+  setSolids(["x","o"]);
+  
+  setMap(map`
+  ttt
+  ttt
+  ttt`);
+  
+  const tileMap = new Map();
+  
+  
+  const allTiles = [
+	[new Tile(0,0), new Tile(1,0), new Tile(2,0)],
+	[new Tile(0,1), new Tile(1,1), new Tile(2,1)],
+	[new Tile(0,2), new Tile(1,2), new Tile(2,2)]
+  ]
+  
+  checkForWinner(allTiles)
+  
+  let currentX = null;
+  let currentY = null;
+  // true == x, false == o
+  let currentTurn = true;
+  
+  function addAndClear() {
+	try {
+	  if(currentTurn == true) allTiles[currentY][currentX].addX();
+	  if(currentTurn == false) allTiles[currentY][currentX].addO();
+	} catch(e) {
+	  currentX = null;
+	  currentY = null;
+	  return;
+	}
+	currentTurn = !currentTurn
+	currentX = null;
+	currentY = null;
+  }
+  
+  onInput("a", () => {
+	if(currentX == null) {
+	  currentX = 0; 
+	  return;
+	}
+	currentY = 0;
+	addAndClear()
   })
-}
-
-const restartGame = async () => {
-  clearText();
-  gameover = false
-  // Remove all x's
-  getAll(x).forEach((x) => {
-    x.remove();
+  onInput("s", () => {
+	if(currentX == null) {
+	  currentX = 1; 
+	  return;
+	}
+	currentY = 1;
+	addAndClear() ;
+  })
+  
+  onInput("d", () => {
+	if(currentX == null) {
+	  currentX = 2; 
+	  return;
+	}
+	currentY = 2;
+	addAndClear();
+  })
+  
+  afterInput(() => {
+	// console.log(currentTurn)
+	checkForWinner(allTiles);
   });
-  // Remove all o's
-  getAll(o).forEach((o) => {
-    o.remove();
-  });
-
-  // Clear Board
-  board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-  ];
-}
-
-// inputs for player movement control
-onInput("w", () => {
-  if (!gameover) {
-    getFirst(player).y -= 1;
-  }
-});
-
-onInput("a", () => {
-  if (!gameover) {
-    getFirst(player).x -= 1;
-  }
-});
-
-onInput("s", () => {
-  if (!gameover) {
-    getFirst(player).y += 1;
-  }
-});
-
-onInput("d", () => {
-  if (!gameover) {
-    getFirst(player).x += 1;
-  }
-});
-
-onInput("j", () => {
-  if (gameover) {
-    restartGame();
-    return;
-  }
-
-  let playerPosition = [getFirst(player).x, getFirst(player).y];
-
-  // Check if can place in tile
-  if (getTile(playerPosition[0], playerPosition[1]).length > 1) {
-    return;
-  }
-
-  if (!turn) {
-    // Place X
-    addSprite(playerPosition[0], playerPosition[1], x);
-    board[playerPosition[1]][playerPosition[0]] = "x";
-  } else {
-    // Place Os
-    addSprite(playerPosition[0], playerPosition[1], o);
-    board[playerPosition[1]][playerPosition[0]] = "o";
-  }
-
-  // Check for winner
-  console.log("board", board)
-  // Check rows)
-  if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][2] == "x") {
-    gameOver("x");
-  } else if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][2] == "o") {
-    gameOver("o");
-  }
-  if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][2] == "x") {
-    gameOver("x");
-  } else if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][2] == "o") {
-    gameOver("o");
-  }
-  if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][2] == "x") {
-    gameOver("x");
-  } else if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][2] == "o") {
-    gameOver("o");
-  }
-  // Check columns
-  if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[2][0] == "x") {
-    gameOver("x");
-  } else if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[2][0] == "o") {
-    gameOver("o");
-  }
-  if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[2][1] == "x") {
-    gameOver("x");
-  } else if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[2][1] == "o") {
-    gameOver("o");
-  }
-  if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[2][2] == "x") {
-    gameOver("x");
-  } else if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[2][2] == "o") {
-    gameOver("o");
-  }
-  //Check diagonals
-  if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == "x") {
-    gameOver("x");
-  } else if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == "o") {
-    gameOver("o");
-  }
-  if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] == "x") {
-    gameOver("x");
-  } else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] == "o") {
-    gameOver("o");
-  }
-
-  //Check for draw
-  if (!gameover) {
-    counter = 0
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (board[i][j] == "") {
-          counter++
-        }
-      }
-    }
-    if (counter == 0) {
-      gameOver("draw")
-    }
-  }
-  turn = !turn;
-});
