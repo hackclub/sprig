f8ab60bd50d3da2560f2ffae09982797589aa3f9
DevIos01 <GamingCraft52@gmail.com>
2024-07-19 15:13:13 +0200

    fix

--- .github/scripts/split_into_batches.py
+++ .github/scripts/split_into_batches.py
@@ -2,17 +2,19 @@ import os
 import sys
 import glob
 import shutil
+from math import ceil
 
-def split_files_into_batches(src_dir, dest_dir, batch_size=10):
+def split_files_into_batches(src_dir, dest_dir, total_batches):
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
 
     js_files = glob.glob(os.path.join(src_dir, "*.js"))
     total_files = len(js_files)
-    
-    for i in range(0, total_files, batch_size):
-        batch_files = js_files[i:i + batch_size]
-        batch_dir = os.path.join(dest_dir, f"batch_{i // batch_size + 1}")
+    batch_size = ceil(total_files / total_batches)
+
+    for i in range(total_batches):
+        batch_files = js_files[i * batch_size:(i + 1) * batch_size]
+        batch_dir = os.path.join(dest_dir, f"batch_{i + 1}")
         
         if not os.path.exists(batch_dir):
             os.makedirs(batch_dir)
@@ -22,11 +24,12 @@ def split_files_into_batches(src_dir, dest_dir, batch_size=10):
             print(f"Copied {file} to {batch_dir}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python split_into_batches.py <src_dir> <dest_dir>")
+    if len(sys.argv) != 4:
+        print("Usage: python split_into_batches.py <src_dir> <dest_dir> <total_batches>")
         sys.exit(1)
 
     src_dir = sys.argv[1]
     dest_dir = sys.argv[2]
+    total_batches = int(sys.argv[3])
 
-    split_files_into_batches(src_dir, dest_dir)
+    split_files_into_batches(src_dir, dest_dir, total_batches)
