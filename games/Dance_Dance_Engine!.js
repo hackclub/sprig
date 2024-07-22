efacfb0aae91a265b5b44f934d884a7cdde21cd4
Marios Mitsios <xrteach@gmail.com>
2024-07-22 11:31:05 -0400

    Update metadata on Dance_Dance_Engine!.js

--- games/Dance_Dance_Engine!.js
+++ games/Dance_Dance_Engine!.js
@@ -3,7 +3,8 @@ First time? Check out my code: its not half bad!
 
 @title: Dance Dance Engine!!
 @author: Shibedev12
-@tags: [DDR Engine]
+@tags: ["DDR Engine"]
+@addedOn: 2024-07-22
 For Documentation on how to work with this, check out my site!
 https://shibedevsite.neocities.org/DDE/whatis
 */
