fc319a2c94a247e7cf319a292ef15df3f4c4d2ce
miqdad1234 <139256425+miqdad1234@users.noreply.github.com>
2024-07-23 05:09:53 -0700

    Update get_the_car_out.js
    Added a move counter
    Added horizontal cars and further improved puzzle layout
    Touched up the art

--- games/get_the_car_out.js
+++ games/get_the_car_out.js
@@ -14,40 +14,41 @@ const box = "o";
 const ball = "b"
 const goal = "g";
 const wall = "w";
+const vplayer = "n";
 
 // assign bitmap art to each sprite
 setLegend(
   [ player, bitmap`
-....033333330...
-....033333330...
-.....3333333....
-....033333330...
-....033333330...
-.....3366633....
-.....3366633....
-.....3366633....
-.....3366633....
-.....3366633....
-.....3366633....
-....033666330...
-....033333330...
-...33333333333..
-...33333333333..
-................`],
+................
+..33333333333...
+..33333333333...
+...033333330....
+...033666330....
+....3366633.....
+....3366633.....
+....3366633.....
+....3366633.....
+....3366633.....
+....3366633.....
+...033333330....
+...033333330....
+....3333333.....
+...033333330....
+...033333330....`],
   [ box, bitmap`
 ................
 ................
-.............77.
-...00....00..77.
-...77777777..77.
-...777777777.77.
-...7L0L0L0L7777.
-...70L0L0L07777.
-...7L0L0L0L7777.
-...777777777.77.
-...77777777..77.
-...00....00..77.
-.............77.
+.............66.
+...00....00..66.
+...66666666..66.
+...666666666.66.
+...679797976666.
+...697979796666.
+...679797976666.
+...666666666.66.
+...66666666..66.
+...00....00..66.
+.............66.
 ................
 ................
 ................`],
@@ -69,22 +70,22 @@ setLegend(
 0..............0
 .00000000000000.`],
   [ goal, bitmap`
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777
-7777777777777777`],
+0000000000000000
+2222222222222222
+0000000000000000
+0000000000200000
+0000000000020000
+0000000000002000
+0000000000000200
+2222222222222220
+0000000000000200
+0000000000002000
+0000000000020000
+0000000000200000
+0000000000000000
+2222222222222222
+0000000000000000
+0000000000000000`],
   [ wall, bitmap`
 CCCCCCCCCCCCCCCC
 CCCCCCCCCCCCCCCC
@@ -101,75 +102,88 @@ CCCCCCCCCCCCCCCC
 CCCCCCCCCCCCCCCC
 CCCCCCCCCCCCCCCC
 CCCCCCCCCCCCCCCC
-CCCCCCCCCCCCCCCC`]
+CCCCCCCCCCCCCCCC`],
+  [ vplayer, bitmap`
+................
+................
+.............33.
+00.00......0033.
+333333333333333.
+333333333333333.
+333336666666333.
+333336666666333.
+333336666666333.
+333333333333333.
+333333333333333.
+00.00......0033.
+.............33.
+................
+................
+................`],
 );
 
 // create game levels
 let level = 0; // this tracks the level we are on
 const levels = [
   map`
-wwwwwwwwwwwwwwww
-wb.............w
-w..p...p...p...w
-wo.p.p.p.p.p.p.g
-w....p...p...p.w
-w..............w
-wwwwwwwwwwwwwwww`,
+wwwwwwww
+wb.pnnnw
+w..p..pw
+wo.p.ppg
+w..p..pw
+w..n..nw
+wwwwwwww`,
   map`
-wwwwwwwwwwwwwwwwwww
-wb................w
-w.p..pp...p...p...w
-w.p..pp..pp...p...w
-wop.p.p.p.p.p.p.p.g
-w...p..pp...pp..ppw
-w...p..pp...p...p.w
-w.................w
-wwwwwwwwwwwwwwwwwww`,
-  map`
-wwwwwwwwwwwwwwwwwwwwww
-wb...................w
-w....................w
-w.p...p..............w
-w.p...p.....p.p.p.pp.w
-wop...p...pp..p.pp...g
-w...p...p............w
-w...p...p............w
-w...p...p............w
-w....................w
-wwwwwwwwwwwwwwwwwwwwww`,
+wwwwwwwww
+wb.nnp..w
+w..ppp..w
+wo.ppp..g
+w..ppnn.w
+w..p....w
+wwwwwwwww`,
   map`
 wwwwwwwwww
-wb.......w
-w..p.....w
-wo.pp.pp.g
-w...p.p..w
-w........w
-w........w
+wbpnnnnnpw
+w.p.....pw
+w.p..nn.pw
+wop..p..pg
+w.p..p..pw
+w.nnnp..nw
+w....n..pw
 wwwwwwwwww`,
   map`
-wwwwwwwwww
-wb.......w
-w.p.p.p..w
-wopp.p...g
-w........w
-w.......pw
-wwwwwwwwww`,
+wwwwwwwww
+wbnnnpnnw
+w..n.pnpw
+wo.p.pppg
+w..p.pppw
+w..nnnppw
+w....nnnw
+wwwwwwwww`,
   map`
-wwwwwwwwwww
-wb........w
-w.........w
-w.pppp..ppw
-wop.p.p.ppg
-w.........w
-w.........w
-wwwwwwwwwww`
+wwwwwwwwwwwwwww
+wb.nnnpn.nn.n.w
+w..p..p.p.p.p.w
+wo.p..p.p.p.p.g
+w..pnnnnp.p.p.w
+w.nnn...pnnnp.w
+wwwwwwwwwwwwwww`,
+  map`
+wwwwwwwwwwwwwww
+wbnnnnnnnp.nnnw
+wnnnnn.p.p..p.w
+wo.p.p.p.p..p.g
+w..p.p.p.p.nnnw
+w..p.p.nnn....w
+wnnpnnnp.p....w
+wwwwwwwwwwwwwww`,
 ];
 
 // set the map displayed to the current level
 const currentLevel = levels[level];
 setMap(currentLevel);
 
-setSolids([ player, box,  wall ]); // other sprites cannot go inside of these sprites
+setSolids([ player, box,  wall ,vplayer]); // other sprites cannot go inside of these sprites
 
 // allow certain sprites to push certain other sprites
 setPushables({
@@ -195,12 +209,15 @@ onInput("j", () => {
 });
 
 // inputs for  player movement cont
+
+var move = 14
         
     onInput("s", () => { 
       getAll(player).forEach((item, index) => {          
         if (getFirst(ball).x ===  item.x && getFirst(ball).y === item.y){
           item.y += 1; // positive y is downwards
           getFirst(ball).y += 1; // positive y is downwards
+          move -=1
         }
       })
     });
@@ -209,7 +226,8 @@ onInput("j", () => {
          getAll(player).forEach((item, index) => {          
           if ( getFirst(ball).x ===  item.x && getFirst(ball).y === item.y){
             item.y -= 1; 
-            getFirst(ball).y -= 1; 
+            getFirst(ball).y -= 1;
+             move -=1
           }
          })
     });
@@ -219,6 +237,7 @@ onInput("j", () => {
           if ( getFirst(ball).x ===  item.x && getFirst(ball).y === item.y){
             item.x += 1;
             getFirst(ball).x += 1;
+             move -=1
           }
         })
     });
@@ -228,21 +247,55 @@ onInput("j", () => {
           if ( getFirst(ball).x ===  item.x && getFirst(ball).y === item.y){
             item.x -= 1; 
             getFirst(ball).x -= 1; 
+             move -=1
           }
         })
     });
+
+ onInput("d", () => {
+        getAll(vplayer).forEach((item, index) => {          
+          if ( getFirst(ball).x ===  item.x && getFirst(ball).y === item.y){
+            item.x += 1;
+            getFirst(ball).x += 1;
+             move -=1
+          }
+        })
+    });
+
+    onInput("a", () => {
+        getAll(vplayer).forEach((item, index) => {          
+          if ( getFirst(ball).x ===  item.x && getFirst(ball).y === item.y){
+            item.x -= 1; 
+            getFirst(ball).x -= 1;
+            move -=1
+          }
+        })
+    });
+
 afterInput(()=>{
+  clearText()
+  if(move===0){
+   addText("you lose!", { y: 4, color: color`0` }); 
+    move=14
+    setMap(levels[0])
+
+  }
+  addText("moves:" + move, { y: 1, color: color`2` }); 
   console.log(tilesWith(goal, box).length)
   if (tilesWith(goal, box).length>0) {
     // increase the current level number
       level = level + 1;
-    if (levels[level] !== undefined) {
+    if (levels[level] !== undefined){
         setMap(levels[level]);
-    } else {
-      addText("you win!", { y: 4, color: color`3` });
+        move=8* (level + 1)
+        addText("moves:" + move, { y: 1, color: color`2` }); 
+    }
+    else {
+      addText("you win!", { y: 4, color: color`6` });
     }
   }    
-})
+});
+
 
   // make sure the level exists before we load it
 
