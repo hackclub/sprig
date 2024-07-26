2b71663b8c648bcdfa4630ec15c0b0eae64a22da
Marios Mitsios <xrteach@gmail.com>
2024-07-26 10:18:14 -0400

    Update metadata in super_amazing_maze.js

--- super_awesome_maze.js
+++ super_awesome_maze.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: super awesome platformer
-@author: 
+@author: anayal8er
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-07-26
 */
 
 const player = "p"
