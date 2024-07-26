26350bdef25c783f1ab3c29d74a5bd667b4a4ef7
Marios Mitsios <xrteach@gmail.com>
2024-07-26 09:25:09 -0400

    Update metadata on Crossy Road.js

--- games/Crossy Road.js
+++ games/Crossy Road.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Crossy Road
-@author: 
+@author: Bunnzulu
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-07-26
 */
 
 const player = "p"
