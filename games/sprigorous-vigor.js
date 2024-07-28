f21e8917b400ac21d3c8c34ace20c9b0d84159bb
Boston M. <69646049+boston2029@users.noreply.github.com>
2024-07-27 22:05:08 -0500

    Update sprigorous-vigor.js

--- games/sprigorous-vigor.js
+++ games/sprigorous-vigor.js
@@ -5,7 +5,7 @@ https://sprig.hackclub.com/gallery/getting_started
 @title: Sprigorous Vigor
 @author: boston2029
 @tags: []
-@addedOn: 2024-07-26
+@addedOn: 2024-07-27
 */
 
 const player = "p"
@@ -99,7 +99,24 @@ setLegend(
 ...H9HH99HH9H...
 ...H99HHHH99H...
 ...HHHHHHHHHH...
-................`]
+................`],
+  ['x', bitmap`
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555`]
 )
 
 setSolids([player, wall, pushable])
@@ -107,32 +124,32 @@ setSolids([player, wall, pushable])
 let level = 0
 const levels = [
   map`
-wwwwnwwwwww
-...........
-wwwwwwwwww.
-...........
-.wwwwwwwwww
-...........
-wwwwwwwwww.
-p..........`,
+wwwwnw...ww
+wwww...w...
+wwwwwww.ww.
+w..........
+w.wwwwwwwww
+w........ww
+wwwwwwww.ww
+wp.......ww`, // begin
   map`
-nwwwwwwwwww
-...........
-wwwwwwwwww.
-www....www.
-ww..ww.....
+nww...w...w
+..w.w...w..
+w...wwwwww.
+wwww...www.
+ww...w.....
 ww.wwwwwwww
 ww....wwwww
-wwwwwpwwwww`,
+wwwwwpwwwww`, // need to get out
   map`
-..........p
+w.........p
 w.wwwwwwwww
+wbwxwwwwxww
 wbwwwwwwwww
-wbwwwwwwwww
-..wwwwwwwww
-...wwwww...
-...wwwww...
-..........n`,
+w.wwxwwxwww
+w..wwxxw...
+w..wwww..w.
+w.......wwn`, // i can push these
   map`
 wwwwwwwwwwp
 wwwwwwwwwwb
@@ -141,7 +158,7 @@ wwwwbwwwwwb
 ww...wwwww.
 ww.n.wwwww.
 ww...wwwwww
-wwwwwwwwwww`,
+wwwwwwwwwww`, // no text pushables
   map`
 n..........
 .bbbbbbbbbb
@@ -150,7 +167,7 @@ n..........
 .bbbbbbbbbb
 .bbbbbbbbbb
 .bbbbbbbbbb
-.bbbbbbbbbp`,
+.bbbbbbbbbp`, // nononono
   map`
 ....wnw....
 wwwww.wwwww
@@ -159,7 +176,7 @@ wwwww.wwwww
 wwwww.wwwww
 wwwww.wwwww
 wwwww.wwwww
-wwwwwpwwwww`,
+wwwwwpwwwww`, // "trust me bro"
   map`
 .wwwwnwwww.
 .wwww.wwww.
@@ -168,7 +185,7 @@ wwwwwpwwwww`,
 .wwww.wwww.
 .wwww.wwww.
 .wwww.wwww.
-.wwwwpwwww.`,
+.wwwwpwwww.`, //
   map`
 ..wwwnwww..
 ..www.www..
@@ -177,7 +194,7 @@ wwwwwpwwwww`,
 ..www.www..
 ..www.www..
 ..www.www..
-..wwwpwww..`,
+..wwwpwww..`, //
   map`
 ....wnw....
 ....w.w....
@@ -186,25 +203,63 @@ wwwwwpwwwww`,
 ....w.w....
 ....w.w....
 ....w.w....
-....wpw....`,
+....wpw....`, //
   map`
-....wn.....
-......w....
-....w......
-......w....
-....w......
-......w....
-....w......
-.....pw....`,
+.w.w......w
+w.w.w.w.w.w
+.w.w...ww.w
+w.w.w.w.wnw
+.w.w...w.w.
+w.w.w.w.w.w
+.w.w...w.w.
+w.w.wpw.w.w`, // checkerboard
   map`
 n..........
-...........
-...........
-...........
-...........
-...........
-...........
-..........p`,
+wwwwww.wwww
+wwwwww.wwww
+wwwww..wwww
+wwwww.wwwww
+wwwww..wwww
+wwwwww.wwww
+wwwwww....p`, // time it right
+  map`
+wwwwwwwwwwwwwwwwwww
+w.......wwwwwwwwwww
+w.wwwww.wwwwwwwww.w
+w.wwwww.wwwwwwwww.w
+w..wn...........w.w
+w.wwww.www......w.w
+www.wwwwww......w.w
+p...wwwwww......w.w
+ww..www....w.w.ww.w
+www..wwwwwwwww.ww.w
+w.w...wwwwwwww.ww.w
+w.ww..ww..........w
+w..w..............w
+w..w..w...........w
+w..w..wwwwwwwwwwwww
+w..w..wwwwwwwwwwwww
+w.....wwwwwwwwwwwww
+wwwwwwwwwwwwwwwwwww`, // surprise
+  map`
+w.w..............w
+.w.w............w.
+w.w.w..........w..
+.w.w.w......www...
+w.w.w...bbbbbb....
+.b.w.w.w....w.....
+b.b.w.w....w......
+.b.b.wwwwww.....p.
+b.b.b...ww........
+.b.b.b.www........
+b.b.b.w...w.......
+.www.b.w...w......
+bw.wb.w.....w.....
+.bnbwbww.....b....
+b...b.........w...
+.b.b.b.........w..
+b.b.b.w.........w.
+.b.b.w.w.........w`, // surprise
   map`
 bb.wwwwwwww
 bnbw.....cw
@@ -226,17 +281,59 @@ p......b..b
 b.b...b....`
 ]
 
-setMap(levels[level])
+setMap(map`
+www
+wpw
+www`)
+
+addText("SPRIGOROUS VIGOR", {
+  y:2,
+  color:color`2`
+})
+
+addText("Press L to start", {
+  y:13,
+  color:color`2`
+})
 
 setPushables({
   [ player ]: [player, pushable],
   [pushable]: [pushable, pushable]
 })
 
-addText("where am i?", {
-  x: 4,
-  y: 14
-})
+playTune(tune`
+312.5: B5-312.5,
+312.5: A5-312.5,
+312.5: G5-312.5,
+312.5: F5-312.5,
+312.5: G4-312.5,
+312.5: A4-312.5,
+312.5: B4-312.5,
+312.5: C5-312.5,
+312.5: B5^312.5,
+312.5: B5-312.5,
+312.5: B5~312.5,
+312.5,
+312.5: E5-312.5,
+312.5: D5-312.5,
+312.5: C5-312.5,
+312.5: B4-312.5,
+312.5: G4/312.5,
+312.5: G4/312.5,
+312.5: G4/312.5,
+312.5,
+312.5: B5/312.5,
+312.5: A5/312.5,
+312.5: G5/312.5,
+312.5: F5/312.5,
+312.5: C4~312.5 + E4~312.5 + G4~312.5 + F4^312.5 + D4^312.5,
+312.5: A5/312.5,
+312.5: B5/312.5,
+312.5: B5/312.5,
+312.5: E4-312.5,
+312.5: D4~312.5 + C4^312.5,
+312.5: A4-312.5,
+312.5: B5-312.5`, Infinity)
 
 onInput("w", () => {
   getFirst(player).y -= 1
@@ -252,6 +349,39 @@ onInput("d", () => {
 })
 onInput("l", () => {
   level=0
+  playTune(tune`
+312.5: B5-312.5,
+312.5: A5-312.5,
+312.5: G5-312.5,
+312.5: F5-312.5,
+312.5: G4-312.5,
+312.5: A4-312.5,
+312.5: B4-312.5,
+312.5: C5-312.5,
+312.5: B5^312.5,
+312.5: B5-312.5,
+312.5: B5~312.5,
+312.5,
+312.5: E5-312.5,
+312.5: D5-312.5,
+312.5: C5-312.5,
+312.5: B4-312.5,
+312.5: G4/312.5,
+312.5: G4/312.5,
+312.5: G4/312.5,
+312.5,
+312.5: B5/312.5,
+312.5: A5/312.5,
+312.5: G5/312.5,
+312.5: F5/312.5,
+312.5: C4~312.5 + E4~312.5 + G4~312.5 + F4^312.5 + D4^312.5,
+312.5: A5/312.5,
+312.5: B5/312.5,
+312.5: B5/312.5,
+312.5: E4-312.5,
+312.5: D4~312.5 + C4^312.5,
+312.5: A4-312.5,
+312.5: B5-312.5`, Infinity)
   clearText()
   addText("where am i?", {
     x: 4,
@@ -260,6 +390,7 @@ onInput("l", () => {
   setMap(levels[level])
 })
 
+let moverInterval
 afterInput(() => {
   const playerTile = getTile(getFirst(player).x, getFirst(player).y)
   const hasReachedPortal = playerTile.some(sprite => sprite.type === portal)
@@ -283,12 +414,13 @@ bbb`)
       case 2:
         addText("need to get out", {
           x: 5,
-          y: 3
+          y: 3,
+          color:color`1`
         })
         break
       case 3:
         addText("i can push these", {
-          x: 1,
+          x: 3,
           y: 1
         })
         break
@@ -309,19 +441,33 @@ bbb`)
         })
         break
       case 11:
-        addText("run quick", {
-          x: 3,
-          y: 7
+        addText("time it right", {
+          y: 7,
+          color:color`1`
         })
+        let directionRight = true
+        moverInterval = setInterval(function(){
+          if (getFirst(portal).x==10) {
+            directionRight=false
+          }
+          if (getFirst(portal).x==0) {
+            directionRight=true
+          }
+          directionRight?getFirst(portal).x+=1:getFirst(portal).x-=1
+        }, 100)
         break
       case 12:
+        clearInterval(moverInterval)
+        addText("surprise lol", {x:7,y:14, color: color`2`})
+        break
+      case 14:
         addText("who do you choose", {y:12, color: color`1`})
         break
-      case 13:
-        addText("you won!", {color: color`0`, y:0})
-        addText("press l to replay!", {color: color`0`, y:2})
-        addText("or mess with", {color: color`0`, y:6})
-        addText("these pushables!", {color: color`0`, y:7})
+      case 15:
+        addText("you won!", {color: color`0`, y:1})
+        addText("press l to replay!", {color: color`0`, y:3})
+        addText("or mess with", {color: color`0`, y:13})
+        addText("these pushables!", {color: color`0`, y:14})
         break
     }
   }
