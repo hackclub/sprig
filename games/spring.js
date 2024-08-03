f3c1058201465431651b660f71e8f9f22036f3aa
TechPandaPro <javaisntcoffee@gmail.com>
2024-08-03 15:51:28 -0700

    Improve arrow constraints, apply constraints to "L" key

--- games/spring.js
+++ games/spring.js
@@ -11,6 +11,7 @@ https://sprig.hackclub.com/gallery/getting_started
 const player = "p"
 
 const wall = "w"
+const wall_no_stick = "n"
 
 const arrow_0 = "0"
 const arrow_45 = "1"
@@ -22,6 +23,7 @@ const arrow_270 = "6"
 const arrow_315 = "7"
 
 const arrowIncrement = 45
+const lastArrow = 360 / arrowIncrement - 1
 
 setLegend(
   [ arrow_0, bitmap`
@@ -193,34 +195,51 @@ setLegend(
 0099999999999900
 0099999999999900
 0000000000000000
-0000000000000000` ]
+0000000000000000` ],
+  [ wall_no_stick, bitmap`
+.00000000000000.
+0000000000000000
+00LLL111LLLLLL00
+00LL111LLLLLL100
+00L111LLLLLL1L00
+00111LLLLLL1LL00
+0011LLLLLL1LLL00
+001LLLLLL1LLL100
+00LLLLLL1LLL1100
+00LLLLL1LLL11L00
+00LLLL1LLL11LL00
+00LLL1LLL11LLL00
+00LL1LLL11LLLL00
+00L1LLL11LLLLL00
+0000000000000000
+.00000000000000.` ]
 )
 
-setSolids([ player, wall ])
+setSolids([ player, wall, wall_no_stick ])
 
 let level = 0
 const levels = [
   map`
-....................
-....................
-....................
-....................
-....................
-.ww.ww.ww.ww.ww.ww.w
-....................
-....................
-....................
-....................
-...w...w...w...w...w
-....................
-.w...w...w...w...w..
-.............w......
-....................
-w..................w
-w..................w
-....................
-w.wpw.w.w.w.w.w.w.w.
-wwwwwwwwwwwwwwwwwwww`
+nnnnnnnnnnnnnnnnnnnn
+n..................n
+n..................n
+n..................n
+n..................n
+nww.ww.ww.ww.ww.ww.n
+n..................n
+n..................n
+n..................n
+n..................n
+n..w...w...w...w...n
+n..................n
+nw...w...w...w...w.n
+n............w.....n
+n..................n
+n..................n
+n..................n
+n..................n
+n.wp..w.w.w.w.w.w.wn
+nwwwwwwwwwwwwwwwwwwn`
 ]
 
 setMap(levels[level])
@@ -250,63 +269,35 @@ onInput("s", () => {
 
 onInput("i", () => {
   if (arrowType === null && !inAir) {
-    addSprite(0, 0, arrow_0)
-    arrowType = arrow_0
+    let newArrowType = Number(arrow_0)
+    while (checkArrowIsInvalid(newArrowType)) {
+      newArrowType += 1
+      if (newArrowType === lastArrow) throw new Error("No possible moves")
+    }
+    newArrowType = newArrowType.toString()
+    
+    addSprite(0, 0, newArrowType)
+    arrowType = newArrowType
     setArrowPosition()
   }
 })
 
+// TODO: simplify code in "j" and "l" listeners to avoid duplicating
 onInput("j", () => {
   if (arrowType !== null) {
-    let nextArrow = true
-
-    // TODO: rather than "looping" the arrow around, simply prevent it from being moved past the bounds
-    // TODO: same logic (almost) would be added to the "l" input listener. it's not there currently for testing purposes.
-    while (nextArrow) {
-      const arrowTypeNum = Number(arrowType)
-      
-      const lastArrow = 360 / arrowIncrement - 1
-      const newType = (arrowTypeNum === lastArrow) ? 0 : (arrowTypeNum + 1)
-      
-      const arrowSprite = getFirst(arrowType)
-      arrowSprite.type = newType.toString()
-      arrowType = newType.toString()
-  
-      setArrowPosition()
-
-      nextArrow = checkArrowIsInvalid()
-    }
+    const arrowTypeNum = Number(arrowType)
     
-    // console.log(getArrowDeg())
+    const newType = (arrowTypeNum === lastArrow) ? 0 : (arrowTypeNum + 1)
+    
+    const isInvalid = checkArrowIsInvalid(newType)
 
-    // const p = getFirst(player)
-    // const walls = getAll(wall)
-    // const arrowDeg = getArrowDeg()
-    // const touching = walls.filter(w => (
-    //   (w.y === p.y && Math.abs(w.x - p.x) <= 1) ||
-    //   (w.x === p.x && Math.abs(w.y - p.y) <= 1)
-    // ))
-    // .map(w => ({ angle: Math.atan2(p.y-w.y, p.x-w.x)*(180/Math.PI) })).map(w => ({ angle: w.angle < 0 ? w.angle + 360 : w.angle })).map(w => ({ angle: w.angle, distance: Math.abs(getArrowDeg()-w.angle) % 360 }))
-    // const someIsInvalid = walls.some(w => {
-    //   const isTouching = (
-    //     (w.y === p.y && Math.abs(w.x - p.x) <= 1) ||
-    //     (w.x === p.x && Math.abs(w.y - p.y) <= 1)
-    //   )
-      
-    //   if (!isTouching) return false
-      
-    //   let angle = Math.atan2(p.y-w.y, p.x-w.x)*(180/Math.PI)
-    //   angle = angle < 0 ? angle + 360 : angle
+    if (isInvalid) return
+    
+    const arrowSprite = getFirst(arrowType)
+    arrowSprite.type = newType.toString()
+    arrowType = newType.toString()
 
-    //   const distance = Math.abs(arrowDeg-angle) % 360
-    //   if (distance <= 45) return true
-    // })
-    // if (someIsInvalid) console.log("nope!")
-    // console.log(touching)
-    // if (touching.some(t => t.distance <= 45)) {
-    //   console.log("nope!")
-    // }
-    // console.log(touching)
+    setArrowPosition()
   }
 })
 
@@ -314,13 +305,16 @@ onInput("l", () => {
   if (arrowType !== null) {
     const arrowTypeNum = Number(arrowType)
     
-    const lastArrow = 360 / arrowIncrement - 1
     const newType = (arrowTypeNum === 0) ? lastArrow : (arrowTypeNum - 1)
+    
+    const isInvalid = checkArrowIsInvalid(newType)
+
+    if (isInvalid) return
     
     const arrowSprite = getFirst(arrowType)
     arrowSprite.type = newType.toString()
     arrowType = newType.toString()
-    
+
     setArrowPosition()
   }
 })
@@ -341,29 +335,18 @@ onInput("k", () => {
     yVel = -startVel * Math.sin(rad)
 
     setMapFromParsed(fullMap)
-
-    // const touchingWalls = getTouching()
-    // console.log(touchingWalls)
     
     const playerSprite = getFirst(player)
     fullX = playerSprite.x
     fullY = playerSprite.y
     
     centerMap()
-
-    // console.log(fullX)
-    // console.log(fullY)
-    // console.log(deg)
-    // console.log(xVel)
-    // console.log(yVel)
     
     const interval = setInterval(() => {
       setMapFromParsed(fullMap)
       
       const p = getFirst(player)
       
-      // playerSprite.x += xVel
-      // playerSprite.y += yVel
       fullX += xVel
       fullY += yVel
 
@@ -375,24 +358,7 @@ onInput("k", () => {
       
       yVel += gravity
 
-      // if (
-      //   // (xVel && oldX === playerSprite.x) ||
-      //   // (yVel && oldY === playerSprite.y)
-      //   Math.round(fullX) !== playerSprite.x ||
-      //   Math.round(fullY) !== playerSprite.y
-      // ) {
-      //   console.log("clear");
-      //   clearInterval(interval);
-      // }
-
-      // if (isEffectivelyZero()) {
-      //   console.log("clear")
-      //   clearInterval(interval)
-      // }
-
       const walls = getAll(wall)
-      // TODO: check which side player is touching wall, compared against vel
-      // if (walls.some(w => playerTouchingWall(playerSprite, w))) {
       if (
         walls.some(w => (
           (w.x === p.x && w.y === p.y - 1 && yVel < 0 && !isEffectivelyZero(yVel)) ||
@@ -401,12 +367,11 @@ onInput("k", () => {
           (w.y === p.y && w.x === p.x - 1 && xVel < 0 && !isEffectivelyZero(xVel))
         ))
       ) {
+        // TODO: consider playing sound effect here
         console.log("clear")
         clearInterval(interval)
         inAir = false
       }
-
-      // TODO: prevent arrow from showing up when interval still exists
     
       fullMap = getParsedMap()
       
@@ -448,16 +413,9 @@ function setArrowPosition() {
   }
 }
 
-function getArrowDeg() {
-  if (arrowType === null) return null
-  return Number(arrowType) * arrowIncrement
-}
-
-// function getArrowDeg(thisArrowType) {
-//   if (typeof thisArrowType === "string" || typeof thisArrowType === "number")
-//     return Number(arrowType) * arrowIncrement
-//   else if (arrowType === null) return null
-//   else return Number(arrowType) * arrowIncrement
+// function getArrowDeg() {
+//   if (arrowType === null) return null
+//   return Number(arrowType) * arrowIncrement
 // }
 
 function getParsedMap() {
@@ -593,32 +551,34 @@ function centerMap() {
 //   )
 // }
 
-function checkArrowIsInvalid() {
+function checkArrowIsInvalid(thisArrowType) {
   const p = getFirst(player)
   const walls = getAll(wall)
-  const arrowDeg = getArrowDeg()
-
-  console.log("\n\n\nNEW CHECK")
+  const arrowDeg = getArrowDeg(thisArrowType)
   
-  const someIsInvalid = walls.filter(w => {
+  const someIsInvalid = walls.some(w => {
     const isTouching = (
       (w.y === p.y && Math.abs(w.x - p.x) <= 1) ||
       (w.x === p.x && Math.abs(w.y - p.y) <= 1)
     )
 
     if (!isTouching) return false
-
-    console.log(isTouching)
     
-    let _angle = Math.atan2(p.y-w.y, w.x-p.x)*(180/Math.PI)
-    angle = _angle < 0 ? _angle + 360 : _angle
+    let angle = Math.atan2(p.y-w.y, w.x-p.x)*(180/Math.PI)
+    angle = angle < 0 ? angle + 360 : angle
 
     const distance = Math.abs(arrowDeg-angle) % 360
-    console.log({ angle, _angle, distance, px: p.x, py: p.y, wx: w.x, wy: w.y, dx: p.x-w.x, dy: p.y-w.y })
     if (distance <= 45) return true
-  }).length >= 1
+  })
   
   return someIsInvalid
+}
+
+function getArrowDeg(thisArrowType) {
+  if (typeof thisArrowType === "string" || typeof thisArrowType === "number")
+    return Number(thisArrowType) * arrowIncrement
+  else if (arrowType === null) return null
+  else return Number(arrowType) * arrowIncrement
 }
 
 function isEffectivelyZero(num) {
@@ -626,12 +586,5 @@ function isEffectivelyZero(num) {
   return Math.abs(num) < epsilon
 }
 
-// console.log(getParsedMap())
-
-// setMapFromParsed(zoomMap(getParsedMap()))
-
 fullMap = getParsedMap()
 centerMap()
-
-// console.log(`width: ${width()}`)
-// setMapFromParsed(zoomMap(getParsedMap(), 1, 0))
