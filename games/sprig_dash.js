c05a90517df0da853ff5aafb811d53a63bb623dd
graham <graham@hackclub.com>
2024-08-05 13:59:03 -0400

    Fixing metadata & filepath

--- games/sprig_dash.js
+++ games/sprig_dash.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Sprig Dash
-@author: 
+@author: akshatk-khurana
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-08-05
 */
 
 const player = "p";
