2779e005b0d0db60cb3c3877f97d81793606298e
TechPandaPro <javaisntcoffee@gmail.com>
2024-08-05 01:44:16 -0700

    Improve diagonal collisions, add lava functionality

--- games/spring.js
+++ games/spring.js
@@ -499,7 +499,7 @@ const melody3 = tune`
 150: D4-150 + C5-150,
 450`
 
-setSolids([ player, wall, wall_no_stick ])
+// setSolids([ player, wall, wall_no_stick ])
 
 let level = 1
 const levels = [
@@ -598,6 +598,7 @@ let yVel = 0
 let gravity = 0.1
 
 let inAir = false
+let gameOver = false
 
 let arrowType = null
 
@@ -611,26 +612,30 @@ let zoom = {
 }
 
 onInput("w", async () => {
-  if (inAir) return
+  if (gameOver || inAir) return
   panBy(0, -1)
 })
 
 onInput("a", () => {
-  if (inAir) return
+  if (gameOver || inAir) return
   panBy(-1, 0)
 })
 
 onInput("s", () => {
-  if (inAir) return
+  if (gameOver || inAir) return
   panBy(0, 1)
 })
 
 onInput("d", () => {
-  if (inAir) return
+  if (gameOver || inAir) return
   panBy(1, 0)
 })
 
 onInput("i", () => {
+  // TODO: consider combining this early return with the 3rd (same for jkl)
+  // TODO: reset game when pressing any key. this should be a separate function
+  if (gameOver) return
+  
   if (zoom.isZoomedOut) {
     resetPan()
     return
@@ -656,52 +661,34 @@ onInput("i", () => {
 
 // TODO: simplify code in "j" and "l" listeners to avoid duplicating
 onInput("j", () => {
+  if (gameOver) return
+  
   if (zoom.isZoomedOut) {
     resetPan()
     return
   }
   
   if (arrowType === null) return
-  
-  const arrowTypeNum = arrowCounters.indexOf(arrowType)
-  
-  const newType = arrowCounters[(arrowTypeNum === lastArrow) ? 0 : (arrowTypeNum + 1)]
-  
-  const isInvalid = checkArrowIsInvalid(newType)
 
-  if (isInvalid) return
-  
-  const arrowSprite = getFirst(arrowType)
-  arrowSprite.type = newType
-  arrowType = newType
-
-  setArrowPosition()
+  rotateArrow(1)
 })
 
 onInput("l", () => {
+  if (gameOver) return
+  
   if (zoom.isZoomedOut) {
     resetPan()
     return
   }
   
   if (arrowType === null) return
-  
-  const arrowTypeNum = arrowCounters.indexOf(arrowType)
-  
-  const newType = arrowCounters[(arrowTypeNum === 0) ? lastArrow : (arrowTypeNum - 1)]
-  
-  const isInvalid = checkArrowIsInvalid(newType)
 
-  if (isInvalid) return
-  
-  const arrowSprite = getFirst(arrowType)
-  arrowSprite.type = newType
-  arrowType = newType
-
-  setArrowPosition()
+  rotateArrow(-1)
 })
 
 onInput("k", () => {
+  if (gameOver) return
+  
   if (zoom.isZoomedOut) {
     resetPan()
     return
@@ -731,14 +718,10 @@ onInput("k", () => {
   
   centerMap()
   
-  const interval = setInterval(async () => {
+  const interval = setInterval(() => {
     const times = []
     
-    let midStamp;
-    
-    midStamp = Date.now()
     setMapFromParsed(fullMap)
-    times.push({ time: Date.now()-midStamp, line: 764 })
     
     const p = getFirst(player)
     
@@ -750,42 +733,74 @@ onInput("k", () => {
 
     yVel = Math.min(yVel+gravity, 1)
 
-    midStamp = Date.now()
-    const walls = getAll(wall)
-    if (
-      walls.some(w => (
-        (w.x === p.x && w.y === p.y - 1 && yVel < 0 && !isEffectivelyZero(yVel)) ||
-        (w.y === p.y && w.x === p.x + 1 && xVel > 0 && !isEffectivelyZero(xVel)) ||
-        (w.x === p.x && w.y === p.y + 1 && yVel > 0 && !isEffectivelyZero(yVel)) ||
-        (w.y === p.y && w.x === p.x - 1 && xVel < 0 && !isEffectivelyZero(xVel))
-      ))
-    ) {
-      // TODO: consider playing sound effect here
-      // console.log("clear")
-      clearInterval(interval)
-      inAir = false
-    } else {
-      const wallsNoStick = getAll(wall_no_stick)
-      if (
-        wallsNoStick.some(w => (
-          (w.y === p.y && w.x === p.x + 1 && xVel > 0 && !isEffectivelyZero(xVel)) ||
-          (w.y === p.y && w.x === p.x - 1 && xVel < 0 && !isEffectivelyZero(xVel))
-        ))
-      ) {
-        xVel = 0
+    const overlap = [...getAll(wall), ...getAll(wall_no_stick)].find(w => w.x === p.x && w.y === p.y)
+    if (overlap) {
+      if (yVel > xVel) {
+        p.x -= xVel / Math.abs(xVel)
         fullX = p.x
-      }
-      if (
-        wallsNoStick.some(w => (
-          (w.x === p.x && w.y === p.y + 1 && yVel > 0 && !isEffectivelyZero(yVel)) ||
-          (w.x === p.x && w.y === p.y - 1 && yVel < 0 && !isEffectivelyZero(yVel))
-        ))
-      ) {
-        yVel = 0
+      } else {
+        p.y -= yVel / Math.abs(yVel)
         fullY = p.y
       }
     }
-    times.push({ time: Date.now()-midStamp, line: 818 })
+
+    const lavaOverlap = getAll(lava).find(l => l.x === p.x && l.y === p.y)
+    if (lavaOverlap) {
+      clearInterval(interval)
+      inAir = false
+      gameOver = true
+      addText("Game Over", {
+        y: 6,
+        color: color`2`
+      })
+      // addText("You hit the lava!", {
+      //   y: 8,
+      //   color: color`2`
+      // })
+      addText("Press any button", {
+        y: 8,
+        color: color`2`
+      })
+      addText("to play again!", {
+        y: 9,
+        color: color`2`
+      })
+    } else {
+      const walls = getAll(wall)
+      if (
+        walls.some(w => (
+          (w.x === p.x && w.y === p.y - 1 && yVel < 0 && !isEffectivelyZero(yVel)) ||
+          (w.y === p.y && w.x === p.x + 1 && xVel > 0 && !isEffectivelyZero(xVel)) ||
+          (w.x === p.x && w.y === p.y + 1 && yVel > 0 && !isEffectivelyZero(yVel)) ||
+          (w.y === p.y && w.x === p.x - 1 && xVel < 0 && !isEffectivelyZero(xVel))
+        ))
+      ) {
+        // TODO: consider playing sound effect here
+        // console.log("clear")
+        clearInterval(interval)
+        inAir = false
+      } else {
+        const wallsNoStick = getAll(wall_no_stick)
+        if (
+          wallsNoStick.some(w => (
+            (w.y === p.y && w.x === p.x + 1 && xVel > 0 && !isEffectivelyZero(xVel)) ||
+            (w.y === p.y && w.x === p.x - 1 && xVel < 0 && !isEffectivelyZero(xVel))
+          ))
+        ) {
+          xVel = 0
+          fullX = p.x
+        }
+        if (
+          wallsNoStick.some(w => (
+            (w.x === p.x && w.y === p.y + 1 && yVel > 0 && !isEffectivelyZero(yVel)) ||
+            (w.x === p.x && w.y === p.y - 1 && yVel < 0 && !isEffectivelyZero(yVel))
+          ))
+        ) {
+          yVel = 0
+          fullY = p.y
+        }
+      }
+    }
 
     fullMap = getParsedMap()
     centerMap()
@@ -795,6 +810,33 @@ onInput("k", () => {
 // afterInput(() => {
   
 // })
+
+function rotateArrow(rotateCount) {
+  const arrowTypeNum = arrowCounters.indexOf(arrowType)
+  
+  const arrowCount = lastArrow + 1
+
+  // TODO: improve this logic (or at least use a ternary operator)
+  let newTypeNum = arrowTypeNum + rotateCount
+  if (newTypeNum > lastArrow) newTypeNum = newTypeNum % arrowCount
+  else if (newTypeNum < 0) {
+    newTypeNum = arrowCount + (newTypeNum % arrowCount)
+    if (newTypeNum === arrowCount) newTypeNum = 0
+  }
+  // console.log(newTypeNum)
+
+  const newType = arrowCounters[newTypeNum]
+  
+  const isInvalid = checkArrowIsInvalid(newType)
+
+  if (isInvalid) return
+  
+  const arrowSprite = getFirst(arrowType)
+  arrowSprite.type = newType
+  arrowType = newType
+
+  setArrowPosition()
+}
 
 function setArrowPosition() {
   if (arrowType !== null) {
@@ -838,7 +880,7 @@ function getParsedMap() {
   return map
 }
 
-function setMapFromParsed(parsedMap) {  
+function setMapFromParsed(parsedMap) {
   setMap(
     parsedMap.map(row =>
       row.map(tile => tile.length === 0 ? "." : tile[0]).join("")
@@ -866,7 +908,7 @@ function setMapFromParsed(parsedMap) {
       for (let i = 1; i < tile.length; i++) {
         const sprite = tile[i]
         // console.log(tile)
-        addSprite(x, y, tile)
+        addSprite(x, y, sprite)
       }
     }
   }
@@ -1039,19 +1081,27 @@ function checkArrowIsInvalid(thisArrowType) {
   
   const someIsInvalid = walls.some(w => {
     const isTouching = (
-      (w.y === p.y && Math.abs(w.x - p.x) <= 1) ||
-      (w.x === p.x && Math.abs(w.y - p.y) <= 1)
+      (w.y === p.y && Math.abs(w.x - p.x) === 1) ||
+      (w.x === p.x && Math.abs(w.y - p.y) === 1)
     )
 
-    if (!isTouching) return false
+    const isTouchingDiagonal = (
+      !isTouching &&
+      Math.abs(w.x - p.x) === 1 && Math.abs(w.y - p.y) === 1
+    )
+
+    if (!isTouching && !isTouchingDiagonal) return false
     
     let angle = Math.atan2(p.y-w.y, w.x-p.x)*(180/Math.PI)
     angle = angle < 0 ? angle + 360 : angle
 
     let distance = Math.abs(arrowDeg-angle) % 360
     distance = distance > 180 ? (180 - (distance % 180)) : distance
-    // console.log(distance)
-    if (distance < 90) return true
+    
+    if (
+      (isTouching && distance < 90) ||
+      (isTouchingDiagonal && distance < 45)
+    ) return true
   })
   
   return someIsInvalid
