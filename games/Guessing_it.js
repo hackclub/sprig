72e85aff0abf11fde797a992b5915e4808ca083d
HudRairigh <XiuRairigh@outlook.com>
2024-08-06 14:16:44 -0400

    Update Guessing_it.js

--- games/Guessing_it.js
+++ games/Guessing_it.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Guessing game
-@author: 
+@author:Hud.co 
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-7-28
 */
 
 slides = new Array(
