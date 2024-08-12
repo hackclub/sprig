878c915669cc110e02bcc11d3983e864f391ebbe
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-12 23:53:46 +0200

    fixed not all tooltips showing up

--- src/lib/codemirror/autocomplete.tsx
+++ src/lib/codemirror/autocomplete.tsx
@@ -8,16 +8,18 @@ function completeSprigFunctions(context: CompletionContext) {
 	let word = context.matchBefore(/\w*/);
 	if (!word || (word.from == word.to && !context.explicit)) return null;
 
-	function option(label: string, docId: string, info: string) {
+	function option(label: string, docId: string, info?: string) {
+		const docs = document.createElement("div");
+		docs.innerHTML = sprigDocs;
+
 		const reg = new RegExp(
 			`<h3 id="${docId}">([\n]|.)+?(?=<h2|<h3|<!)`,
 			"g"
 		);
-		const match = reg.exec(sprigDocs);
-		console.log(match?.[0]);
+		const match = reg.exec(docs.innerHTML);
 
 		const infoEl = document.createElement("div");
-		infoEl.innerHTML = match ? match[0] : info;
+		infoEl.innerHTML = match ? match[0] : info ?? "";
 
 		return {
 			label,
@@ -30,83 +32,23 @@ function completeSprigFunctions(context: CompletionContext) {
 	return {
 		from: word.from,
 		options: [
-			option(
-				"setLegend()",
-				"setlegendbitmaps",
-				"Tell Sprig what types of sprites are available in your game. Bitmap keys must be a single character. We recommend storing character keys in variables."
-			),
-			option(
-				"setBackground()",
-				"setbackgroundbitmapkey",
-				"Tiles a bitmap as the background of the game. This only changes the visuals of the game."
-			),
-			option(
-				"setMap()",
-				"setmaplevel",
-				"Clears the game and loads a new level."
-			),
-			option(
-				"setSolids()",
-				"setsolidsbitmapkey",
-				"Set the solids of the game."
-			),
-			option(
-				"setPushables()",
-				"setpushablespushmap",
-				"Set the pushables of the game"
-			),
-			option("width()", "width", "Get the width of the current map"),
-			option("height()", "height", "Get the height of the current map"),
-			option(
-				"onInput()",
-				"oninputtype-callback",
-				"Do something when the player presses a control."
-			),
-			option(
-				"afterInput()",
-				"afterinputcallback",
-				"Runs after every input event has finished being handled. Useful for tasks like checking win states."
-			),
-			option(
-				"getTile()",
-				"gettilex-y",
-				"Returns a list of the sprites in the specified tile."
-			),
-			option(
-				"tilesWith()",
-				"tileswithtype",
-				"Returns a list of the tiles that contain type."
-			),
-			option(
-				"addSprite()",
-				"addspritex-y-spritetype",
-				"Creates a new sprite of the given type."
-			),
-			option(
-				"clearTile()",
-				"cleartilex-y",
-				"Removes all sprites from the specified tile."
-			),
-			option(
-				"getAll()",
-				"getalltype",
-				"Returns all sprites of the given type. If no bitmap key is specified, it returns all the sprites in the game."
-			),
-			option(
-				"getFirst()",
-				"getfirsttype",
-				"Returns the first sprite of a given type. Useful if you know there’s only one of a sprite, such as with a player character."
-			),
-			option(
-				"addText()",
-				"addtextstring-options---x-y-color",
-				"Add text to the game"
-			),
-			option(
-				"clearText()",
-				"cleartext",
-				"Clears all text on the screen."
-			),
+			option("setLegend()", "setlegendbitmaps"),
+			option("setBackground()", "setbackgroundbitmapkey"),
+			option("setMap()", "setmaplevel"),
+			option("setSolids()", "setsolidsbitmapkey"),
+			option("setPushables()", "setpushablespushmap"),
+			option("width()", "width"),
+			option("height()", "height"),
+			option("onInput()", "oninputtype-callback"),
+			option("afterInput()", "afterinputcallback"),
+			option("getTile()", "gettilex-y"),
+			option("tilesWith()", "tileswithtype"),
+			option("addSprite()", "addspritex-y-spritetype"),
+			option("clearTile()", "cleartilex-y"),
+			option("getAll()", "getalltype"),
+			option("getFirst()", "getfirsttype"),
+			option("addText()", "addtextstring-options---x-y-color"),
+			option("clearText()", "cleartext"),
 			option("playTune()", "playTune", "Play a tune"),
 
 			option("bitmap``", "bitmap", "Create a bitmap"),
