2f8288d8381ef33449443a573ada75621cb39a0a
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-14 00:15:17 +0200

    made "run" a dropdown/run combo

--- src/components/design-system/button.tsx
+++ src/components/design-system/button.tsx
@@ -1,20 +1,20 @@
-import type { ComponentChild, JSX } from 'preact'
-import type { IconType } from 'react-icons'
-import styles from './button.module.css'
+import type { ComponentChild, JSX } from "preact";
+import type { IconType } from "react-icons";
+import styles from "./button.module.css";
 
 interface ButtonProps {
-	icon?: IconType
-	type?: 'button' | 'submit' | 'reset'
-	iconSide?: 'left' | 'right'
-	bigIcon?: boolean
-	accent?: boolean
-	class?: string | undefined
-	disabled?: boolean
-	spinnyIcon?: boolean
-	loading?: boolean
-	children?: ComponentChild
-	role?: JSX.HTMLAttributes<HTMLButtonElement>['role']
-	onClick?: () => void
+	icon?: IconType;
+	type?: "button" | "submit" | "reset";
+	iconSide?: "left" | "right";
+	bigIcon?: boolean;
+	accent?: boolean;
+	class?: string | undefined;
+	disabled?: boolean;
+	spinnyIcon?: boolean;
+	loading?: boolean;
+	children?: ComponentChild;
+	role?: JSX.HTMLAttributes<HTMLButtonElement>["role"];
+	onClick?: (e: JSX.TargetedMouseEvent<HTMLButtonElement>) => void;
 }
 
 export default function Button(props: ButtonProps) {
@@ -22,20 +22,20 @@ export default function Button(props: ButtonProps) {
 		<button
 			class={`
 				${styles.button}
-				${props.accent ? styles.accent : ''}
-				${props.bigIcon ? styles.bigIcon : ''}
-				${props.spinnyIcon ? styles.spinnyIcon : ''}
-				${props.loading ? styles.loading : ''}
-				${props.class ?? ''}
+				${props.accent ? styles.accent : ""}
+				${props.bigIcon ? styles.bigIcon : ""}
+				${props.spinnyIcon ? styles.spinnyIcon : ""}
+				${props.loading ? styles.loading : ""}
+				${props.class ?? ""}
 			`}
-			role={props.role ?? 'button'}
-			type={props.type ?? 'button'}
+			role={props.role ?? "button"}
+			type={props.type ?? "button"}
 			disabled={!!props.disabled || !!props.loading}
-			onClick={() => props.onClick?.()}
+			onClick={(e) => props.onClick?.(e)}
 		>
-			{props.icon && props.iconSide !== 'right' && <props.icon />}
+			{props.icon && props.iconSide !== "right" && <props.icon />}
 			{props.children}
-			{props.icon && props.iconSide === 'right' && <props.icon />}
+			{props.icon && props.iconSide === "right" && <props.icon />}
 		</button>
-	)
+	);
 }
