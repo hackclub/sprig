3267b96171578feaf1471f8ef018283ba4feee6f
Cheru Berhanu <mail@cheru.dev>
2024-08-13 15:48:47 -0400

    Update love-bug.js

--- games/love-bug.js
+++ games/love-bug.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: love-bug
-@author: 
+@author: b3arora
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-08-13
 */
 
 //too many bombs
