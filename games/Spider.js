51dc8697b6306bd338115c825a4c12dae2009931
Cheru Berhanu <mail@cheru.dev>
2024-08-15 21:54:24 -0400

    Update Spider.js

--- games/Spider.js
+++ games/Spider.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Spider
-@author: 
+@author: AlexGaloy
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-08-15
 */
 
 const spider = "p"
@@ -415,3 +415,4 @@ afterInput(() => {
   lastX = "" + getFirst(spider).x
   lastY = "" + getFirst(spider).y
 })
+
