e5107f0eef09a08c64d36730725ef7d404004491
Marios Mitsios <xrteach@gmail.com>
2024-08-19 14:38:31 -0400

    Update metadata in Push-Peril.js

--- games/Push-Peril.js
+++ games/Push-Peril.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Box Pushing Game
-@author: 
+@author: HEHE-HTML
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-08-18
 */
 
 const player = "p"
