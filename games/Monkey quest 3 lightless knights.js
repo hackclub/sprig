c0a18b7f7f421cfe95b565c335f3ef6043497113
Marios Mitsios <xrteach@gmail.com>
2024-08-20 18:07:38 -0400

    Update metadata in Monkey quest 3 lightless knights.js

--- games/Monkey quest 3 lightless knights.js
+++ games/Monkey quest 3 lightless knights.js
@@ -5,7 +5,7 @@ https://sprig.hackclub.com/gallery/getting_started
 @title: Monkey quest 3: lightless knights
 @author: Eamonn
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-08-20
 */
 
 let r = 0;
