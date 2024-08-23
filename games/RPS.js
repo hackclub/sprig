08a8695c6aee19418462067e5723ac4adda33e5c
Cheru Berhanu <mail@cheru.dev>
2024-08-23 12:01:32 -0400

    Update RPS.js

--- games/RPS.js
+++ games/RPS.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: RPS
-@author: 
+@author: Shahm Najeeb 
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-08-23
 */
 
 const player = "p";
@@ -124,3 +124,4 @@ afterInput(() => {
   const result = determineWinner(playerMove, opponentMove);
   addText(result, { x: 0, y: 0 });
 })
+
