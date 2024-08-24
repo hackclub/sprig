98892b018c6a84df5f5ab1ae7d56f9a73b0a532c
Kushagra Sharma <76271136+CodeOS99@users.noreply.github.com>
2024-08-24 11:44:58 +0530

    Final bugfix and balance change
    The barrels spawn more now since previously it seemed really slow to beat the boss.

--- games/GhoulThrasherFinal.js
+++ games/GhoulThrasherFinal.js
@@ -388,7 +388,7 @@ let buttonCoords = {
 }
 
 let nuclearBarrelCurrent = 0;
-let nuclearBarrelMax = 25;
+let nuclearBarrelMax = 15;
 
 setLegend(
   [skull, bitmap`
@@ -2811,14 +2811,14 @@ setInterval(() => {
     attackCounter++;
     if (houseHealth === phaseTwoChange) {
       bossPhase = 2;
-      nuclearBarrelMax = 35
+      nuclearBarrelMax = 25
     }
     if (houseHealth === phaseThreeChange) {
       bossPhase = 3;
-      nuclearBarrelMax = 45
+      nuclearBarrelMax = 35
     }
     if (houseHealth === phaseFourChange) {
-      nuclearBarrelMax = 55
+      nuclearBarrelMax = 45
     }
     if (attackCounter >= attackCounterMax / bossPhase) {
       attackCounter = 0;
