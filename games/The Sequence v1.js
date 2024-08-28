3a528aa0a54e6fd1fe8ee2c86adfdec694e75179
Cheru Berhanu <mail@cheru.dev>
2024-08-28 17:19:14 -0400

    Update The Sequence v1.js

--- games/The Sequence v1.js
+++ games/The Sequence v1.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: The Sequence
-@author: 
+@author: liaa-dev 
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-08-28
 */
 
 const unrevealed = "b";
@@ -632,3 +632,4 @@ function resetVars() {
     y: 1
   };
 }
+
