0d0babbce8227ed092b54cf1e9efb9c2ffa63170
Cheru Berhanu <mail@cheru.dev>
2024-09-03 10:36:22 -0400

    Update eager_rattata.js

--- games/eager_rattata.js
+++ games/eager_rattata.js
@@ -2,11 +2,10 @@
 First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
-@title: 
-@author: 
+@title: JumperWho
+@author: CreativeDragon1
 @tags: []
-@img: ""
-@addedOn: 2024-00-00
+@addedOn: 2024-09-03
 */
 
 const player = "p"
@@ -384,3 +383,4 @@ setInterval(() => {
     }
   }
 }, 5)
+
