10ce28b7e7ebdcf77e187288bcfad7e196bbf08a
Cheru Berhanu <mail@cheru.dev>
2024-09-03 10:20:43 -0400

    Update JB_Dodge_game.js

--- games/JB_Dodge_game.js
+++ games/JB_Dodge_game.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: JB_Dodgy_Meteor_Game
-@author: 
-@tags: [dodge]
-@addedOn: 2024-00-00
+@author: JbBurnsWasHere
+@tags: ["dodge"]
+@addedOn: 2024-09-03
 */
 
 const player = "p";
@@ -146,3 +146,4 @@ function checkHit() {
   }
   return false;
 }
+
