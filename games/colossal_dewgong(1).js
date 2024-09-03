b141e71ba0eb9f685c3009d93ca3d8e5a05cc22f
Cheru Berhanu <mail@cheru.dev>
2024-09-03 10:41:04 -0400

    Update colossal_dewgong(1).js

--- games/colossal_dewgong(1).js
+++ games/colossal_dewgong(1).js
@@ -2,10 +2,10 @@
 First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
-@title: 
-@author: 
+@title: Journey of the Yeti 
+@author: Andrew Zhu
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-09-03
 */
 
 const player = "p"
@@ -279,3 +279,4 @@ afterInput(() => {
     setMap(levels[level]);
   }
 })
+
