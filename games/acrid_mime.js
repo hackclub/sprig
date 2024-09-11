715746d6e8676b6b8b757f8a69e34c978c2da146
Shubham Panth <GamingCraft52@gmail.com>
2024-09-11 21:12:09 +0200

    Automated Commit - acrid_mime

---
+++ games/acrid_mime.js
@@ -0,0 +1,59 @@
+/*
+First time? Check out the tutorial game:
+https://sprig.hackclub.com/gallery/getting_started
+
+@title: 
+@author: 
+@tags: []
+@img: ""
+@addedOn: 2024-00-00
+*/
+
+const player = "p"
+
+setLegend(
+  [player, bitmap`
+................
+................
+.......000......
+.......0.0......
+......0..0......
+......0...0.0...
+....0003.30.0...
+....0.0...000...
+....0.05550.....
+......0...0.....
+.....0....0.....
+.....0...0......
+......000.......
+......0.0.......
+.....00.00......
+................`]
+)
+
+setSolids([])
+console.log("hey")
+console.error("hey")
+let level = 0
+const levels = [
+  map`
+p.
+..`
+]
+
+setMap(levels[level])
+
+setPushables({
+  [player]: []
+})
+
+onInput("s", () => {
+  getFirst(player).y += 1
+})
+onInput("w", () => {
+  getFirst(player).y -= 1
+})
+
+afterInput(() => {
+
+})
