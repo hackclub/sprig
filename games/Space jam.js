9cf61bef7242eb6029e7591c0a30b494e576254f
Cheru Berhanu <mail@cheru.dev>
2024-09-17 14:20:14 -0400

    Update Space jam.js

--- games/Space jam.js
+++ games/Space jam.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Space jam
-@author: 
+@author: Praveenkushinpi 
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-09-17
 */
 const rocket = "r";
 const space = "s";
