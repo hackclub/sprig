4a3c19ee42521cff77927890038b56249a57d551
Cheru Berhanu <mail@cheru.dev>
2024-09-27 12:26:08 -0400

    Update dragon.js

--- games/dragon.js
+++ games/dragon.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Hard Game
-@author: 
+@author: VoidDevX1
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-09-27
 */
 
 
