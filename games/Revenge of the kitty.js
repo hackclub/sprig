8e2a435c990c5fb4aa751569d0211d3e3b8cf6f4
Cheru Berhanu <mail@cheru.dev>
2024-09-27 12:25:47 -0400

    Update Revenge of the kitty.js

--- games/Revenge of the kitty.js
+++ games/Revenge of the kitty.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Battle of the colors
-@author: 
+@author: Pugx23
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-09-27
 */
 
 const player = "p"
