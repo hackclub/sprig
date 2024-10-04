5e057883efc8e405a3aae6c89b6778fadc342dad
Graham Darcey <graham@hackclub.com>
2024-10-04 11:47:42 -0400

    Fixes comments in plag check

--- .github/scripts/plagiarism_check.py
+++ .github/scripts/plagiarism_check.py
@@ -8,7 +8,6 @@ import argparse
 import re
 import random
 
-
 def load_files_from_dir(dir, suffix):
 	all_game_paths = []
 	for (dirpath, dirnames, filenames) in walk(dir):
@@ -24,7 +23,7 @@ def load_data(path):
 	dlen = len(dd)
 	if dd is None or dlen == 0:
 		dd = ""
-
+	
 	return preprocess(dd)
 
 
@@ -45,7 +44,7 @@ def filter_code(code):
 	code = re.sub('tune`.*?`', '', code, flags=re.MULTILINE | re.DOTALL)
 	code = re.sub('map`.*?`', '', code, flags=re.MULTILINE | re.DOTALL)
 	code = re.sub('^/\\*(.|[\r\n])*?\\*/', '', code, flags=re.DOTALL)
-	code = re.sub('(//)(.+?)(?=[\n\r]|\\*\\))', '', code, flags=re.MULTILINE | re.DOTALL)
+
 	return code
 
 
@@ -56,32 +55,28 @@ def preprocess(code):
 def text_length_score(text1, text2):
 	return 1 - abs(len(text1) - len(text2)) / (len(text1) + len(text2))
 
-
 def tokenize(text, num_tokens):
 	return set(nltk.ngrams(text.lower().split(" "), num_tokens))
-
 
 def symmetrical_diff_score(text1, text2, num_tokens):
 	set1 = tokenize(text1, num_tokens)
 	set2 = tokenize(text2, num_tokens)
 	symmetrical_difference = set1.symmetric_difference(set2)
-	return 1.0 - (len(symmetrical_difference) / (len(set1) + len(set2)))
+	return 1.0 - (len(symmetrical_difference) / (len(set1)+len(set2)))
 
 
 def compare(document, other_document):
 	len_score = text_length_score(document, other_document)
-
+	
 	return len_score * 0.1 + \
 		symmetrical_diff_score(document, other_document, 1) * 0.2 + \
 		symmetrical_diff_score(document, other_document, 2) * 0.3 + \
 		symmetrical_diff_score(document, other_document, 3) * 0.4
 
-
 class DocumentComparison:
 	def __init__(self, path, score):
-		self.path = path
+		self.path = path		
 		self.score = score
-
 
 def create_doc_comparison(item):
 	return DocumentComparison(item[2], compare(item[0], item[1]))
@@ -118,7 +113,7 @@ def find_matching_docs(input_doc_path, all_games, threshold, log):
 				print("Here are the top %d matches" % len(top_matches))
 			for document in top_matches:
 				print("%f - %s" % (document.score, document.path))
-
+		
 		return len(documents_exceeding_threshold)
 
 
@@ -138,14 +133,12 @@ def check_all_games():
 
 
 if __name__ == '__main__':
-	# run_tests()
-	# check_all_games()
+	#run_tests()
+	#check_all_games()
 
-	parser = argparse.ArgumentParser(
-		description='Compare an input javascript file w/ the contents of a directory, and returns similarity scores')
+	parser = argparse.ArgumentParser(description='Compare an input javascript file w/ the contents of a directory, and returns similarity scores')
 	parser.add_argument('doc_dir', type=str, help='a path to a directory of documents')
-	parser.add_argument('threshold', type=float,
-						help='similarity threshold (above which duplicate warnings will be returned)')
+	parser.add_argument('threshold', type=float, help='similarity threshold (above which duplicate warnings will be returned)')
 	parser.add_argument('input_doc', type=str, help='a path to an input document')
 	args = parser.parse_args()
 
