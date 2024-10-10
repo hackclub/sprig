472c64feb5c4eeba2586e39ebe90abb03c6985ce
Alex Park <parkalex380@gmail.com>
2024-10-10 15:06:28 -0500

    Update metadata

--- games/Tug-of-War.js
+++ games/Tug-of-War.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Tug-of-War
-@author: 
-@tags: [multiplayer]
-@addedOn: 2024-00-00
+@author: liamcharger
+@tags: ['multiplayer']
+@addedOn: 2024-10-10
 */
 
 const player = "p"
