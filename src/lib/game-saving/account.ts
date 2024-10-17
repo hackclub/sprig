f6be744c4718affb51f73f922db1c5754c31a8bb
Cosmin-Mare <cosmintudor.mare@cnamd.onmicrosoft.com>
2024-10-17 16:25:44 +0300

    add function to move game to backend saving

--- src/lib/game-saving/account.ts
+++ src/lib/game-saving/account.ts
@@ -59,6 +59,7 @@ export interface Game {
 	code: string
 	tutorialName?: string
 	tutorialIndex?: number
+	isSavedOnBackend?: boolean
 	roomParticipants?: RoomParticipant[]
 	isRoomOpen?: boolean
 	password?: string
@@ -255,7 +256,7 @@ export const getGame = async (id: string | undefined): Promise<Game | null> => {
 	return { id: _game.id, ..._game.data() } as Game
 }
 
-export const makeGame = async (ownerId: string, unprotected: boolean, name?: string, code?: string, tutorialName?: string, tutorialIndex?: number): Promise<Game> => {
+export const makeGame = async (ownerId: string, unprotected: boolean, name?: string, code?: string, tutorialName?: string, tutorialIndex?: number, isSavedOnBackend?: boolean): Promise<Game> => {
 	
 	const createdDate = Timestamp.now()
 	const data = {
@@ -266,7 +267,8 @@ export const makeGame = async (ownerId: string, unprotected: boolean, name?: str
 		name: name ?? generateGameName(),
 		code: code ?? '',
 		tutorialName: tutorialName ?? null,
-		tutorialIndex: tutorialIndex ?? null
+		tutorialIndex: tutorialIndex ?? null,
+		isSavedOnBackend: isSavedOnBackend ?? false,
 	}
 	const _game = await addDocument('games', data);
 	return { id: _game.id, ...data } as Game
@@ -337,4 +339,8 @@ export const getSnapshotData = async (id: string): Promise<SnapshotData | null> 
 
 export const updateUserGitHubToken = async (userId: string, githubAccessToken: string, githubId: string, githubUsername: string): Promise<void> => {
     await updateDocument('users', userId, { githubAccessToken, githubId, githubUsername });
+}
+
+export const moveGameToBackendSaving = async (game: Game): Promise<Game> => {
+	return makeGame(game.ownerId, game.unprotected, game.name, game.code, game.tutorialName, game.tutorialIndex, true);
 }
