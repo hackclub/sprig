5eaa7cb89b4aebcebaa156ca7544e75abc7f2e50
Mare Cosmin <147330889+Cosmin-Mare@users.noreply.github.com>
2024-10-30 19:37:05 +0200

    fix metadata

--- games/Apple_Drop.js
+++ games/Apple_Drop.js
@@ -2,10 +2,10 @@
 First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
-@title: game
-@author: 
+@title: Apple_Drop
+@author: wvminecraftkid
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-10-30
 */
 
 const player = "p"
