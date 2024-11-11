398bb950eced50acdb891939142b91dff13788e1
Gus Ruben <95830851+gusruben@users.noreply.github.com>
2024-11-11 15:20:19 -0500

    Set 'added on' field

--- games/Brix-and-Boxes.js
+++ games/Brix-and-Boxes.js
@@ -1,11 +1,8 @@
 /*
-First time? Check out the tutorial game:
-https://sprig.hackclub.com/gallery/getting_started
-
 @title: Bricks And Boxes
 @author: Amino47
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-11-11
 */
 
 const player = "p"
