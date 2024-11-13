d237c5e33c8b33fcd7812674d81b2f4369b6e1f6
Alex Park <parkalex380@gmail.com>
2024-11-12 20:19:28 -0600

    Update metadata

--- games/Pixeltron-6000.js
+++ games/Pixeltron-6000.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Pixeltron 6000
-@author: 
-@tags: []
-@addedOn: 2024-00-00
+@author: salping
+@tags: ['sandbox', 'utility']
+@addedOn: 2024-11-12
 */
 
 const player = "w"
