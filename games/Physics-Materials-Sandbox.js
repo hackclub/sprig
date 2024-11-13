e263b3786e5631b25185aed940c935659b96b608
Alex Park <parkalex380@gmail.com>
2024-11-12 18:15:59 -0600

    Update metadata

--- games/Physics-Materials-Sandbox.js
+++ games/Physics-Materials-Sandbox.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Pixel Box
-@author: 
-@tags: []
-@addedOn: 2024-00-00
+@author: GriffinHengelsberg
+@tags: ['sandbox', 'simulation']
+@addedOn: 2024-11-12
 */
 
 var tickTime = 100; //miliseconds
