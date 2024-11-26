b47e1fdc4fc17f654811a4af645af101d7596ecd
Alex Park <parkalex380@gmail.com>
2024-11-25 18:35:41 -0600

    Update metadata

--- games/RaycastingGame.js
+++ games/RaycastingGame.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: RaycastingGame
-@author: 
+@author: Simone2011
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-11-25
 */
 const player = "l"
 const pen = "p"
