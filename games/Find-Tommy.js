9aa8e21887c4ccea84398503085b25cc66a10478
Gus Ruben <95830851+gusruben@users.noreply.github.com>
2024-11-25 22:12:00 -0500

    Add Metadata

--- games/Find-Tommy.js
+++ games/Find-Tommy.js
@@ -1,11 +1,8 @@
 /*
-First time? Check out the tutorial game:
-https://sprig.hackclub.com/gallery/getting_started
-
 @title: Find-Tommy!
-@author: 
+@author: Mysteryman386
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-11-20
 */
 
 const player = "p"
