5ed4701aa02131f9fced71258ff5a9c876681561
Gus Ruben <95830851+gusruben@users.noreply.github.com>
2024-12-04 01:42:51 -0500

    Fix metadata

--- games/catch-the-banana.js
+++ games/catch-the-banana.js
@@ -1,11 +1,8 @@
 /*
-First time? Check out the tutorial game:
-https://sprig.hackclub.com/gallery/getting_started
-
 @title: Catch The Banana
 @author: Sayed Hashim
 @tags: ["timed", "retro"]
-@addedOn: 2024-00-00
+@addedOn: 2024-11-29
 */
 
 const player = "p";
