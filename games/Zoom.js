5a03962e77852d57ea574aa8537ad57052e4e408
Aperaine <127672703+Aperaine@users.noreply.github.com>
2024-12-20 18:57:12 +0900

    Update Zoom.js

--- games/Zoom.js
+++ games/Zoom.js
@@ -3,52 +3,20 @@ Use WASD to move
 Use J to restart a level
 @title: Zoom
 @author: Aperaine
-@tags: ['puzzle', 'music']
-@addedOn: 2024-11-05
+@tags: [puzzle, quick, movement]
+@addedOn: 2024-00-00
 */
 
 const player = "p"
 const wall = "w"
+const wall1 = "W"
 const background = "b"
+const background1 = "B"
 const goal = "g"
 let direction = "down"
 let moving = false
 
 //Music
-const melody = tune`
-150: G4^150 + B5~150,
-150: D5^150 + G5~150,
-150: G4^150 + B5~150,
-150: D5^150 + G5~150,
-150: G4^150 + B5~150,
-150: D5^150 + G5~150,
-150: G4^150 + B5~150,
-150: D5^150 + G5~150,
-150: D4^150 + D5~150,
-150: A4^150 + D5~150,
-150: D4^150 + D5~150,
-150: A4^150 + D5~150,
-150: D4^150 + D5~150,
-150: A4^150 + D5~150,
-150: D4^150 + D5~150,
-150: A4^150 + D5~150,
-150: E4^150 + G5~150,
-150: B4^150 + E5~150,
-150: E4^150 + G5~150,
-150: B4^150 + E5~150,
-150: E4^150 + G5~150,
-150: B4^150 + E5~150,
-150: E4^150 + G5~150,
-150: B4^150 + E5~150,
-150: C4^150 + C5~150,
-150: G4^150 + C5~150,
-150: C4^150 + C5~150,
-150: G4^150 + C5~150,
-150: C4^150 + C5~150,
-150: G4^150 + C5~150,
-150: C4^150 + C5~150,
-150: G4^150 + C5~150`
-const playback = playTune(melody, Infinity)
 const levelComplete = tune`
 100: C5-100,
 100: G5-100,
@@ -84,7 +52,7 @@ const gameComplete = tune`
 750`
 
 setLegend(
-  [ player, bitmap`
+  [player, bitmap`
 0000000000000000
 0444444444444440
 0444444444444440
@@ -100,9 +68,9 @@ setLegend(
 0440000000000440
 0444444444444440
 0444444444444440
-0000000000000000` ],
-  [ wall, bitmap`
-D00000000000000D
+0000000000000000`],
+  [wall, bitmap`
+.00000000000000.
 0D000000000000D0
 00D0000000000D00
 000D00000000D000
@@ -117,8 +85,25 @@ D00000000000000D
 000D00000000D000
 00D0000000000D00
 0D000000000000D0
-D00000000000000D`],
-  [ background, bitmap`
+.00000000000000.`],
+  [wall1, bitmap`
+.00000000000000.
+0C000000000000C0
+00C0000000000C00
+000C00000000C000
+0000C000000C0000
+00000C0000C00000
+000000C00C000000
+0000000CC0000000
+0000000CC0000000
+000000C00C000000
+00000C0000C00000
+0000C000000C0000
+000C00000000C000
+00C0000000000C00
+0C000000000000C0
+.00000000000000.`],
+  [background, bitmap`
 FFFFDDDDDDDDFFFF
 FFFDDDDDDDDDDFFF
 FFDDDDDDDDDDDDFF
@@ -135,7 +120,24 @@ FDDDDDDDDDDDDDDF
 FFDDDDDDDDDDDDFF
 FFFDDDDDDDDDDFFF
 FFFFDDDDDDDDFFFF`],
-  [ goal, bitmap`
+  [background1, bitmap`
+HHHHCCCCCCCCHHHH
+HHHCCCCCCCCCCHHH
+HHCCCHHHHHHCCCHH
+HCCHHHHHHHHHHCCH
+CCCHHCCCCCCHHCCC
+CCHHCCCCCCCCHHCC
+CCHHCCHHHHCCHHCC
+CCHHCCHHHHCCHHCC
+CCHHCCHHHHCCHHCC
+CCHHCCHHHHCCHHCC
+CCHHCCCCCCCCHHCC
+CCCHHCCCCCCHHCCC
+HCCHHHHHHHHHHCCH
+HHCCCHHHHHHCCCHH
+HHHCCCCCCCCCCHHH
+HHHHCCCCCCCCHHHH`],
+  [goal, bitmap`
 ...........2....
 ..........222...
 .........662....
@@ -273,13 +275,49 @@ wgw..........w
 w.ww.........w
 w......w.....w
 w......w.....w
-wwwwwwwwwwwwww`
+wwwwwwwwwwwwww`,
+  map`
+WWWWWWWWWWWWWW
+W.........gggW
+W.........WWWW
+W............W
+W............W
+W............W
+W............W
+Wp...........W
+Wp...........W
+Wp...........W
+WWWWWWWWWWWWWW`,
+  map`
+WWWWWWWWWWWWWW
+WWWW......pWWW
+WWWg......gWWW
+WpgW......WgWW
+W...W....W...W
+W....WWWW....W
+W...W....W...W
+WWgW......WgpW
+WWWg......gWWW
+WWWp......WWWW
+WWWWWWWWWWWWWW`,
+  map`
+WWWWWWWWWWWWWW
+WW..........WW
+W............W
+W.WWWWgWWgW..W
+W.gWgWWWgWg..W
+W.gWgWgWWgW..W
+Wp...........W
+Wp...W.......W
+WW...........W
+WW..........WW
+WWWWWWWWWWWWWW`
 ]
 
 
 setMap(levels[level])
 setBackground(background)
-addText("WASD to move\nJ to reset", { x:3,y: 4, color: color`2` });
+addText("WASD to move\nJ to reset", { x: 3, y: 4, color: color`2` });
 
 onInput("s", () => {
   direction = "down"
@@ -308,48 +346,50 @@ onInput("j", () => {
 })
 
 const movePlayer = () => {
-  const goalCovered = tilesWith(goal,player).length;
+  let playerMoved = true;
 
-  if (goalCovered === tilesWith(player).length){
+  while (playerMoved) {
+    playerMoved = false;
+
+    const playerChars = getAll(player);
+    let canMove = playerChars.length;
+
+    for (const playerChar of playerChars) {
+      const nextX = playerChar.x + (direction === "left" ? -1 : direction === "right" ? 1 : 0);
+      const nextY = playerChar.y + (direction === "up" ? -1 : direction === "down" ? 1 : 0);
+
+      if (!getTile(nextX, nextY).some(tile => tile.type == wall) && !getTile(nextX, nextY).some(tile => tile.type == wall1) && !getTile(nextX, nextY).some(tile => tile.type == player)) {
+        playerChar.x = nextX;
+        playerChar.y = nextY;
+
+        playerMoved = true;
+      } else {
+        canMove -= 1;
+      }
+    }
+
+    if (canMove == 0) {
+      break;
+    }
+
+  }
+
+  const goalCovered = tilesWith(goal, player).length;
+
+  if (goalCovered === tilesWith(player).length) {
     level += 1;
     const currentLevel = levels[level];
     if (currentLevel !== undefined) {
-      clearText()
+      clearText();
       setMap(currentLevel);
-      playTune(levelComplete)
+      playTune(levelComplete);
+      if (level > 9){
+        setBackground(background1)
+      }
     } else {
       addText("you win!", { y: 4, color: color`2` });
-      playback.end()
-      playTune(gameComplete)
+      playTune(gameComplete);
     }
   }
-
-  const player1Char = getFirst(player)
-  const nextX1 = player1Char.x + (direction === "left" ? -1 : direction === "right" ? 1 : 0)
-  const nextY1 = player1Char.y + (direction === "up" ? -1 : direction === "down" ? 1 : 0)
-
-  if (tilesWith(player).length > 1){
-
-    const player2Char = getAll(player)[1]
-    const nextX2 = player2Char.x + (direction === "left" ? -1 : direction === "right" ? 1 : 0)
-    const nextY2 = player2Char.y + (direction === "up" ? -1 : direction === "down" ? 1 : 0)
-
-    if (!getTile(nextX1, nextY1).some(tile => tile.type == wall) && !getTile(nextX1, nextY1).some(tile => tile.type == player) || !getTile(nextX2, nextY2).some(tile => tile.type == wall) && !getTile(nextX2, nextY2).some(tile => tile.type == player)){
-      player1Char.x = nextX1
-      player1Char.y = nextY1
-
-      player2Char.x = nextX2
-      player2Char.y = nextY2
-
-      movePlayer()
-    }
-  }
-  else {
-    if (!getTile(nextX1, nextY1).some(tile => tile.type == wall) && !getTile(nextX1, nextY1).some(tile => tile.type == player)){
-      player1Char.x = nextX1
-      player1Char.y = nextY1
-
-      movePlayer()
-    }
-  }
+  
 }
