2ecceb9c732ba4d992aeb2528444c1c431400deb
Gus Ruben <95830851+gusruben@users.noreply.github.com>
2024-12-20 13:09:07 -0500

    Add metadata

--- games/Genius-Hour.js
+++ games/Genius-Hour.js
@@ -1,11 +1,8 @@
 /*
-First time? Check out the tutorial game:
-https://sprig.hackclub.com/gallery/getting_started
-
-@title: 
-@author: 
+@title: Genius Hour 
+@author: GangleTADC
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-12-13
 */
 
 const player = "p"
