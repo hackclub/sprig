a191198f813426791c479151be347d701c32b81e
yug-khandelwal <hereisyug@gmail.com>
2024-12-30 19:44:12 -0500

    Update flappy-bird-with-gravity-and-sound.js

--- games/flappy-bird-with-gravity-and-sound.js
+++ games/flappy-bird-with-gravity-and-sound.js
@@ -2,7 +2,7 @@
 First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
-@title: Flappy Bird
+@title: flappy-bird-with-gravity-and-sound
 @author: Yug Khandelwal
 @addedOn: 2024-12-24
 */
