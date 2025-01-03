114388b9fc9680c783bd34586cdef4605974cbce
graham <graham@hackclub.com>
2025-01-03 09:01:02 -0500

    Fixing metadata

--- games/ClassicPinPon.js
+++ games/ClassicPinPon.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: PinPon
-@author: 
+@author: pmm-inf
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2025-01-03
 */
 
 const player1 = "1"
