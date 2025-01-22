45b134c29c008116091d04cb08767b5f6036cc36
Mare Cosmin <147330889+Cosmin-Mare@users.noreply.github.com>
2025-01-22 19:42:02 +0200

    Update subway-surfish.js

--- games/subway-surfish.js
+++ games/subway-surfish.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: subway surfish
-@author: 
+@author: scottwellcorn
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-01-22
 */
 const player = "p"
 const wall = "w"
