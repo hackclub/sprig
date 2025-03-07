b93ee0c55a4978ec390188c32467ca9a78a9b795
graham <graham@hackclub.com>
2025-03-07 13:27:00 -0500

    Fixing metadata

--- games/adventure.js
+++ games/adventure.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: adventure
-@author: 
+@author: Loshua1
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2025-03-07
 */
 
 const bandit = "a"
