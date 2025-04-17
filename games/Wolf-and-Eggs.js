38d660893d305e6b016be60a631c1fefa37f5a4e
diantoniuksc <162093949+diantoniuksc@users.noreply.github.com>
2025-04-17 14:07:52 +0200

    Update Wolf-and-Eggs.js

--- games/Wolf-and-Eggs.js
+++ games/Wolf-and-Eggs.js
@@ -3,47 +3,65 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: Wolf and Eggs
-@author: 
+@author: diantoniuksc
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2025-04-17
 */
 
 const wolf = "w"
 const eggSprite = "e"
+const stoneSprite = "s"
 
 setLegend(
   [wolf, bitmap`
-000000....000000
-......0...0.....
-......00000.....
-000....0.0......
-0.00..0..0..000.
-0..00.0...0.0.00
-0...0003.30.0..0
-0...0.0...000..0
-00..0.05550....0
-.0....0...0....0
-.0...0....0...00
-.00..0...0....0.
-..000.000000..0.
-......0.0..0000.
-.....00.00......
-................`],
+......000000......
+.....00000000.....
+....0000000000....
+...000000000000...
+...000020200000...
+...000000000000...
+....0000000000....
+..000000000000....
+00000000000000....
+00000000000000....
+.....000000000....
+.....000000000....
+00000000000000....
+..000000000000....
+...00000000000....
+.....00000000.....`],
   [eggSprite, bitmap`
 ................
 ................
 ................
 ................
-................
-................
-................
 .......00.......
-.....0....0.....
-....0......0....
-....0......0....
-....0......0....
-.....0....0.....
+.....000000.....
+....00....00....
+...00......00...
+...00......00...
+...00......00...
+....00....00....
+.....000000.....
 .......00.......
+................
+................
+................`],
+  [stoneSprite, bitmap`
+................
+................
+................
+................
+.....000000.....
+...00......00...
+..0..00..00..0..
+..0...0000...0..
+..0....00....0..
+..0...0000...0..
+..0..0....0..0..
+...00......00...
+.....000000.....
+................
 ................
 ................`]
 )
@@ -51,24 +69,22 @@ setLegend(
 setSolids([])
 
 let score = 0
+let gameOver = false
 
-// Egg spawn and fall behavior
 const eggPaths = [
-  { x: 1, spawnY: 0, catchY: 6 },  // Left side
-  { x: 6, spawnY: 0, catchY: 6 }   // Right side
+  { x: 1, spawnY: 0, catchY: 6 },
+  { x: 6, spawnY: 0, catchY: 6 }
 ]
 
 let wolfPos = 0
-let eggs = []
+let fallingItems = [] // now contains both eggs and stones
 
-// Place the wolf at the correct position
 function placeWolf() {
   getAll(wolf).forEach(w => w.remove())
   const pos = eggPaths[wolfPos]
   addSprite(pos.x, pos.catchY, wolf)
 }
 
-// Set up the map (empty background)
 setMap(map`
 ........
 ........
@@ -79,59 +95,77 @@ setMap(map`
 ........`)
 placeWolf()
 
-// Control inputs to move the wolf
-onInput("a", () => { wolfPos = 0; placeWolf() }) // Move left
-onInput("d", () => { wolfPos = 1; placeWolf() }) // Move right
+onInput("a", () => { if (!gameOver) { wolfPos = 0; placeWolf() } })
+onInput("d", () => { if (!gameOver) { wolfPos = 1; placeWolf() } })
 
-// Function to spawn a new egg
-function spawnEgg() {
-  const i = Math.floor(Math.random() * 2)  // Randomly choose left or right
+function spawnItem() {
+  const i = Math.floor(Math.random() * 2)
+  const isEgg = Math.random() < 0.7 // 70% chance for egg, 30% for stone
   const { x, spawnY } = eggPaths[i]
-  eggs.push({ path: i, x: x, y: spawnY })
+  fallingItems.push({
+    type: isEgg ? "egg" : "stone",
+    path: i,
+    x: x,
+    y: spawnY
+  })
 }
 
-// Function to update all eggs
-function updateEggs() {
-  eggs.forEach(egg => egg.y += 1)  // Move eggs down by 1 unit
+function updateItems() {
+  fallingItems.forEach(item => item.y += 1)
 
-  for (let i = eggs.length - 1; i >= 0; i--) {
-    const egg = eggs[i]
-    const end = eggPaths[egg.path]
-    
-    // If the egg has reached or passed the catchY
-    if (egg.y >= end.catchY) {
-      const wolfSpot = eggPaths[wolfPos]
-      
-      // If the egg is caught
-      if (egg.x === wolfSpot.x && egg.y === wolfSpot.catchY) {
-        score++  // Increase score on catch
+  for (let i = fallingItems.length - 1; i >= 0; i--) {
+    const item = fallingItems[i]
+    const end = eggPaths[item.path]
+    const wolfSpot = eggPaths[wolfPos]
+
+    if (item.y >= end.catchY) {
+      if (item.x === wolfSpot.x && item.y === wolfSpot.catchY) {
+        if (item.type === "egg") {
+          score++
+        } else if (item.type === "stone") {
+          endGame("You caught a stone!\nGAME OVER\nScore:" + score)
+          return
+        }
       } else {
-        score = 0  // Reset score if the egg is missed
+        if (item.type === "egg") {
+          score = 0
+        }
       }
 
       clearText()
       addText(`Score: ${score}`, { x: 1, y: 0, color: color`3` })
-
-      // Remove the egg after processing (caught or missed)
-      eggs.splice(i, 1)
+      fallingItems.splice(i, 1)
     }
   }
 }
 
-// Function to draw all falling eggs on the screen
-function drawEggs() {
-  getAll(eggSprite).forEach(e => e.remove())  // Remove existing eggs
-  eggs.forEach(egg => addSprite(egg.x, egg.y, eggSprite))  // Draw the eggs
+function drawItems() {
+  getAll(eggSprite).forEach(e => e.remove())
+  getAll(stoneSprite).forEach(s => s.remove())
+
+  fallingItems.forEach(item => {
+    const sprite = item.type === "egg" ? eggSprite : stoneSprite
+    addSprite(item.x, item.y, sprite)
+  })
 }
 
-// Main game loop to spawn, update and draw eggs
+function endGame(msg) {
+  gameOver = true
+  clearText()
+  addText(msg, { x: 1, y: 3, color: color`3` })
+}
+
 setInterval(() => {
-  if (Math.random() < 0.7) spawnEgg()  // Random egg spawn
-  updateEggs()  // Update positions of eggs
-  drawEggs()    // Draw updated eggs
+  if (gameOver) return
+
+  if (Math.random() < 0.7) spawnItem()
+  updateItems()
+  drawItems()
 }, 600)
 
-// Update score display after each move
 afterInput(() => {
-
+  if (!gameOver) {
+    clearText()
+    addText(`Score: ${score}`, { x: 1, y: 0, color: color`3` })
+  }
 })
