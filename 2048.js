0189824af3c945d16b67300ba948747be1aadd87
Waleed Ajmal <145844316+Ibne-billa@users.noreply.github.com>
2025-08-04 20:57:21 +0500

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -3,9 +3,9 @@ First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
 @title: 2048- new
-@author: 
-@tags: []
-@addedOn: 2025-00-00
+@author: Waleed Ajmal
+@tags: [puzzle,logic]
+@addedOn: 2025-08-04
 */
 
 const player = "p"
