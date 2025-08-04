2b30dd4de6494718aee2d04e743164a235af6c2c
A41K <95142989+A41K@users.noreply.github.com>
2025-08-04 18:36:37 +0200

    Changed the players textures, changed enemy -> player2, Fixed the tags and metadata

--- games/RogueLite.js
+++ games/RogueLite.js
@@ -1,12 +1,12 @@
 /*
-@title: RogueLhttps://sprig.hackclub.com/~ite
+@title: RogueLite
 @author: A41K
-@tags: [CTF]
+@tags: [#CTF, #puzzle #twoplayer]
 @addedOn: 2025-08-01
 */
 
-const player = "p";
-const enemy = "e";
+const player1 = "p";
+const player2 = "e";
 const fakegoal = "f";
 const box = "b";
 const goal = "g";
@@ -17,39 +17,39 @@ const help= "j";
 const ty= "t";
 
 setLegend(
-  [ player, bitmap`
+  [ player1, bitmap`
 ................
-.LLLLLLLLLLLLLL.
-.L000000000000L.
-.L022200002220L.
-.L022000002200L.
-.L022200002220L.
-.L000000000000L.
-.L000000000000L.
-.L000000000000L.
-.L020000000020L.
-.L022000000220L.
-.L002222222200L.
-.L000000000000L.
-.L000000000000L.
-.LLLLLLLLLLLLLL.
+..333333333333..
+.33333333333333.
+.33222333322233.
+.33200333300233.
+.33200333300233.
+.33333333333333.
+.33333333333333.
+.33333333333333.
+.30333333333303.
+.30033333333003.
+.33003333330033.
+.33300000000333.
+.33333333333333.
+..333333333333..
 ................` ],
-  [ enemy,  bitmap`
+  [ player2, bitmap`
 ................
-.33333333333333.
-.30000000000003.
-.30CC000000CC03.
-.300CC0000CC003.
-.30022200222003.
-.30020200202003.
-.30022200222003.
-.30000000000003.
-.30000000000003.
-.30022222222003.
-.30020000002003.
-.30000000000003.
-.30000000000003.
-.33333333333333.
+..777777777777..
+.77777777777777.
+.77222777722277.
+.77200777700277.
+.77200777700277.
+.77777777777777.
+.77777777777777.
+.77777777777777.
+.70777777777707.
+.70077777777007.
+.77007777770077.
+.77700000000777.
+.77777777777777.
+..777777777777..
 ................` ],
   [ goal,  bitmap`
 ......L133......
@@ -172,7 +172,7 @@ setLegend(
 ................`],
 )
 
-setSolids([player, enemy, wall]);
+setSolids([player1, player2, wall]);
 
 let level = 0
 let scorePlayer1 = 0;
@@ -390,70 +390,70 @@ if (shouldPlaySecret) {
 setMap(levels[level])
 
 setPushables({
-  [player]: [],
-  [enemy]: []
+  [player1]: [],
+  [player2]: []
 });
 
 // Player 1 (WASD Controls)
 onInput("s", () => {
-  getFirst(player).y += 1
+  getFirst(player1).y += 1
 })
 
 onInput("w", () => {
-  getFirst(player).y -= 1
+  getFirst(player1).y -= 1
 })
 
 onInput("a", () => {
-  getFirst(player).x -= 1
+  getFirst(player1).x -= 1
 })
 
 onInput("d", () => {
-  getFirst(player).x += 1
+  getFirst(player1).x += 1
 })
 
 // Player 2 (Arrow Keys Controls)
 onInput("k", () => {
-  getFirst(enemy).y += 1
+  getFirst(player2).y += 1
 })
 
 onInput("i", () => {
-  getFirst(enemy).y -= 1
+  getFirst(player2).y -= 1
 })
 
 onInput("j", () => {
-  getFirst(enemy).x -= 1
+  getFirst(player2).x -= 1
 })
 
 onInput("l", () => {
-  getFirst(enemy).x += 1
+  getFirst(player2).x += 1
 })
 
 function updateScores(winner) {
-  if (winner === player) {
+  if (winner === player1) {
     scorePlayer1 += 1;
-  } else if (winner === enemy) {
+  } else if (winner === player2) {
     scorePlayer2 += 1;
   }
 }
 
 function checkWin() {
-  const playerTile = getTile(getFirst(player).x, getFirst(player).y);
-  const enemyTile = getTile(getFirst(enemy).x, getFirst(enemy).y);
+  const player1Tile = getTile(getFirst(player1).x, getFirst(player1).y);
+  const player2Tile = getTile(getFirst(player2).x, getFirst(player2).y);
 
-  if (playerTile.some(sprite => sprite.type === goal)) {
+  if (player1Tile.some(sprite => sprite.type === goal)) {
     addText("Player1 Won!", { x: 5, y: 5, color: color`7` });
     console.log("Player 1 wins!");
-    updateScores(player);
+    updateScores(player1);
     level++; // Increment the level
     if (level < levels.length) {
       setMap(levels[level]);
     }
   }
 
-  if (enemyTile.some(sprite => sprite.type === goal)) {
+  if (player2Tile.some(sprite => sprite.type === goal)) {
     addText("Player2 Won!", { x: 5, y: 5, color: color`3` });
     console.log("Player 2 wins!");
-    updateScores(enemy);
+    updateScores(player2);
     level++; // Increment the level
     if (level < levels.length) {
       setMap(levels[level]);
