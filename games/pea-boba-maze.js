f7ecc789e3e446c11d73799fff46a4071b6b0cb3
Kailey Liou <kaileyliou09@gmail.com>
2025-09-14 21:21:19 -0400

    Update pea-boba-maze.js

--- games/pea-boba-maze.js
+++ games/pea-boba-maze.js
@@ -1,14 +1,15 @@
 /* 
 @title: pea-boba-maze
-@description: My game has 14 levels of different mazes that increasingly get more challenging. The goal is to get the green pea to the boba.
+@description: A simple maze game where you get the green pea to the boba.
 @author: Kailey Liou
 @tags: []
-@addedOn: 2025-08-04
+@addedOn: 2025-07-26
 */
 
 const player = "p"
 const wall = "w";
 const goal = "g";
+const trap = "t";
 const myTune1 = tune`
 500: C5~500,
 15500`;
@@ -21,6 +22,22 @@ const myTune3 = tune`
 const myTune4 = tune`
 500: C5/500,
 15500`;
+const stepLimits = [
+  6, // Level 0
+  7, // Level 1
+  14, // Level 2
+  17, // Level 3
+  9, // Level 4
+  31, // Level 5
+  13, // Level 6
+  33, // Level 7
+  17, // Level 8
+  13, // Level 9
+  9, // Level 10
+  15, // Level 11
+  51, // Level 12
+  28  // Level 13
+];
 
 setLegend(
 	[ player, bitmap`
@@ -73,6 +90,23 @@ HHHHHHHHHHHHHHHH`],
 ....CC0CC0C0C...
 .....CCC0CCC....
 ......C0CC0.....
+................`],
+    [ trap, bitmap`
+................
+................
+................
+.......7........
+.....8.7.D......
+.....11711......
+....1117111.....
+...111111111....
+..11101110111...
+..11118881111...
+..11111111111.8.
+...111617111.88.
+....11111118888.
+.....11111......
+.....D...8......
 ................`]
 );
 
@@ -104,12 +138,12 @@ g..w..
 .w....
 ....w.`,
     map`
-w.......w..
-..www.w...w
+w.......wt.
+t.www.w...w
 ...wpwgww..
 .w.w.w..w..
 .w...w.www.
-.....w.....`,
+...t.w.....`,
     map`
 ww..w.w
 .......
@@ -120,56 +154,58 @@ w.wwww.
 .ww..www.
 ...w..ww.
 ww.ww....
-...wgw.w.
+...wgwtw.
 .www.ww..
 .....w..w`,
     map`
-....ww..
-w.w...w.
+t...ww..
+w.w...wt
 p..ww...
-wwwg.w..
-w..w...w`,
+wwwg.w.t
+w.tw...w`,
 	map`
 ...w..
 ...w.g
 .w.ww.
 .w....
-pw..ww`,
+pw.tww`,
   	map`
-.ww......
+.ww...t..
 p...w...w
 w.w.wgw.w
 w.w.www.w
 w.w.....w
 w.wwww..w
-.........`,
+t......t.`,
     map`
 .........
 ww.www.w.
 .....w.w.
 .w.wgw...
-..wwww.w.
+.twwww.w.
 ..wwpwww.
 w........`,
     map`
 pw...w...w.....
 .w.w.w.w.w.www.
-...w...w...w.w.
+...w...w...wtw.
 wwwwwwwwwwww.w.
 g...w......w.w.
 ww.w..w..w.....
-.....w.w..www..
-ww.w..w.w......`,
+.t...w.w..www..
+ww.w.tw.w......`,
   	map`
 ........w..wp
 ww.w.ww.ww.w.
 ww.w.ww...ww.
 .......ww....
-ww.ww....wwww
+ww.wwt...wwww
 ww..ww.w....g
 .ww...ww.ww.w
-....www......`
+.t..www....t.`
 ]
+
+let stepsRemaining = stepLimits[level];
 
 setMap(levels[level])
 
@@ -178,40 +214,96 @@ setPushables({
 })
 
 onInput("w", () => {
-	getFirst(player).y -= 1
-    playTune(myTune1);
+    if (stepsRemaining > 0) {
+      getFirst(player).y -= 1;
+      stepsRemaining--;
+      playTune(myTune1);
+    }
 });
 
 onInput("a", () => {
-	getFirst(player).x -= 1
-    playTune(myTune2);
+    if (stepsRemaining > 0) {
+      getFirst(player).x -= 1;
+      stepsRemaining--;
+      playTune(myTune2);
+    }
 });
 
 onInput("s", () => {
-	getFirst(player).y += 1
-    playTune(myTune3);
+    if (stepsRemaining > 0) {
+      getFirst(player).y += 1;
+      stepsRemaining--;
+      playTune(myTune3);
+    }
 });
 
 onInput("d", () => {
-	getFirst(player).x += 1
-    playTune(myTune4);
+    if (stepsRemaining > 0) {
+      getFirst(player).x += 1;
+      stepsRemaining--;
+      playTune(myTune4);
+    }
 });
 
 onInput("j", () => {
-	setMap(levels[level])
+	setMap(levels[level]);
+    stepsRemaining = stepLimits[level];
 });
 
-afterInput(() => {
-  const goalsCovered = tilesWith(player, goal);
+// afterInput(() => {
+//   const goalsCovered = tilesWith(player, goal);
 
+//   if(goalsCovered.length >= 1) {
+//     level = level + 1;
+
+//     if(level < levels.length) {
+//       setMap(levels[level]);
+//     }
+//     else {
+//       addText("you win!", { y: 4, color: color`6` });
+//     }
+//   }
+// })
+
+afterInput(() => {
+  clearText();
+  addText(`Level ${level + 1}  Steps: ${stepsRemaining}`, { y: 14, color: color`D` });
+
+  const playerObj = getFirst(player);
+  const playerTile = getTile(playerObj.x, playerObj.y);
+
+  // trap
+  if (playerTile.some(t => t.type === trap)) {
+    addText("You hit a trap!", { y: 6, color: color`3` });
+    setTimeout(() => {
+      setMap(levels[level]);
+      stepsRemaining = stepLimits[level];
+      clearText();
+    }, 1000);
+    return;
+  }
+
+  // step limit
+  if (stepsRemaining <= 0) {
+    addText("Out of steps!", { y: 6, color: color`3` });
+    setTimeout(() => {
+      setMap(levels[level]);
+      stepsRemaining = stepLimits[level];
+      clearText();
+    }, 1000);
+    return;
+  }
+
+  // win
+  const goalsCovered = tilesWith(player, goal);
   if(goalsCovered.length >= 1) {
     level = level + 1;
 
     if(level < levels.length) {
       setMap(levels[level]);
-    }
-    else {
-      addText("you win!", { y: 4, color: color`6` });
+      stepsRemaining = stepLimits[level];
+    } else {
+      addText("You win!", { y: 4, color: color`6` });
     }
   }
-})
+});
