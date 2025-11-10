749392b27978df57b62f9bfab4becc8f9f937bd9
leolevd <74661500+leolevd@users.noreply.github.com>
2025-11-10 19:30:41 +0200

    Add bots and glue traps to Capture The Base game

--- games/Capture_The_Base.js
+++ games/Capture_The_Base.js
@@ -11,6 +11,11 @@ const player2 = "2"
 const p1base = "f"
 const p2base = "w"
 const back = "b"
+const botF = "F"
+const botW = "W"
+const glueTrap = "g"
+
+
 
 setLegend(
   [ player, bitmap`
@@ -79,24 +84,72 @@ setLegend(
 7775555555555777
 7777777777777777`], [back, bitmap`
 4444444444444444
+4442224444444424
+4442444444444444
+2244444224444444
+4444444424444444
+4244444444442444
 4444444444444444
+4444422444244444
+4444442444444444
 4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444
-4444444444444444`]
+4244444444442444
+4444444424444444
+2244424224244444
+4442444444444444
+4442224444244424
+4444424444244444`], [botF, bitmap`
+.D..H.H..H.H..D.
+DDDH.H.HH.H.HDDD
+.D..H.H..H.H..D.
+0D000000000000D0
+0333333333333330
+0332333333332330
+0329233333329230
+0329223333229230
+0329992332999230
+0332292332922330
+0333323333233330
+0333333333333330
+0333333333333330
+0333333333333330
+0333333333333330
+0000000000000000`], [botW, bitmap`
+.3..6.6..6.6..3.
+3336.6.66.6.6333
+.3..6.6..6.6..3.
+0300000000000030
+0555555555555550
+0552555555552550
+0527255555527250
+0527225555227250
+0527772552777250
+0552272552722550
+0555525555255550
+0555555555555550
+0555555555555550
+0555555555555550
+0555555555555550
+0000000000000000`], [glueTrap, bitmap`
+...2222222222...
+...22222222222..
+..2221112222222.
+..22211111122222
+2222111111111222
+2222111111111122
+2222111H11H11122
+2222111111111122
+222111HHHHHH1122
+222111HHHHHH1222
+2221111HHHH11222
+.221111111111222
+.222111111122222
+.22222222222222.
+...22222222222..
+...22222222.....`]
 )
 
-setSolids([player, player2])
+setSolids([player, player2, botF, botW])
 
 let level = 0
 let scoreW = 0
@@ -105,10 +158,10 @@ let finished = 0
 const levels = [
   map`
 ............
-............
+..........W.
 ..1........w
-f........2..
-............
+f....g...2..
+.F..........
 ............`
 ]
 
@@ -116,17 +169,56 @@ function touching(sprite1, sprite2) {
   return sprite1.x === sprite2.x && sprite1.y === sprite2.y;
 }
 
+
 setMap(levels[level])
 setBackground(back)
 
 setPushables({
   [player]: [player2],
-  [player2]: [player]
+  [player2]: [player],
+  [botF]: [player2],
+  [botW]: [player]
 })
 
-function move(player, d) {
+function botMove() {
   if (finished) return;
-  if (player == "1") {
+  if (getFirst(botF).y != getFirst(player2).y) {
+    if (getFirst(botF).y > getFirst(player2).y) {
+      getFirst(botF).y -= 1
+    } else {
+      getFirst(botF).y += 1
+    }
+  }
+  if (getFirst(botF).x != getFirst(player2).x) {
+    if (getFirst(botF).x > getFirst(player2).x) {
+      getFirst(botF).x -= 1
+    } else {
+      getFirst(botF).x += 1
+    }
+  }
+
+
+  if (getFirst(botW).y != getFirst(player).y) {
+    if (getFirst(botW).y > getFirst(player).y) {
+      getFirst(botW).y -= 1
+    } else {
+      getFirst(botW).y += 1
+    }
+  }
+  if (getFirst(botW).x != getFirst(player).x) {
+    if (getFirst(botW).x > getFirst(player).x) {
+      getFirst(botW).x -= 1
+    } else {
+      getFirst(botW).x += 1
+    }
+  }
+}
+
+function move(p, d) {
+  if (touchingGlueTrap(p)) return
+  if (finished) return;
+  if (p == "1") {
+    
     switch (d) {
       case "l":
         getFirst(player).x -= 1
@@ -140,7 +232,7 @@ function move(player, d) {
       case "d":
         getFirst(player).y += 1
     }
-  } else if (player == "2") {
+  } else if (p == "2") {
     switch (d) {
       case "l":
         getFirst(player2).x -= 1
@@ -159,6 +251,17 @@ function move(player, d) {
 
 }
 
+function createGlueTrap() {
+  if (getAll(glueTrap).length >= 1) {
+    getAll(glueTrap)[getAll(glueTrap).length-1].remove()
+  }
+  addSprite(Math.floor(Math.random() * width()), Math.floor(Math.random() * height()), glueTrap)
+  setTimeout(function() {
+    getAll(glueTrap)[getAll(glueTrap).length-1].remove()
+    createGlueTrap()
+  }, 5000)
+} createGlueTrap()
+
 function drawScore() {
   addText("Water: " + scoreW, {x: 10, y: 0, color: color`5`})
   addText("Fire: " + scoreF, {x: 10, y: 2, color: color`3`})
@@ -168,8 +271,22 @@ function clear() {
   setMap(levels[level])
   clearText()
   finished = 0
+  drawScore()
 }
+function touchingGlueTrap(playerWhich) {
 
+  if (getAll(glueTrap).length === 0) return false
+  if (playerWhich == "1") {
+    for (let element of getAll(glueTrap)) {
+      if (touching(getFirst(player), element)) return true
+    }
+  } else {
+    for (let element of getAll(glueTrap)) {
+      if (touching(getFirst(player2), element)) return true
+    }
+  }
+  return false
+}
 function check() {
   if (finished) return;
   if (touching(getFirst(player), getFirst(p2base))) {
@@ -178,12 +295,14 @@ function check() {
     drawScore()
     setTimeout(clear, 5000)
     finished = 1
+    return
   } else if (touching(getFirst(player2), getFirst(p1base))) {
     scoreW += 1
     addText("Water won!", {x: 0, y: 2, color: color`5`})
     drawScore()
     setTimeout(clear, 5000)
     finished = 1
+    return
   }
 }
 
@@ -214,5 +333,5 @@ onInput("l", () => {
 afterInput(() => {
     check()
 })
-
+setInterval(botMove, 1000)
 drawScore()
