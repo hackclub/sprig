d18af12d359a6f5868e36a9fbd32d9a179da46c5
Aymen Djoudi <djoudiaymenbusiness@gmail.com>
2026-03-21 02:06:23 +0100

    Update The-Maze-Runner.js

--- games/The-Maze-Runner.js
+++ games/The-Maze-Runner.js
@@ -12,41 +12,38 @@ const coin = "c";
 const enemy = "e";
 const exit = "x";
 const melody = tune`
-500,
-500: E4~500 + C5/500 + G4^500,
-500: F4~500 + D5/500,
-500: G4~500 + C5/500,
-500: A4~500 + G4^500,
-500: C5/500 + E4~500 + G4-500,
-500: D5/500 + F4~500,
-500: C5/500 + G4~500,
-500: A4~500 + G4^500,
-500: C5/500 + E4~500 + G4-500,
-500: D5/500 + F4~500,
-500: C5/500 + G4~500,
-500: A4~500 + G4^500,
-500: C5/500 + E4~500 + G4-500,
-500: D5/500 + F4~500,
-500: C5/500 + G4~500,
-500: A4~500 + G4^500,
-500: C5/500 + E4~500 + G4-500,
-500: D5/500 + F4~500,
-500: C5/500 + G4~500,
-500: A4~500 + G4^500,
-500: C5/500 + E4~500 + G4-500,
-500: D5/500 + F4~500,
-500: C5/500 + G4~500,
-500: A4~500 + G4^500,
-500: C5/500 + E4~500 + G4-500,
-500: D5/500 + F4~500,
-500: C5/500 + G4~500,
-500: A4~500 + G4^500,
-500: C5/500 + E4~500 + G4-500,
-500: D5/500 + F4~500,
-500: C5/500 + G4~500`;
-
-// Current game state (used for future expansion)
-let gameState = "playing"; 
+476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4^476.1904761904762,
+476.1904761904762: D5/476.1904761904762,
+476.1904761904762: G4~476.1904761904762 + C5/476.1904761904762,
+476.1904761904762: A4~476.1904761904762 + G4^476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4-476.1904761904762,
+476.1904761904762: D5/476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4~476.1904761904762,
+476.1904761904762: A4~476.1904761904762 + G4^476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4-476.1904761904762,
+476.1904761904762: D5/476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4~476.1904761904762,
+476.1904761904762: A4~476.1904761904762 + G4^476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4-476.1904761904762,
+476.1904761904762: D5/476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4~476.1904761904762,
+476.1904761904762: A4~476.1904761904762 + G4^476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4-476.1904761904762,
+476.1904761904762: D5/476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4~476.1904761904762,
+476.1904761904762: A4~476.1904761904762 + G4^476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4-476.1904761904762,
+476.1904761904762: D5/476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4~476.1904761904762,
+476.1904761904762: A4~476.1904761904762 + G4^476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4-476.1904761904762,
+476.1904761904762: D5/476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4~476.1904761904762,
+476.1904761904762: A4~476.1904761904762 + G4^476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4-476.1904761904762,
+476.1904761904762: D5/476.1904761904762,
+476.1904761904762: C5/476.1904761904762 + G4~476.1904761904762`; 
 
 setLegend(
   [ player, bitmap`
@@ -119,6 +116,8 @@ setLegend(
 ................`],
   [ exit, bitmap`
 ................
+................
+................
 .....444444.....
 ....47777774....
 ...477DDDD774...
@@ -129,32 +128,61 @@ setLegend(
 ...477DDDD774...
 ....47777774....
 .....444444.....
-................
-................
 ................
 ................
 ................`]
 );
 
-// Play melody
 playTune(melody, 3);
 
 const levels = [
   map`
 wwwwwwwwwwwwwwww
 ws....c........w
-w.wwww.wwww.ww.w
-w....w...cw....w
-www.wwww.ww.ww.w
-w....w....w....w
-w.ww.w....wwww.w
-wc.w...cc.w....w
-wcwwwww.wwwww..w
-w....w......e..w
-ww.wwwwwwwwww.cw
+w.wwww..www.ww.w
+w....w...cw..w.w
+www.ww..wwww.w.w
+w..e.w.e..ww.w.w
+w..w.w....ww.w.w
+wc.w...cc.ww...w
+wcwwwww.wwwwww.w
+w...........e..w
+ww.wwww.wwwww.cw
 w..cc........c.w
-wewwwwww.wwwwwww
-w.............xw
+wewwwwww.www.www
+w.......e.....xw
+wwwwwwwwwwwwwwww`,
+  map`
+wwwwwwwwwwwwwwww
+ws.....c..e.wccw
+w.www.......wccw
+w...w.w.....we.w
+www.w....w..w..w
+w...w..c.w.....w
+w.www.wwwwwww..w
+wc.......cw....w
+w..ww.e.c.wwww.w
+w.........w.e..w
+w..c...w.cw....w
+w...w..wwwww...w
+we..w......w...w
+w...w.c..c....xw
+wwwwwwwwwwwwwwww`,
+ map`
+wwwwwwwwwwwwwwww
+ws..........ec.w
+w.www.w...wwww.w
+w.c...w...w....w
+www.wwwww.w.ww.w
+w....ec........w
+w.www.ww.wwwww.w
+w...w....c.....w
+w.w.w.wwwwwwww.w
+w.w...w......e.w
+w.ww.ww.wwwwww.w
+w.....ce....c..w
+w.wwwwwww.wwww.w
+we............xw
 wwwwwwwwwwwwwwww`,
   map`
 wwwwwwwwwwwwwwww
@@ -162,63 +190,63 @@ ws..cw...ccwcwcw
 wwww.....cc..w.w
 w.w..w.wwwwwww.w
 w.w..w.......w.w
-w.w..wwwwwww.w.w
+w.w..www.www.w.w
 w.w...e..w...w.w
 w.w...ww.w.....w
 wcwww.ww.wwwweww
 w........w.....w
 w.wwewww...w...w
 w....c.w...w...w
-w...ww.www.wwwww
-w..........c...w
-wwwwwwwwwwwwww.x`,
+w...ww.www.www.w
+wc.........c..xw
+wwwwwwwwwwwwwwww`,
   map`
 wwwwwwwwwwwwwwww
 w........w.....w
-ws.......w.....w
+ws..........e..w
 wwwwww...w.....w
-w..eww.wwwwwww.w
-wcc....ww.cccw.w
-w...w......www.w
+w..e....wwwwww.w
+wcc.......cccw.w
+w...w.....wwww.w
 w.wwwww...w....w
-w.w.......w...xw
-w.w....w..w....w
-w.w....w..wwww.w
-w.wwwwwwwww..w.w
-w.........wwww.w
-w..............w
+w.........w...xw
+w....e.w..w....w
+w......w....e..w
+w.wwwwwww.w....w
+w....w....w....w
+w.e............w
 wwwwwwwwwwwwwwww`,
   map`
 wwwwwwwwwwwwwwww
-ws.ww..w......xw
+ws..w..we.....xw
 w...w..w...w...w
-w..ww.cwww.wwwww
-w...w.cw..c....w
-w.w.wccw..cww..w
-www.w..ww..wwwww
-w...w..www.w...w
-w..ww..w...w...w
-w......w..www.ew
+w...w.cwww.wwwww
+w.....cw..c.e.cw
+www.wccw.....cew
+w...w..w...wwwww
+w...weew...w...w
+w..ww......w...w
+w.........www.ew
 w..e.......w...w
 wwwww.cc.c.....w
-w...w...cc.....w
-w.wwwwww.......w
+wc....e.cc.....w
+wc.e...........w
 wwwwwwwwwwwwwwww`,
   map`
 wwwwwwwwwwwwwwww
-ws..........w.xw
+ws..ecccccccw.xw
 w.wwwwwwwwwww..w
 w...........e..w
-w.wwwww..wwww..w
+w.www.w..wwww..w
 w.w...w..w..w.ww
-w.w...w..wwcw..w
-w.w...w...wcw..w
-w.w...www.wcww.w
-w.w.....w.w.w..w
-w.w.....w...w..w
+w.www.w..wwcw..w
+w.w.c.w...wcw..w
+w.w.wwwww.wcww.w
+w.w.w...w.wew..w
+w.w...w.w...w..w
 w.wwwww.w.....ww
-w....w..wwww...w
-w..............w
+wc.w.w..wwww...w
+w..w..c........w
 wwwwwwwwwwwwwwww`,
   map`
 wwwwwwwwwwwwwwww
@@ -235,13 +263,77 @@ w.w.w.w.wwwww..w
 w.wcwcwc.ccw..ew
 w.www.wwww.e...w
 w.c...c..w.....w
-wwwwwwwwwwwwwwww`
+wwwwwwwwwwwwwwww`,
+  map`
+wwwwwwwwwwwwwwwwwwww
+ws......w........c.w
+w.......w..e..c.c.cw
+w..c.c..w..ww......w
+w..wwww.wc.ww.we...w
+w..wwwwe......w..e.w
+w.e......c.e..wc...w
+www.www.ww.wwww..cew
+wwwewww..e.wwww.c..w
+ww....wwwwwwwww.wwww
+ww.wwc.c........wwww
+wwe...wwwwwwwc.cwwww
+ww.wwwwwwwwww.w.wwww
+ww.ww.c.c....cw.wwww
+ww.wwcwwwwewwww.wwww
+ww.........wwwwcwwww
+wwcww.wwwwcwwww.wcww
+wwc..ewwww.wwww.w.ww
+wwwwwwwwww...c..c.xw
+wwwwwwwwwwwwwwwwwwww`,
+  map`
+wwwwwwwwwwwwwwwwwww
+wswcccccccccccccccw
+wcwcwwwwwwwwwwwwwcw
+wcwcwcccccccccccwcw
+wcwcwcwwwwwwwwwcwcw
+wcwcwcwcccccccwcwcw
+wcwcwcwcwwwwwcwcwcw
+wcwcwcwcwcccwcwcwcw
+wcwcwcwcwcwcwcwcwcw
+wcwcwcwcwxwcwcwcwcw
+wcwcwcwcwwwcwcwcwcw
+wcwcwcwcccccwcwcwcw
+wcwcwcwwwwwwwcwcwcw
+wcwcwcccccccccwcwcw
+wcwcwwwwwwwwwwwcwcw
+wcwcccccccccccccwcw
+wcwwwwwwwwwwwwwwwcw
+wcccccccccccccccccw
+wwwwwwwwwwwwwwwwwww`
 ];
-// Tracks the current level index
 let currentLevel = 0;
-setMap(levels[currentLevel]);
-
-setSolids([player, wall, enemy]);
+setMap(map`
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwww`);
+addText("The Maze Runner", { x: 2, y: 4, color: color`3` });
+addText("Press 'i' to start", { x: 1, y: 8, color: color`1` });
+onInput("i", () => {
+  clearText();
+  setMap(levels[currentLevel]);
+});
+onInput("j", () => {
+  clearText();
+  restartGame();
+});
 
 let score = 0;
 let isMoving = false;
@@ -273,9 +365,6 @@ function restartGame() {
   setMap(levels[currentLevel]);
 }
 
-onInput("j", () => {
-  restartGame();
-});
 
 function movePlayer() {
   if (isMoving) {
@@ -284,90 +373,104 @@ function movePlayer() {
       const newX = playerSprite.x + moveDirection.x;
       const newY = playerSprite.y + moveDirection.y;
 
-      // Ensure the new position is not a wall
       if (!getTile(newX, newY).some(t => t.type === wall)) {
         playerSprite.x = newX;
         playerSprite.y = newY;
 
-        // Check if player collects a coin
         const coins = getTile(newX, newY).filter(t => t.type === coin);
         if (coins.length > 0) {
           coins.forEach(coinSprite => {
-            coinSprite.remove();  // Remove the coin
+            coinSprite.remove();
           });
           score += 1;
         }
 
-        // Check if player reaches the exit
         if (tilesWith(player, exit).length > 0) {
           currentLevel++;
           if (currentLevel < levels.length) {
             setMap(levels[currentLevel]);
           } else {
-            // Display final message on a clean map
             setMap(map`
-..w...w.........
-...w.w..........
-....w..www.w.w..
-....w..w.w.w.w..
-....w..www.www..
-................
-........w.......
-................
-..w...w.w.w..w..
-..w...w.w.ww.w..
-..w.w.w.w.w.ww..
-..ww.ww.w.w..w..
-................
-................
-................`);
+ccccccccccccccccc
+cwxxxwxwwwwxwxxwc
+cxwwwxxwxxwxwxxwc
+cxxwxxxwxxwxwxxwc
+cxxwxxxwxxwxwxxwc
+cxxwxxxwwwwxwwwwc
+cxxxxxxxxxxxxxxxc
+ccccccccccccccccc
+cxxxxxxxxwxxxcxxc
+cwxxxxxwxxxwxxxwc
+cwxxxxxwxwxwwxxwc
+cwxxxxxwxwxwxwxwc
+cxwxwxwxxwxwxxwwc
+cxxwxwxxxwxwxxxwc
+cxxxxxxxxxxxxxxxc
+ccccccccccccccccc`);
 
             isMoving = false;
 
-            // Listen for the "j" key press to restart the game
-            onInput("j", restartGame);
           }
         }
 
-        // Check if player is caught by an enemy
         if (tilesWith(player, enemy).length > 0) {
-          console.log("Game Over. Score: " + score);
+          addText("Game Over",{ x: 5, y: 1, color: `3` });
+          addText("Score: " + score,{ x: 5, y: 3, color: `1` });
+          addText("Press'j'To Restart",{ x: 1, y: 13, color: `1` });
           isMoving = false;
           setMap(map`
-................
-................
-................
-................
-................
-..w...www.w.....
-..w...w.w.w.....
-..w...w.w.w.....
-..w...w.w.w.....
-..w...w.w.w.....
-..www.www.www...
-................
-................
-................
-................`);
+....................
+....................
+....................
+....................
+....................
+....w...wwww.w......
+....w...w..w.w......
+....w...w..w.w......
+....w...w..w.w......
+....w...w..w.w......
+....www.wwww.www....
+....................
+....................
+....................
+....................
+....................`);
         }
       }
 
-      isMoving = false; // Stop movement after reaching the next tile
+      isMoving = false
     }
   }
 }
 
-// Call movePlayer function at regular intervals
 setInterval(movePlayer, 100);
 
-// Enemy movement (random)
 setInterval(() => {
   const allEnemies = getAll(enemy);
+
   allEnemies.forEach((e) => {
+    let dx = 0;
+    let dy = 0;
+
     const direction = Math.floor(Math.random() * 4);
-    if (direction === 0 && e.y > 0) e.y -= 1;
-    else if (direction === 1 && e.y < height() - 1) e.y += 1;
-    else if (direction === 2 && e.x > 0) e.x -= 1;
-    else if (direction === 3 && e.x < width() - 1) e.x += 1;
+
+    if (direction === 0) dy = -1; 
+    if (direction === 1) dy = 1;  
+    if (direction === 2) dx = -1; 
+    if (direction === 3) dx = 1;
+
+    const newX = e.x + dx;
+    const newY = e.y + dy;
+
+    if (
+      newX >= 0 &&
+      newX < width() &&
+      newY >= 0 &&
+      newY < height() &&
+      !getTile(newX, newY).some(t => t.type === wall)
+    ) {
+      e.x = newX;
+      e.y = newY;
+    }
   });
 }, 1000);
