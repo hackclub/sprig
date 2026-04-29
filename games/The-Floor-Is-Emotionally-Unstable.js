ba0889abc036a82d6a5dec328abd9d4300af24e8
NellowTCS <neeljaiswal23@gmail.com>
2026-04-28 20:30:07 -0600

    Add metadata comments to The Floor Is Emotionally Unstable

--- games/The-Floor-Is-Emotionally-Unstable.js
+++ games/The-Floor-Is-Emotionally-Unstable.js
@@ -1,6 +1,9 @@
 /*
 @title: The Floor Is Emotionally Unstable
 @description: The floor has feelings. Navigate its mood swings to reach the exit!
+@author: NellowTCS
+@tags: ["puzzle", "maze", "movement", "randomness", "multi-level"]
+@addedOn: 2026-04-25
 */
 
 const player = "p"
