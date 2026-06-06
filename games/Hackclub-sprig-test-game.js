b6a811eb98b3990347581a944b45afd948fbb026
Drew Fitzgerald <crabeykiller@gmail.com>
2026-06-06 23:17:17 +1200

    Sprig App - Hackclub sprig test game

---
+++ games/Hackclub-sprig-test-game.js
@@ -0,0 +1,56 @@
+/*
+First time? Check out the tutorial game:
+https://sprig.hackclub.com/gallery/getting_started
+
+@title: Test club game
+@description: 
+@author: 
+@tags: ['tag1', 'tag2']
+@addedOn: 2025-00-00
+*/
+
+const player = "playerTile"
+
+setLegend(
+  [ player, bitmap`
+................
+................
+.......000......
+.......0.0......
+......0..0......
+......0...0.0...
+....0003.30.0...
+....0.0...000...
+....0.05550.....
+......0...0.....
+.....0....0.....
+.....0...0......
+......000.......
+......0.0.......
+.....00.00......
+................` ]
+)
+
+setSolids([])
+
+let level = 0
+const levels = [
+  map`
+    p.
+    ..
+  `
+]
+
+setMap(levels[level])
+
+setPushables({
+  [ player ]: []
+})
+
+onInput("s", () => {
+  getFirst(player).y += 1
+})
+
+afterInput(() => {
+  
+})
