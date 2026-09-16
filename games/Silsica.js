311f240c9085941ea65156910a22d3e4a0d7ef00
aadistech1-glitch <aadistech1@gmail.com>
2026-09-16 13:00:46 +0530

    Change addedOn date to 2026-09-16
    Updated the addedOn date to reflect the correct release date.

--- games/Silsica.js
+++ games/Silsica.js
@@ -3,7 +3,7 @@
 @description: "this is a maze where a player escapes the silsica using portals and other upgrades"
 @author: Aadi Vinayak A Pillai
 @tags: ['puzzle']
-@addedOn: 2026-08-19
+@addedOn: 2026-09-16
 
 Instructions:
 Press W,A,S,D for moving
