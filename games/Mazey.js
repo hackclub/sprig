1d9801e8cae56b5e47275060cfab6db0bc7083fe
K-BIDDOLIYA <kartikbiddoliya@gmail.com>
2026-09-17 13:12:42 +0530

    Update Mazey.js

--- games/Mazey.js
+++ games/Mazey.js
@@ -11,9 +11,10 @@ const wall = "w";
 const one = "o";
 const two = "t";
 const three = "3";
-const fakegoal = "f";
+const key = "k";
 const goal = "g";
 let count = 0;
+let hasKey = false;
 
 setLegend(
   [ player, bitmap`
@@ -66,7 +67,7 @@ setLegend(
 2222220000000222
 2220000000000002
 2222222222222222
-2222222222222222`],
+2222222222222222` ],
   [ two, bitmap`
 2222222222222222
 2222222222222222
@@ -83,7 +84,7 @@ setLegend(
 2222000222222222
 2222000000000222
 2222222222222222
-2222222222222222`],
+2222222222222222` ],
   [ three, bitmap`
 2222222222222222
 2222222222222222
@@ -100,24 +101,24 @@ setLegend(
 2222200002222222
 2222222222222222
 2222222222222222
-2222222222222222`],
-  [ fakegoal, bitmap`
-7777777777777777
-7000000000000007
-7077777777777707
-7070000000000707
-7070777777770707
-7070700000070707
-7070707777070707
-7070707007070707
-7070707007070707
-7070707777070707
-7070700000070707
-7070777777770707
-7070000000000707
-7077777777777707
-7000000000000007
-7777777777777777`],
+2222222222222222` ],
+  [ key, bitmap`
+................
+................
+......000.......
+.....0...0......
+.....0...0......
+......000.......
+......0.........
+......0.........
+......0.........
+......000.......
+.....0...0......
+....0.....0.....
+................
+................
+................
+................` ],
   [ goal, bitmap`
 7777777777777777
 7000000000000007
@@ -134,20 +135,21 @@ setLegend(
 7070000000000707
 7077777777777707
 7000000000000007
-7777777777777777`]
+7777777777777777`
+  ]
 );
 
 const levels = [
   map`
 wwwwwwwwwwwwwwwwwwwwwww
-wg.wf.......w.........w
+wg.wk.......w.........w
 w..w..wwww..w.w.......w
 w..w..w.....w...w.ww..w
 w..w..w...wwww..w..w..w
-w..w..w.w...fw..w..w..w
+w..w..w.w...kw..w..w..w
 w.www...ww...ww.w.www.w
 w.....w.w..w....w.....w
-w..w............wf....w
+w..w............wk....w
 w..w..www..w.w.www....w
 w..w...w.p.w..........w
 w......w...w.....wwww.w
@@ -156,30 +158,30 @@ w...........www..w..w.w
 w..ww..w.ww......w..w.w
 w......w.....w...w..w.w
 w.wwwwww..wwwwwwwwwww.w
-w.....fw........w...w.w
+w.....kw........w...w.w
 w..........www..www.w.w
 w..wwww.....w...w...w.w
 w...w...ww..w...w...w.w
-w.........w.........wfw
+w.........w.........wkw
 wwwwwwwwwwwwwwwwwwwwwww`,
-  
+
   map`
 wwwwwwwwwwwwwwwwwwwwwwwww
 w......w.....w....w...wgw
-w....w.fw.ww.w....w.w.w.w
+w....w.kw.ww.w....w.w.w.w
 w.ww...ww...............w
 w......w...w..www...ww..w
 w......w...w....w.......w
-w...w......w....fwww.ww.w
+w...w......w....kwww.ww.w
 w..www...........w......w
-w....fwww.www.wwwww.....w
+w....kwww.www.wwwww.....w
 w...w...............www.w
 w.w.....www...w.........w
 w.w..w......w.w....www..w
 w....w......w.w.w.w..w..w
 w....w...w...pw.......w.w
 www......wwwwww...w...w.w
-wf............w....w..w.w
+wk............w....w..w.w
 w..w.www......w.ww.w..w.w
 w.......w.ww.w.......ww.w
 w..wwww.w..w.w..........w
@@ -187,40 +189,40 @@ w.......w..w.w..www..w..w
 w.......................w
 w.w.ww...............w..w
 w.w....www..www..wwww...w
-w............fw.........w
+w............kw.........w
 wwwwwwwwwwwwwwwwwwwwwwwww`,
-  
+
   map`
 wwwwwwwwwwwwwwwwwwwwwwwwww
-wf..........w.....w......w
+wk..........w.....w......w
 w.ww.www.........www....ww
 w..w.....wwww...w...w....w
-ww.w..w.wwf....ww.....w.fw
+ww.w..w.wwk....ww.....w.kw
 w..w................w..www
 w...w.wwww..wwww..w...w..w
 w.w.w...........ww....w..w
 ww......w.w.w.........w..w
 w.w.....w...w.....w...w..w
 w...w.....w.....ww..w....w
-w.....w......fwww........w
+w.....w......kwww........w
 w.ww....ww..ww...........w
 w.w....w.............w...w
 w.....w.....w..w.w....w..w
 wgw.........w.........w..w
 www...........ww.....ww..w
-ww..www..w...w.......wf..w
+ww..www..w...w.......wk..w
 w...w.w....p...w...w.www.w
 w...w....www.ww....w.....w
 w.........w....w...w...w.w
 w.w.......w....www.......w
 wwwwwww............w.w...w
-w.w.fww.w.w.ww......www..w
+w.w.kww.w.w.ww......www..w
 w...........w............w
 wwwwwwwwwwwwwwwwwwwwwwwwww`,
 
   map`
 wwwwwwwwwwwwwwwwwwwwwwwwwwww
-w..............wg......w..fw
+w..............wg......w..kw
 w....ww......www..w....w...w
 w.w....w.w.....w..w.......ww
 w..........w...w...........w
@@ -228,36 +230,36 @@ w....w.........wwww........w
 w...w..w...w..........www..w
 w...w......w...........w...w
 w...w..w...w....w......w...w
-wf.....w....w.w.w.w.w..w...w
+wk.....w....w.w.w.w.w..w...w
 wwwwwwwww..w....w......w...w
 w.........ww...www.....w...w
 w..........w.........w.....w
 w...w...p............w.w.www
 w...w...w..w....w..........w
-www...ww...w...fw..........w
+www...ww...w...kw..........w
 w.............wwwww..w...www
-w....w....w..........w....fw
+w....w....w..........w....kw
 w....w..............w...w..w
 w...w............w......w..w
 w...w.....w..ww..w......w..w
-wf..w.....ww.....w.w....w..w
+wk..w.....ww.....w.w....w..w
 ww.....w.......w......w....w
 w......w.......w...w..w.w..w
 w...w..ww......ww..w..w....w
 w...w.....w....w...w.......w
-w...w.....wf...............w
+w...w.....wk...............w
 wwwwwwwwwwwwwwwwwwwwwwwwwwww`,
 
   map`
 wwwwwwwwwwwwwwwwwwwwwww
-wg.wf.......w.........w
+wg.wk.......w.........w
 w..w..wwww..w.w.......w
 w..w..w.....w...w.ww..w
 w..w..w...wwww..w..w..w
-w..w..w.w...fw..w..w..w
+w..w..w.w...kw..w..w..w
 w.www...ww...ww.w.www.w
 w.....w.w..w....w.....w
-w..w............wf....w
+w..w............wk....w
 w..w..www..w.w.www....w
 w..w...w.p.w..........w
 w......w...w.....wwww.w
@@ -266,37 +268,50 @@ w...........www..w..w.w
 w..ww..w.ww......w..w.w
 w......w.....w...w..w.w
 w.wwwwww..wwwwwwwwwww.w
-w.....fw........w...w.w
+w.....kw........w...w.w
 w..........www..www.w.w
 w..wwww.....w...w...w.w
 w...w...ww..w...w...w.w
-w.........w.........wfw
-wwwwwwwwwwwwwwwwwwwwwww`,  
+w.........w.........wkw
+wwwwwwwwwwwwwwwwwwwwwww`,
   
-]
+];
 
 setMap(levels[count]);
 setSolids([ player, wall ]);
 
-onInput("s", () => { getFirst(player).y += 1 })
-onInput("d", () => { getFirst(player).x += 1 })
 onInput("a", () => { getFirst(player).x -= 1 })
 onInput("w", () => { getFirst(player).y -= 1 })
-onInput("j", () => { setMap(levels[count = 0]) })
+onInput("s", () => { getFirst(player).y += 1 })
+onInput("d", () => { getFirst(player).x += 1 })
+
+
+onInput("j", () => {
+  count = 0;
+  hasKey = false;
+  setMap(levels[count]);
+});
 
 afterInput(() => {
-  // Check if player is on the same tile as a goal
   const playerTile = getFirst(player);
-  const goalsAtPlayer = getTile(playerTile.x, playerTile.y).filter(t => t.type === goal);
+  const tiles = getTile(playerTile.x, playerTile.y);
 
-  if (goalsAtPlayer.length > 0) {
-    count++; // Move to next level index
+  if (tiles.some(t => t.type === key)) {
+    hasKey = true;
+    tiles.find(t => t.type === key).remove();
+    clearText();
+    addText("KEY FOUND!", { x: 5, y: 1, color: color`3` });
+  }
+
+  if (tiles.some(t => t.type === goal) && hasKey) {
+    count++;
+    hasKey = false;
 
     if (count < levels.length) {
       setMap(levels[count]);
     } else {
-      // No more levels left!
-      addText("You Win!");
+      clearText();
+      addText("YOU WON!", { x: 6, y: 7, color: color`3` });
     }
   }
 });
