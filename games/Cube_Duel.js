ace8381a486c6620a5d6b1eb3b394e959452165e
A-Ritvik <1ritvik22@gmail.com>
2026-09-19 17:56:35 -0400

    Fix formatting of addedOn date in Cube_Duel.js

--- games/Cube_Duel.js
+++ games/Cube_Duel.js
@@ -3,7 +3,7 @@
 @author: Ritvik
 @description: A red cube chasing you down as you use attacks, vaults, movement, and waiting to take them down across different maps.
 @tags: ['action', 'puzzle', 'turn-based']
-@addedOn: `2026-09-19`
+@addedOn: 2026-09-19
 
  CONTROLS:
   'w', 'a', 's', 'd' : Move (Restores 1 Energy)
